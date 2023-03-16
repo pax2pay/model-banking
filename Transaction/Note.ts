2765ff22a8a9eb994d7b931ebb4cd16aba93e28e
Anton Kroner <anton.kroner@pax2pay.com>
2023-03-16 13:15:12 +0100

    Fixed Note.Creatable is.

--- Transaction/Note.ts
+++ Transaction/Note.ts
@@ -1,4 +1,5 @@
 import * as isoly from "isoly"
+
 export interface Note extends Note.Creatable {
 	author: string
 	created: isoly.DateTime
@@ -8,13 +9,13 @@ export namespace Note {
 	export function fromCreatable(note: Creatable, author: string): Note {
 		return { ...note, created: isoly.DateTime.now(), author: author }
 	}
-	export namespace Creatable {
-		export function is(value: Creatable | any): value is Note {
-			return true
-		}
-	}
 	export interface Creatable {
 		text?: string
 		action?: "approve" | "reject"
+	}
+	export namespace Creatable {
+		export function is(value: Creatable | any): value is Creatable {
+			return true
+		}
 	}
 }
