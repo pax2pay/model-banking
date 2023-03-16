a40125a9e1861204c3027d05b1c65325ac4a6fc7
Anton Kroner <anton.kroner@pax2pay.com>
2023-03-16 10:41:58 +0100

    Added finalized transaction status.

--- Transaction/index.ts
+++ Transaction/index.ts
@@ -16,7 +16,7 @@ export interface Transaction extends TransactionCreatable {
 	readonly transacted?: isoly.DateTime
 	balance: number
 	operations: Operation[]
-	status: "review" | "approved" | "rejected" | "processing"
+	status: "review" | "approved" | "rejected" | "processing" | "finalized"
 	notes: TransactionNote[]
 }
 
