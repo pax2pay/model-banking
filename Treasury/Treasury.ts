ef35fdce0f0d7235e7249d744b24fcc8383e8584
SiFalorSum <nilshansander@hotmail.com>
2023-03-31 09:05:41 +0200

    Sorry, premature bravery on my side

--- Treasury/Treasury.ts
+++ Treasury/Treasury.ts
@@ -2,4 +2,4 @@ import * as isoly from "isoly"
 import { Balances } from "../Balances"
 import { Fiat } from "./Fiat"
 
-export type Treasury = Partial<Record<isoly.Currency, { eMoney: Balances.Entry; fiat: Fiat }>>
+export type Treasury = Partial<Record<isoly.Currency, { eMoney: Partial<Record<Balances.Entry, number>>; fiat: Fiat }>>
