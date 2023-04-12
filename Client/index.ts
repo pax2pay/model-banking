316fc97162e50c2a754484f6924d9182d32997f0
Emilia By <emilia.by@pax2pay.com>
2023-04-12 14:47:29 +0200

    removed error from client

--- Client/index.ts
+++ Client/index.ts
@@ -21,11 +21,6 @@ export class Client extends rest.Client<gracely.Error> {
 		const result: Client = new Client(
 			(httpClient = new http.Client<gracely.Error>(server, key, {
 				appendHeader: request => ({ ...request.header, realm: result.realm, organization: result.organization }),
-				onError: async (request: http.Request, response: http.Response) => {
-					const output = { ...response, body: await response.body }
-					console.error("client.onError", output)
-					return false
-				},
 			}))
 		)
 		if (load)
