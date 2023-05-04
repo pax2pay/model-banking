02fa7f9c95242337241c6913d40d30078e602f39
Anton Kroner <anton.kroner@pax2pay.com>
2023-05-04 18:56:48 +0200

    Idea for API key permissions.

---
+++ Key/index.ts
@@ -0,0 +1,36 @@
+import { userwidgets } from "@userwidgets/model"
+import { Realm } from "../Realm"
+
+export interface Creatable extends userwidgets.User.Key.Creatable {
+	permissions: {
+		"*"?: userwidgets.User.Permissions.Application | undefined
+		[organizationId: string]: userwidgets.User.Permissions.Organization | undefined
+	} & Permissions.Realms
+}
+
+export interface Key extends Creatable, userwidgets.User.Key {}
+
+export namespace Permissions {
+	export type Realms = Partial<
+		Record<
+			Realm | "*",
+			userwidgets.User.Permissions.Permission & {
+				organizations: Permissions.Organizations
+				treasury: userwidgets.User.Permissions.Permission
+			}
+		>
+	>
+	export type Organizations = Record<
+		string | "*",
+		userwidgets.User.Permissions.Permission & {
+			accounts: Permissions.Accounts
+			rules: userwidgets.User.Permissions.Permission
+		}
+	>
+	export type Accounts = Record<
+		string | "*",
+		{ transactions: userwidgets.User.Permissions.Permission; rail: userwidgets.User.Permissions.Permission }
+	>
+
+
+}
