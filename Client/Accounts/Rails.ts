d800d43dd3f9d395dd4ddc91566ada2cc73c119d
Aftonsaga <114990644+Aftonsaga@users.noreply.github.com>
2023-05-24 09:35:40 +0200

    fixed rail method (#31)
    Co-authored-by: Emilia By <emilia.by@pax2pay.com>

--- Client/Accounts/Rails.ts
+++ Client/Accounts/Rails.ts
@@ -2,15 +2,13 @@ import * as gracely from "gracely"
 import * as http from "cloudly-http"
 import * as rest from "cloudly-rest"
 import { Rail } from "../../Rail"
+import { Supplier } from "../../Supplier"
 
 export class Rails extends rest.Collection<gracely.Error> {
 	constructor(client: http.Client) {
 		super(client)
 	}
-	async create(organization: string, account: string, supplier: string): Promise<Rail | gracely.Error> {
-		return this.client.post<Rail>(`/api/account/${account}/rail`, supplier, {
-			organization: organization,
-			account: account,
-		})
+	async create(account: string, supplier: Supplier): Promise<Rail | gracely.Error> {
+		return this.client.post<Rail>(`/api/account/${account}/rail`, supplier)
 	}
 }
