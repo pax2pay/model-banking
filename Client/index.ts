fc668ac95b85ddd4c6cc56bab0b6d840e0e95e5e
Anton Kroner <anton.kroner@pax2pay.com>
2023-06-14 10:00:51 +0200

    Updated userwidgets, removed unused stuff.

--- Client/index.ts
+++ Client/index.ts
@@ -1,23 +1,19 @@
 import { gracely } from "gracely"
 import { userwidgets } from "@userwidgets/model"
 import { http } from "cloudly-http"
-import * as rest from "cloudly-rest"
+import { rest } from "cloudly-rest"
 import { Accounts } from "./Accounts"
-import { Application as ClientApplication } from "./Application"
 import { Cards } from "./Cards"
-import { Me as ClientMe } from "./Me"
 import { Operations } from "./Operations"
-import { Organization as ClientOrganization } from "./Organization"
 import { Organizations } from "./Organizations"
 import { Transactions } from "./Transactions"
 import { Treasury } from "./Treasury"
-import { User as ClientUser } from "./User"
 import { Version } from "./Version"
 
 export class Client extends rest.Client<gracely.Error> {
 	realm?: string
 	organization?: string
-	readonly userwidgets = new userwidgets.ClientCollection(this.client, "/widgets")
+	readonly userwidgets = new userwidgets.ClientCollection(this.client, { pathPrefix: "/widgets" })
 	readonly accounts = new Accounts(this.client)
 	readonly operations = new Operations(this.client)
 	readonly organizations = new Organizations(this.client)
@@ -39,13 +35,5 @@ export class Client extends rest.Client<gracely.Error> {
 	}
 }
 export namespace Client {
-	export type Application = ClientApplication
-	export const Application = ClientApplication
-	export type Organization = ClientOrganization
-	export const Organization = ClientOrganization
-	export type Me = ClientMe
-	export const Me = ClientMe
-	export type User = ClientUser
-	export const User = ClientUser
 	export type Unauthorized = (client: rest.Client<never>) => Promise<boolean>
 }
--- Treasury/index.ts
+++ Treasury/index.ts
@@ -1,6 +1,5 @@
 import { Account as TreasuryAccount } from "./Account"
 import { Balance as TreasuryBalance } from "./Balance"
-import { Client as TreasuryClient } from "./Client"
 import { Fiat as TreasuryFiat } from "./Fiat"
 export { Treasury } from "./Treasury"
 
@@ -8,9 +7,7 @@ export namespace Treasury {
 	export type Account = TreasuryAccount
 	export type Balance = TreasuryBalance
 	export type Fiat = TreasuryFiat
-	export type Client = TreasuryClient
 	export const Balance = TreasuryBalance
-	export const Client = TreasuryClient
 	export namespace Account {
 		export type Creatable = TreasuryAccount.Creatable
 		export const Creatable = TreasuryAccount.Creatable
