4bac34f441068565b1b8a48b2308f5fb22956de8
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2023-06-19 17:39:08 +0200

    Added extern type to fiat accounts. (#46)
    * Added extern type to fiat accounts.
    
    * Fixed is method.
    
    * Fixed is methods.

--- Treasury/Account/Creatable.ts
+++ Treasury/Account/Creatable.ts
@@ -1,4 +1,5 @@
 import { isoly } from "isoly"
+import { isly } from "isly"
 import { Realm } from "../../Realm"
 import { Supplier } from "../../Supplier"
 
@@ -7,20 +8,17 @@ export interface Creatable {
 	realm: Realm
 	supplier: Supplier
 	currencies: isoly.Currency[]
-	type: "safeguarded" | "other"
+	type: "safeguarded" | "other" | "external"
 }
 
 export namespace Creatable {
-	export function is(value: Creatable | any): value is Creatable {
-		return (
-			value &&
-			typeof value == "object" &&
-			typeof value.name == "string" &&
-			Realm.is(value.realm) &&
-			Supplier.is(value.supplier) &&
-			Array.isArray(value.currencies) &&
-			value.currencies.every(isoly.Currency.is) &&
-			(value.type == "safeguarded" || value.type == "other")
-		)
-	}
+	export const type = isly.object<Creatable>({
+		name: isly.string(),
+		realm: isly.fromIs("realm", Realm.is),
+		supplier: isly.fromIs("supplier", Supplier.is),
+		currencies: isly.fromIs("Account.Creatable.currencies", isoly.Currency.is).array(),
+		type: isly.string(["safeguarded", "other", "external"]),
+	})
+	export const is = type.is
+	export const flaw = type.flaw
 }
