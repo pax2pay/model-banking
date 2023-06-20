b0ba6384c7af17a5091b7e7f2cd95e7cd0721248
Marcus Hanses <marcus.hanses@pax2pay.com>
2023-06-20 10:16:20 +0200

    Split Entry into failed and succeeded.

--- Settlement/Entry.ts
+++ Settlement/Entry.ts
@@ -1,8 +1,10 @@
 import { isoly } from "isoly"
 import { isly } from "isly"
 
-export interface Entry {
-	status: "succeeded" | "failed"
+export type Entry = Succeeded | Failed
+
+export interface Succeeded {
+	status: "succeeded"
 	type: "capture" | "cancel" | "refund"
 	account: string
 	card: string
@@ -10,14 +12,44 @@ export interface Entry {
 	amount: [isoly.Currency, number]
 	fee: [isoly.Currency, number]
 }
+export interface Failed {
+	status: "failed"
+	type?: "capture" | "cancel" | "refund"
+	account?: string
+	card?: string
+	transaction?: string
+	amount?: [isoly.Currency, number]
+	fee?: [isoly.Currency, number]
+	reason?: string
+	data?: any
+}
 export namespace Entry {
-	export const type = isly.object<Entry>({
-		status: isly.string(),
-		type: isly.string(),
-		account: isly.string(),
-		card: isly.string(),
-		transaction: isly.string(),
-		amount: isly.tuple(isly.fromIs("Entry.amount", isoly.Currency.is), isly.number()),
-		fee: isly.tuple(isly.fromIs("Entry.fee", isoly.Currency.is), isly.number()),
-	})
+	export const type = isly.union<Entry, Succeeded, Failed>(Succeeded.type, Failed.type)
+	export const is = type.is
+	export namespace Succeeded {
+		export const type = isly.object<Succeeded>({
+			status: isly.string("succeeded"),
+			type: isly.union(isly.string("capture"), isly.string("cancel"), isly.string("refund")),
+			account: isly.string(),
+			card: isly.string(),
+			transaction: isly.string(),
+			amount: isly.tuple(isly.fromIs("Entry.amount", isoly.Currency.is), isly.number()),
+			fee: isly.tuple(isly.fromIs("Entry.fee", isoly.Currency.is), isly.number()),
+		})
+		export const is = type.is
+	}
+	export namespace Failed {
+		export const type = isly.object<Failed>({
+			status: isly.string("failed"),
+			type: isly.union(isly.string("capture"), isly.string("cancel"), isly.string("refund")).optional(),
+			account: isly.string().optional(),
+			card: isly.string().optional(),
+			transaction: isly.string().optional(),
+			amount: isly.tuple(isly.fromIs("Entry.amount", isoly.Currency.is), isly.number()).optional(),
+			fee: isly.tuple(isly.fromIs("Entry.fee", isoly.Currency.is), isly.number()).optional(),
+			reason: isly.string().optional(),
+			data: isly.any().optional(),
+		})
+		export const is = type.is
+	}
 }
--- Settlement/index.ts
+++ Settlement/index.ts
@@ -4,31 +4,30 @@ import { isly } from "isly"
 import { Transaction } from "../Transaction"
 import { Entry as SettlementEntry } from "./Entry"
 
-export type Settlement = Succeeded | Failed
-
-export interface Base {
-	id: string
-	created: [string, isoly.DateTime]
-	configuration: string
-	status: string
-}
-
-export interface Failed extends Base {
-	status: "failed"
-	reason: string
-}
-
-export interface Succeeded extends Base {
-	settled?: { user: string; created: isoly.DateTime; transaction: string }
-	amount: Partial<Record<isoly.Currency, number>>
-	fee: Partial<Record<isoly.Currency, number>>
-	entries: SettlementEntry[]
-	status: "succeeded"
-}
+export type Settlement = Settlement.Succeeded | Settlement.Failed
 
 export namespace Settlement {
-	export const type = isly.union<Settlement, Succeeded, Failed>(
-		isly.object<Succeeded>({
+	export const type = isly.union<Settlement, Succeeded, Failed>(Succeeded.type, Failed.type)
+	export interface Base {
+		id: string
+		created: [string, isoly.DateTime]
+		configuration: string
+		status: string
+	}
+	export interface Succeeded extends Base {
+		settled?: { user: string; created: isoly.DateTime; transaction: string }
+		amount: Partial<Record<isoly.Currency, number>>
+		fee: Partial<Record<isoly.Currency, number>>
+		entries: SettlementEntry[]
+		status: "succeeded"
+	}
+	export interface Failed extends Base {
+		status: "failed"
+		reason: string
+	}
+
+	export namespace Succeeded {
+		export const type = isly.object<Succeeded>({
 			id: isly.string(),
 			created: isly.tuple(isly.string(), isly.fromIs("Settlement.created", isoly.DateTime.is)),
 			configuration: isly.string(),
@@ -43,15 +42,19 @@ export namespace Settlement {
 			fee: isly.record(isly.fromIs("Settlement.entries.fee", isoly.Currency.is), isly.number()),
 			entries: SettlementEntry.type.array(),
 			status: isly.string("succeeded"),
-		}),
-		isly.object<Failed>({
+		})
+		export const is = type.is
+	}
+	export namespace Failed {
+		export const type = isly.object<Failed>({
 			id: isly.string(),
 			created: isly.tuple(isly.string(), isly.fromIs("Settlement.created", isoly.DateTime.is)),
 			configuration: isly.string(),
 			status: isly.string("failed"),
 			reason: isly.string(),
 		})
-	)
+		export const is = type.is
+	}
 	export const is = type.is
 	export const flaw = type.flaw
 	export type Result = (Transaction | (gracely.Error & { id: string }))[]
