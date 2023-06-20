8fb24b6b21566394bdbdbc8066cd619771aa0771
Nils Hansander <nilshansander@hotmail.com>
2023-06-20 10:49:43 +0200

    realm dependent supplier type (#50)
    * realm dependent supplier type
    
    * removed comment

--- Realm/index.ts
+++ Realm/index.ts
@@ -1,10 +1,10 @@
 import { isly } from "isly"
+import { Supplier as modelSupplier } from "../Supplier"
 
-export const Realms = ["test", "testUK", "uk", "eu"] as const
-export type Realm = typeof Realms[number]
+export type Realm = typeof Realm.realms[number]
 
 export namespace Realm {
-	export const realms: Realm[] = [...Realms]
+	export const realms = ["test", "testUK", "uk", "eu"] as const
 	export const type: isly.Type<Realm> = isly.union(
 		isly.string("test"),
 		isly.string("testUK"),
@@ -14,5 +14,23 @@ export namespace Realm {
 	export const is = type.is
 	export function toString(): string {
 		return realms.toString().replaceAll(",", ", ") + "."
+	}
+	export const suppliers: Record<Realm, modelSupplier[]> = {
+		test: ["paxgiro"],
+		testUK: ["clearbank"],
+		uk: ["clearbank"],
+		eu: [],
+	}
+	export interface Suppliers extends Record<Realm, modelSupplier[]> {
+		test: ["paxgiro"]
+		testUK: ["clearbank"]
+		uk: ["clearbank"]
+		eu: []
+	}
+	export type Supplier<P extends keyof Suppliers> = Pick<Suppliers, P>[P][number]
+	export namespace Supplier {
+		export function is(realm: Realm, supplier: modelSupplier | any): supplier is Supplier<Realm> {
+			return suppliers[realm].includes(supplier)
+		}
 	}
 }
