e7d0c9d4b998fdaac40eb3414eaaf17e72c43d70
Emilia By <emilia.by@pax2pay.com>
2023-06-27 10:45:29 +0200

    Added settle function to Settlements in client.

--- Client/Settlements.ts
+++ Client/Settlements.ts
@@ -11,7 +11,10 @@ export class Settlements extends rest.Collection<gracely.Error> {
 	async fetch(settlement: string): Promise<Settlement | gracely.Error> {
 		return this.client.get<Settlement>(`/card/settlement/${settlement}`)
 	}
-	async list(): Promise<Omit<Settlement, "entries">[] | gracely.Error> {
-		return this.client.get<Omit<Settlement, "entries">[] & { cursor?: string | undefined }>(`/card/settlement`)
+	async list(): Promise<Settlement.Summary | gracely.Error> {
+		return this.client.get<Settlement.Summary & { cursor?: string | undefined }>(`/card/settlement`)
+	}
+	async settle(settlement: string): Promise<Record<"transaction", string> | gracely.Error> {
+		return this.client.put<Record<"transaction", string>>(`/card/settlement/${settlement}/settled`, {})
 	}
 }
