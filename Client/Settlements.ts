09ca851919ce4d4536b5096d90bd88f76719d6c9
Anton Kroner <anton.kroner@pax2pay.com>
2023-06-28 13:42:49 +0200

    Updated settlement client.

--- Client/Settlements.ts
+++ Client/Settlements.ts
@@ -1,4 +1,5 @@
 import { gracely } from "gracely"
+import { isoly } from "isoly"
 import { http } from "cloudly-http"
 import * as rest from "cloudly-rest"
 import { Settlement } from "../Settlement"
@@ -8,13 +9,19 @@ export class Settlements extends rest.Collection<gracely.Error> {
 		super(client)
 	}
 
-	async fetch(settlement: string): Promise<Settlement | gracely.Error> {
-		return this.client.get<Settlement>(`/card/settlement/${settlement}`)
+	async create(configuration: string): Promise<Settlement | gracely.Error> {
+		return this.client.post<Settlement>(`/card/settlement`, { configuration: configuration })
+	}
+	async fetch(id: string, created: isoly.DateTime): Promise<Settlement | gracely.Error> {
+		return this.client.get<Settlement>(`/card/settlement/${id}?created=${created}`)
 	}
 	async list(): Promise<Settlement.Summary[] | gracely.Error> {
 		return this.client.get<Settlement.Summary[] & { cursor?: string | undefined }>(`/card/settlement`)
 	}
-	async settle(settlement: string): Promise<Settlement | gracely.Error> {
+	async remove(settlement: string): Promise<Settlement | gracely.Error> {
+		return this.client.delete<Settlement>(`/card/settlement/${settlement}`)
+	}
+	async update(settlement: string): Promise<Settlement | gracely.Error> {
 		return this.client.patch<Settlement>(`/card/settlement/${settlement}`, {})
 	}
 }
