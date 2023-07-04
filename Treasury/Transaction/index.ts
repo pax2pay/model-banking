d71088b6caf61c31466a436c1c48931857d88732
Marcus Hanses <marcus.hanses@pax2pay.com>
2023-07-04 13:56:17 +0200

    Added treasury transaction.

---
+++ Treasury/Transaction/index.ts
@@ -0,0 +1,33 @@
+import * as cryptly from "cryptly"
+import * as isoly from "isoly"
+import { Rail } from "../../Rail"
+import { Creatable as TransactionCreatable } from "./Creatable"
+
+export interface Transaction extends TransactionCreatable {
+	debtor: Rail
+	readonly id: cryptly.Identifier
+	readonly created: isoly.DateTime
+}
+export namespace Transaction {
+	export function is(value: any | Transaction): value is Transaction {
+		return (
+			value &&
+			typeof value == "object" &&
+			Rail.is(value.debtor) &&
+			cryptly.Identifier.is(value.id) &&
+			isoly.DateTime.is(value.created) &&
+			Creatable.is({ ...value })
+		)
+	}
+	export function fromCreatable(transaction: TransactionCreatable, debtor: Rail): Transaction {
+		return {
+			debtor: debtor,
+			id: cryptly.Identifier.generate(8),
+			created: isoly.DateTime.now(),
+			...transaction,
+		}
+	}
+
+	export const Creatable = TransactionCreatable
+	export type Creatable = TransactionCreatable
+}
