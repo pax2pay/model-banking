121b5bc44caf29f509fa0e708f02986ed9351305
Anton Kroner <anton.kroner@pax2pay.com>
2023-07-10 10:59:43 +0200

    Some preset names to work with.

--- Card/Preset.spec.ts
+++ Card/Preset.spec.ts
@@ -1,10 +1,8 @@
 import { pax2pay } from "../index"
 
-const preset: pax2pay.Card.Preset = "example1"
-
 describe("Card.Preset", () => {
 	it("is() truthy", () => {
-		expect(pax2pay.Card.Preset.is(preset)).toBeTruthy()
+		expect(pax2pay.Card.Preset.is("p2p-mc-200")).toBeTruthy()
 	})
 	it("is() falsy", () => {
 		expect(pax2pay.Card.Preset.is("not a preset")).toBeFalsy()
