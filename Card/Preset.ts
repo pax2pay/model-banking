494be8c26774e080d7d0b52484febf1b7000f7de
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2023-07-10 11:02:08 +0200

    Some preset names to work with. (#66)

--- Card/Preset.ts
+++ Card/Preset.ts
@@ -1,10 +1,10 @@
 import { isly } from "isly"
 
-const preset = ["example1"] as const
-
-export type Preset = typeof preset[number]
+export type Preset = typeof Preset.names[number]
 
 export namespace Preset {
-	export const type = isly.string(preset)
+	export const names = ["example1", "p2p-pg-200", "p2p-mc-200"] as const
+	export const type = isly.string(names) //isly.union(isly.string("p2p-pg-200"), isly.string("p2p-mc-200"))
 	export const is = type.is
+	export const flaw = type.flaw
 }
