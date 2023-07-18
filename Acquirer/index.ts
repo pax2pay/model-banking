37d13a80ce2faa3701883427f9315f795848f4e9
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2023-07-18 14:49:34 +0200

    Added more stuff to settlement entries. (#72)

---
+++ Acquirer/index.ts
@@ -0,0 +1,18 @@
+import { isoly } from "isoly"
+import { isly } from "isly"
+
+export interface Acquirer {
+	id: string
+	number: string
+	country: isoly.CountryCode.Alpha2
+}
+
+export namespace Acquirer {
+	export const type = isly.object<Acquirer>({
+		id: isly.string(),
+		number: isly.string(),
+		country: isly.fromIs("Acquirer.country", isoly.CountryCode.Alpha2.is),
+	})
+	export const is = type.is
+	export const flaw = type.flaw
+}
