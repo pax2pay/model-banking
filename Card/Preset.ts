acc85172af2f3a520c48175f422d6cf82fc6b55b
marcushanses <79835961+marcushanses@users.noreply.github.com>
2023-07-21 12:16:27 +0200

    Added presets cleaned them up. (#77)

--- Card/Preset.ts
+++ Card/Preset.ts
@@ -3,7 +3,7 @@ import { isly } from "isly"
 export type Preset = typeof Preset.names[number]
 
 export namespace Preset {
-	export const names = ["example1", "p2p-pg-200", "p2p-mc-200", "new-preset"] as const
+	export const names = ["p2p-pg-200", "p2p-mc-200", "p2p-ta-pg-200", "p2p-ta-mc-200"] as const
 	export const type = isly.string(names) //isly.union(isly.string("p2p-pg-200"), isly.string("p2p-mc-200"))
 	export const is = type.is
 	export const flaw = type.flaw
