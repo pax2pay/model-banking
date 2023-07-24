63d363fb98b911523567530538f2d414e767129d
Anton Kroner <anton.kroner@pax2pay.com>
2023-07-24 11:54:16 +0200

    Added tests.

---
+++ Rule/index.ts
@@ -0,0 +1,38 @@
+import { selectively } from "selectively"
+import { isly } from "isly"
+import { definitions } from "./definitions"
+
+const action = ["review", "reject", "flag"] as const
+type Action = typeof action[number]
+const kind = ["authorization", "outbound", "inbound"] as const
+export interface Rule {
+	name: string
+	description: string
+	action: typeof action[number]
+	type: typeof kind[number]
+	condition: string
+	flags: string[]
+}
+
+export namespace Rule {
+	export const type = isly.object<Rule>({
+		name: isly.string(),
+		description: isly.string(),
+		action: isly.string(action),
+		type: isly.string(kind),
+		condition: isly.string(),
+		flags: isly.string().array(),
+	})
+	export const is = type.is
+	export const flaw = type.flaw
+	export function stringify(rule: Rule): string {
+		return `{ label: ${rule.name}, action: ${rule.action}, type: ${rule.type}, condition: ${rule.condition}, description: ${rule.description}. }`
+	}
+	export function evaluate<S extends object>(rules: Rule[], state: S): Record<Action, Rule[]> {
+		const result: Record<Action, Rule[]> = { review: [], reject: [], flag: [] }
+		rules.forEach(
+			r => selectively.resolve(definitions, selectively.parse(r.condition)).is(state) && result[r.action].push(r)
+		)
+		return result
+	}
+}
