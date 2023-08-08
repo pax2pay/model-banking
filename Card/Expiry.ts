60a4e05ce43907c6ac4a1f02577d27d97d73e73e
Anton Kroner <anton.kroner@pax2pay.com>
2023-08-08 09:59:41 +0200

    Added correct padding function to expiry DateTime converter.

--- Card/Expiry.ts
+++ Card/Expiry.ts
@@ -12,7 +12,8 @@ export namespace Expiry {
 	export const type = isly.tuple<Expiry>(isly.number([...year]), isly.number([...month])) // Deconstructing to remove readonly.
 	export const is = type.is
 	export function toDateTime(expiry: Expiry): isoly.DateTime {
-		const month = expiry[1].toString().length == 2 ? `${expiry[1]}` : `0${expiry[1]}`
-		return isoly.DateTime.nextMonth(`20${expiry[0]}-${month}-01T00:00:01.000Z`)
+		return isoly.DateTime.nextMonth(
+			"20" + expiry[0].toString() + "-" + expiry[1].toString().padStart(2, "0") + "-01T00:00:01.000Z"
+		)
 	}
 }
