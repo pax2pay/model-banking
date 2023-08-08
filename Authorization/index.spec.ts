9a150b4e440a6da6749bb7f0b036e131a979d7a2
Aftonsaga <114990644+Aftonsaga@users.noreply.github.com>
2023-08-08 14:48:53 +0200

    Authorization status reason as string array. (#96)

---
+++ Authorization/index.spec.ts
@@ -0,0 +1,57 @@
+import { pax2pay } from "../index"
+
+describe("authorization", () => {
+	it("reason array", () => {
+		expect(pax2pay.Authorization.is(authorization1)).toBeTruthy()
+	})
+	it("reason string", () => {
+		expect(pax2pay.Authorization.is(authorization2)).toBeTruthy()
+	})
+})
+
+const authorization1: pax2pay.Authorization = {
+	status: {
+		code: "05",
+		reason: ["Do not honor.", "Another string", "A third string"],
+	},
+	id: "ahgt3817",
+	card: "yRY9MYOl",
+	created: "2023-08-07T09:25:11.296Z",
+	amount: ["GBP", 100],
+	merchant: {
+		name: "Merchant",
+		id: "abcd1234",
+		category: "4511",
+		country: "KP",
+		address: "Streetname 1, 12345 Towncity",
+	},
+	acquirer: {
+		id: "2345erty",
+		number: "1351858913568",
+		country: "GB",
+	},
+	description: "An upcheck test authorization, to succeed",
+}
+const authorization2: pax2pay.Authorization = {
+	status: {
+		code: "05",
+		reason: "Do not honor.",
+	},
+	id: "ahgt3817",
+	card: "yRY9MYOl",
+	created: "2023-08-07T09:25:11.296Z",
+	amount: ["GBP", 100],
+	merchant: {
+		name: "Merchant",
+		id: "abcd1234",
+		category: "4511",
+		country: "KP",
+		address: "Streetname 1, 12345 Towncity",
+	},
+	acquirer: {
+		id: "2345erty",
+		number: "1351858913568",
+		country: "GB",
+	},
+	description: "An upcheck test authorization, to succeed",
+}
