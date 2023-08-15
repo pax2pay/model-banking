dd602088761738ee6232a3a81e7157ee3922a989
Marcus Hanses <marcus.hanses@pax2pay.com>
2023-08-15 13:41:34 +0200

    Change state to Creatable.

--- Card/Expiry.spec.ts
+++ Card/Expiry.spec.ts
@@ -1,8 +1,8 @@
 import { pax2pay } from "../index"
 
-const years = [23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
+const years = [23, 24]
 
-const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] as const
+const months = [10, 11, 12] as const
 describe("Card.Expiry", () => {
 	describe.each(years)("is()", year => {
 		describe.each(months)("truthy", month => {
--- Rule/State/Authorization.ts
+++ Rule/State/Authorization.ts
@@ -1,9 +1,9 @@
 import type { Authorization as ModelAuthorization } from "../../Authorization"
 
-export type Authorization = ModelAuthorization
+export type Authorization = ModelAuthorization.Creatable
 
 export namespace Authorization {
-	export function from(authorization: ModelAuthorization): Authorization {
+	export function from(authorization: ModelAuthorization.Creatable): Authorization {
 		return authorization
 	}
 }
