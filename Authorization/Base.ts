c12f5432ae5a18d2ff73ac466d7a194cea9a34e8
marcushanses <79835961+marcushanses@users.noreply.github.com>
2023-08-15 10:26:52 +0200

    Failed and approved authorization. (#101)
    * Failed and approved authorization.
    
    * Fix Test.

---
+++ Authorization/Base.ts
@@ -0,0 +1,40 @@
+import { cryptly } from "cryptly"
+import { isoly } from "isoly"
+import { isly } from "isly"
+import { Acquirer } from "../Acquirer"
+import { Merchant } from "../Merchant"
+import { Creatable } from "./Creatable"
+
+export type Base = {
+	id: cryptly.Identifier
+	card: string
+	created: isoly.DateTime
+	amount: [isoly.Currency, number]
+	merchant: Merchant
+	acquirer: Acquirer
+	description: string
+}
+export namespace Base {
+	export function fromCreatable(authorization: Creatable): Base {
+		return {
+			id: cryptly.Identifier.generate(8),
+			card: authorization.card,
+			created: isoly.DateTime.now(),
+			amount: authorization.amount,
+			merchant: authorization.merchant,
+			acquirer: authorization.acquirer,
+			description: authorization.description,
+		}
+	}
+	export const type = isly.object<Base>({
+		id: isly.fromIs("Authorization.id", cryptly.Identifier.is),
+		card: isly.string(),
+		created: isly.fromIs("Authorization.created", isoly.DateTime.is),
+		amount: isly.tuple(isly.fromIs("isoly.Currency", isoly.Currency.is), isly.number()),
+		merchant: Merchant.type,
+		acquirer: Acquirer.type,
+		description: isly.string(),
+	})
+	export const is = type.is
+	export const flaw = type.flaw
+}
