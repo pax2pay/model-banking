738e2568bff0452b84002c8c513f3f2e2a369935
Anton Kroner <anton.kroner@pax2pay.com>
2023-08-23 18:05:57 +0200

    Made it so country code uniquely determines the Address type.

--- Organization/Contact/Address/index.ts
+++ Organization/Contact/Address/index.ts
@@ -6,7 +6,7 @@ import { SE } from "./SE"
 export type Address = GB | SE | Default
 
 export namespace Address {
-	export const type = isly.union<Address, GB, Default>(GB.type, Default.type)
+	export const type = isly.union(GB.type, SE.type, Default.type)
 	export const is = type.is
 	export const flaw = type.flaw
 }
