6742e156bf78a3f3e990831d637627fd45519792
Anton Kroner <anton.kroner@pax2pay.com>
2023-08-24 11:21:07 +0200

    Removed visa.

--- Card/Scheme.ts
+++ Card/Scheme.ts
@@ -2,6 +2,6 @@ import { isly } from "isly"
 
 export type Scheme = typeof Scheme.schemes[number]
 export namespace Scheme {
-	export const schemes = ["mastercard", "visa"] as const
+	export const schemes = ["mastercard"] as const
 	export const type = isly.string(schemes)
 }
