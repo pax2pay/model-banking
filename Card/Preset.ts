04266e5c61bd65a931612aef706d57711e6f40a0
Anton Kroner <anton.kroner@pax2pay.com>
2023-08-28 09:55:09 +0200

    Made separate prefix for test presets.

--- Card/Preset.ts
+++ Card/Preset.ts
@@ -3,8 +3,8 @@ import { isly } from "isly"
 export type Preset = typeof Preset.names[number]
 
 export namespace Preset {
-	export const names = ["p2p-pg-200", "p2p-mc-200", "p2p-ta-pg-200", "p2p-ta-mc-200"] as const
-	export const type = isly.string(names) //isly.union(isly.string("p2p-pg-200"), isly.string("p2p-mc-200"))
+	export const names = ["p2p-mc-200", "test-mc-200", "test-pg-200", "test-ta-pg-200", "test-ta-mc-200"] as const
+	export const type = isly.string(names)
 	export const is = type.is
 	export const flaw = type.flaw
 }
