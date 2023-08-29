8ab10b217070cee52d5416d1e05ee99cfd020494
Marcus Hanses <marcus.hanses@pax2pay.com>
2023-08-29 10:18:22 +0200

    Creatable in state authorization.

--- Rule/State/Authorization.ts
+++ Rule/State/Authorization.ts
@@ -1,12 +1,12 @@
 import { Authorization as ModelAuthorization } from "../../Authorization"
 
-export type Authorization = ModelAuthorization
+export type Authorization = ModelAuthorization.Creatable
 
 export namespace Authorization {
-	export function from(authorization: ModelAuthorization): Authorization {
+	export function from(authorization: ModelAuthorization.Creatable): Authorization {
 		return authorization
 	}
-	export const type = ModelAuthorization.type
+	export const type = ModelAuthorization.Creatable.type
 	export const is = type.is
 	export const flaw = type.flaw
 }
