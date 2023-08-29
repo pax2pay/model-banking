3691554a864ac170ff100aee62ec51aeb7cb8581
Marcus Hanses <marcus.hanses@pax2pay.com>
2023-08-29 10:05:17 +0200

    Requested changes.

--- Amount.ts
+++ Amount.ts
@@ -4,7 +4,7 @@ import { isly } from "isly"
 export type Amount = [isoly.Currency, number]
 
 export namespace Amount {
-	export const type = isly.tuple(isly.fromIs("isoly.Currency", isoly.Currency.is), isly.number())
+	export const type = isly.tuple<Amount>(isly.string(isoly.Currency.types), isly.number())
 	export const is = type.is
 	export const flaw = type.flaw
 }
--- Amounts.ts
+++ Amounts.ts
@@ -4,7 +4,7 @@ import { isly } from "isly"
 export type Amounts = Partial<Record<isoly.Currency, number>>
 
 export namespace Amounts {
-	export const type = isly.record(isly.fromIs("isoly.Currency", isoly.Currency.is), isly.number())
+	export const type = isly.record<Amounts>(isly.string(isoly.Currency.types), isly.number())
 	export const is = type.is
 	export const flaw = type.flaw
 }
