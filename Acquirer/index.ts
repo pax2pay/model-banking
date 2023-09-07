92461bd32aae72cf702266ed514f44c4be2bc5c4
marcushanses <79835961+marcushanses@users.noreply.github.com>
2023-09-07 15:25:45 +0200

    Make acquirer country optional. (#120)

--- Acquirer/index.ts
+++ Acquirer/index.ts
@@ -4,14 +4,14 @@ import { isly } from "isly"
 export interface Acquirer {
 	id: string
 	number: string
-	country: isoly.CountryCode.Alpha2
+	country?: isoly.CountryCode.Alpha2
 }
 
 export namespace Acquirer {
 	export const type = isly.object<Acquirer>({
 		id: isly.string(),
 		number: isly.string(),
-		country: isly.fromIs("Acquirer.country", isoly.CountryCode.Alpha2.is),
+		country: isly.fromIs("Acquirer.country", isoly.CountryCode.Alpha2.is).optional(),
 	})
 	export const is = type.is
 	export const flaw = type.flaw
