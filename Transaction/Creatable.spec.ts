3382d19667621bcf7ebc14bbd1857ceeb9a904da
marcushanses <79835961+marcushanses@users.noreply.github.com>
2023-09-12 11:13:18 +0200

    Fix type for transaction creatable. (#125)

--- Transaction/Creatable.spec.ts
+++ Transaction/Creatable.spec.ts
@@ -2,8 +2,18 @@ import "jest"
 import { pax2pay } from "../index"
 
 describe("transaction", () => {
-	it("test", async () => {
+	it("note", async () => {
 		expect(pax2pay.Transaction.Note.Creatable.is({ text: "text", action: "approve" })).toBeTruthy()
 		expect(pax2pay.Transaction.Note.Creatable.is({})).toBeTruthy()
 	})
+	it("creatable", async () => {
+		expect(pax2pay.Rail.is(transaction.counterpart)).toBeTruthy()
+		expect(pax2pay.Transaction.Creatable.is(transaction)).toBeTruthy()
+	})
 })
+const transaction: pax2pay.Transaction.Creatable = {
+	counterpart: { type: "internal", identifier: "target12" },
+	currency: "USD",
+	amount: 100,
+	description: "internal transaction",
+}
