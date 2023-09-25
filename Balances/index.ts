b67245d8564d4026c460cbefe82633efde278e65
Anton Kroner <anton.kroner@pax2pay.com>
2023-09-25 17:55:37 +0200

    Moved Counterbalances, fixed operation.

--- Balances/index.ts
+++ Balances/index.ts
@@ -1,6 +1,5 @@
 import { isoly } from "isoly"
 import { isly } from "isly"
-import { Counters as CountersBalances } from "./Counters"
 
 export type Balances = Partial<Record<isoly.Currency, Partial<Record<Balances.Entry, number>>>>
 
@@ -12,9 +11,4 @@ export namespace Balances {
 		isly.record(isly.string(entries), isly.number())
 	)
 	export const is = type.is
-	export type Counters = CountersBalances
-	export const Counters = CountersBalances
-	export namespace Counters {
-		export type Counter = CountersBalances.Counter
-	}
 }
--- Operation/Changes.ts
+++ Operation/Changes.ts
@@ -1,60 +1,11 @@
 import { isly } from "isly"
 import { Balances } from "../Balances"
+import { Counterbalances } from "../Counterbalances"
 import { Change as Change } from "./Change"
 
-export type Changes = Partial<Record<Balances.Entry | Balances.Counters.Counter, Change>>
+export type Changes = Partial<Record<Balances.Entry | Counterbalances.Counter, Change>>
 
 export namespace Changes {
-	export const type = isly.object<Changes>({
-		actual: Change.type.optional(),
-		incomingReserved: Change.type.optional(),
-		outgoingReserved: Change.type.optional(),
-	})
+	export const type = isly.record<Changes>(isly.string([...Balances.entries, ...Counterbalances.entries]), Change.type)
 	export const is = type.is
-	export type Incoming = Pick<Record<Balances.Entry, Change<"add">>, "incomingReserved"> & { type: "incoming" }
-	export namespace Incoming {
-		export const type = isly.object<Incoming>({
-			incomingReserved: Change.Add.type,
-		})
-		export const is = type.is
-	}
-	export type FinalizeIncoming = Pick<Record<Balances.Entry, Change<"subtract">>, "incomingReserved"> &
-		Pick<Record<Balances.Entry, Change<"add">>, "actual"> & { type: "finalizeIncoming" }
-	export namespace FinalizeIncoming {
-		export const type = isly.object<FinalizeIncoming>({
-			actual: Change.Add.type,
-			incomingReserved: Change.Subtract.type,
-		})
-		export const is = type.is
-	}
-	export type Outgoing = Pick<Record<Balances.Entry, Change<"add">>, "outgoingReserved"> & { type: "outgoing" }
-	export namespace Outgoing {
-		export const type = isly.object<Outgoing>({
-			outgoingReserved: Change.Add.type,
-		})
-		export const is = type.is
-	}
-	export type FinalizeOutgoing = Pick<Record<Balances.Entry, Change<"subtract">>, "outgoingReserved"> &
-		Pick<Record<Balances.Entry, Change<"add">>, "actual"> & { type: "finalizeOutgoing" }
-	export namespace FinalizeOutgoing {
-		export const type = isly.object<FinalizeOutgoing>({
-			actual: Change.Add.type,
-			outgoingReserved: Change.Subtract.type,
-		})
-		export const is = type.is
-	}
-	export type Deposit = Pick<Record<Balances.Entry, Change<"add">>, "actual"> & { type: "deposit" }
-	export namespace Deposit {
-		export const type = isly.object<Deposit>({
-			actual: Change.Add.type,
-		})
-		export const is = type.is
-	}
-	export type Remove = Pick<Record<Balances.Entry, Change<"subtract">>, "actual"> & { type: "remove" }
-	export namespace Remove {
-		export const type = isly.object<Remove>({
-			actual: Change.Subtract.type,
-		})
-		export const is = type.is
-	}
 }
