61a1fb75860dbdee5c4ff96e633d514a1bca72a1
marcushanses <79835961+marcushanses@users.noreply.github.com>
2023-09-28 11:35:15 +0200

    Suggestion with tests. (#136)
    * Suggestion with tests.
    
    * Changed name on test.
    
    * Removed duplicate.

--- Rail/index.ts
+++ Rail/index.ts
@@ -12,28 +12,7 @@ export namespace Rail {
 	export type Type = typeof rails[number]
 	export const type = isly.string(rails)
 	export function compare(rails: [Rail, Rail]): boolean {
-		let result: boolean
-		if (rails[0].type != rails[1].type)
-			result = false
-		else
-			switch (rails[0].type) {
-				case "iban":
-					result = rails[1].type == "iban" && rails[0].iban == rails[1].iban
-					break
-				case "scan":
-					result = rails[1].type == "scan" && rails[0].sort == rails[1].sort && rails[0].account == rails[1].account
-					break
-				case "paxgiro":
-					result = rails[1].type == "paxgiro" && rails[0].identifier == rails[1].identifier
-					break
-				case "internal":
-					result = rails[1].type == "internal" && rails[0].identifier == rails[1].identifier
-					break
-				case "card":
-					result = rails[1].type == "card" && rails[0].id == rails[1].id
-					break
-			}
-		return result
+		return Object.entries(rails[0]).every(([key, value]: [keyof Rail, Rail[keyof Rail]]) => value == rails[1][key])
 	}
 	export function parse(value: string): Rail | undefined {
 		let result: Rail | undefined
