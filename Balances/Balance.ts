40290e07d2a36d95a5e8fe1c69b1a01fe057d5f8
Marcus Hanses <marcus.hanses@pax2pay.com>
2023-10-02 15:59:04 +0200

    Requested changes.

--- Balances/Balance.ts
+++ Balances/Balance.ts
@@ -6,7 +6,7 @@ export namespace Balance {
 	export type Entry = typeof Entry.values[number]
 	export namespace Entry {
 		export const values = ["actual", "incomingReserved", "outgoingReserved"] as const
-		export const type = isly.string(Entry.values)
+		export const type = isly.string(values)
 		export const is = type.is
 		export const flaw = type.flaw
 	}
--- Operation/Changes.ts
+++ Operation/Changes.ts
@@ -6,9 +6,9 @@ import { Change as Change } from "./Change"
 export type Changes = Partial<Record<Changes.Entry, Change>>
 
 export namespace Changes {
-	export const values = [...Balances.Balance.Entry.values, ...Counterbalances.Counterbalance.Entry.values]
 	export type Entry = Balances.Balance.Entry | Counterbalances.Counterbalance.Entry
 	export namespace Entry {
+		export const values = [...Balances.Balance.Entry.values, ...Counterbalances.Counterbalance.Entry.values]
 		export const type = isly.string(values)
 	}
 	export const type = isly.record<Changes>(Entry.type, Change.type)
