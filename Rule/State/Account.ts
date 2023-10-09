3c35e2c60382e71255ff1e8e774cab2cb7d94d3e
Anton Kroner <anton.kroner@pax2pay.com>
2023-10-09 11:36:25 +0200

    Fixed spelling.

--- Rule/State/Account.ts
+++ Rule/State/Account.ts
@@ -2,17 +2,17 @@ import type { Account as ModelAccount } from "../../Account"
 
 export interface Account extends ModelAccount {
 	transactions: {
-		today: { Count: number; amount: number }
-		incoming: { today: { Count: number; amount: number } }
-		outgoing: { today: { Count: number; amount: number } }
-		card: { today: { Count: number; amount: number } }
+		today: { count: number; amount: number }
+		incoming: { today: { count: number; amount: number } }
+		outgoing: { today: { count: number; amount: number } }
+		card: { today: { count: number; amount: number } }
 	}
 }
 
 export namespace Account {
 	export function from(
 		account: ModelAccount,
-		transactions: { today: { Count: number; amount: number }; card: { today: { Count: number; amount: number } } }
+		transactions: { today: { count: number; amount: number }; card: { today: { count: number; amount: number } } }
 	): Account {
 		return { ...account, transactions }
 	}
