21b74b370b3f17f22392c67a80e76d73ac10d135
marcushanses <79835961+marcushanses@users.noreply.github.com>
2023-10-19 11:55:25 +0200

    Implemented transaction cursor. (#155)

--- Client/Transactions/index.ts
+++ Client/Transactions/index.ts
@@ -9,7 +9,14 @@ import { Notes } from "./Notes"
 export class Transactions extends rest.Collection<gracely.Error> {
 	readonly Notes = new Notes(this.client)
 	constructor(client: http.Client) {
-		super(client)
+		const httpClient = new http.Client<gracely.Error>(client.url, client.key, {
+			appendHeader: client.appendHeader,
+			postprocess: async response =>
+				Array.isArray(await response.body)
+					? http.Response.create({ cursor: response.header.cursor, list: await response.body })
+					: response,
+		})
+		super(httpClient)
 	}
 	async create(account: string, transaction: Transaction.Creatable): Promise<Transaction | gracely.Error> {
 		return this.client.post<Transaction>(`/account/${account}/transaction`, transaction)
@@ -22,16 +29,24 @@ export class Transactions extends rest.Collection<gracely.Error> {
 		dateRange?: isoly.DateRange
 	}): Promise<(Transaction[] & { cursor?: string | undefined }) | gracely.Error> {
 		const query = Object.entries({
+			...(options?.cursor ? { cursor: options.cursor } : {}),
 			...(options?.index ? { index: options.index.join(",") } : {}),
 			...(options?.dateRange ?? {}),
 		})
 			.map(([k, v]) => `${k}=${v}`)
 			.join("&")
 		const path = options?.account ? `/account/${options.account}/transaction` : `/transaction`
-		return this.client.get<Transaction[] & { cursor?: string | undefined }>(
+		const listed = await this.client.get<{ list: Transaction[]; cursor?: string | undefined }>(
 			path + (query && "?" + query),
 			options?.limit ? { limit: options?.limit.toString() } : {}
 		)
+		let result: (Transaction[] & { cursor?: string | undefined }) | gracely.Error
+		if (!gracely.Error.is(listed)) {
+			result = listed.list
+			result.cursor = listed.cursor
+		} else
+			result = listed
+		return result
 	}
 }
 
