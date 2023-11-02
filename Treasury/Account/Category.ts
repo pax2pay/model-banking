5bc85a241dd4f256d884a6f39ce7b5f529621436
Anton Kroner <anton.kroner@pax2pay.com>
2023-11-02 15:41:04 +0100

    Buffer treasury account type.

--- Treasury/Account/Category.ts
+++ Treasury/Account/Category.ts
@@ -1,4 +1,4 @@
 export type Category = typeof Category.type[number]
 export namespace Category {
-	export const type = ["safeguarded", "unsafe", "other", "external"] as const
+	export const type = ["safeguarded", "unsafe", "buffer", "other", "external"] as const
 }
