3e45119fa20698511a563c5a459a97f34d79219e
Anton Kroner <anton.kroner@pax2pay.com>
2023-11-07 15:27:45 +0100

    Accidentally updated isly. Fixed state authorization is.

--- Rule/State/Authorization.ts
+++ Rule/State/Authorization.ts
@@ -1,4 +1,5 @@
 import { isoly } from "isoly"
+import { isly } from "isly"
 import { Authorization as ModelAuthorization } from "../../Authorization"
 
 export interface Authorization extends Omit<ModelAuthorization.Creatable, "amount"> {
@@ -8,7 +9,8 @@ export namespace Authorization {
 	export function from(authorization: ModelAuthorization.Creatable): Authorization {
 		return { ...authorization, time: isoly.DateTime.getTime(isoly.DateTime.now()) }
 	}
-	export const type = ModelAuthorization.Creatable.type
+	// isly.object().omit(): coming soon!!
+	export const type = isly.object<Authorization>({ ...(ModelAuthorization.type as any), time: isly.string() })
 	export const is = type.is
 	export const flaw = type.flaw
 }
