262f6025884400294fadf109ea025d4fb181e670
Marcus Hanses <marcus.hanses@pax2pay.com>
2023-11-09 12:05:07 +0100

    Added statistics to card state.

--- Rule/State/Card.ts
+++ Rule/State/Card.ts
@@ -3,24 +3,29 @@ import { isly } from "isly"
 import { Card as ModelCard } from "../../Card"
 import { Realm } from "../../Realm"
 
-export interface Card extends Omit<ModelCard, "limit"> {
-	used: { count: number; amount: number }
-	reject: { count: number }
+export interface Card extends Omit<ModelCard, "limit">, Card.Statistics {
 	age: { days: number; minutes: number }
 	limit: number
 	original: { currency: isoly.Currency; limit: number }
 }
 
 export namespace Card {
+	export type Statistics = {
+		used: { count: number; amount: number }
+		reject: { count: number }
+	}
+	const initialStatistics = {
+		used: { count: 0, amount: 0 },
+		reject: { count: 0 },
+	}
 	function ageFromTime(time: isoly.DateTime): Card["age"] {
 		const minutes = ~~(Date.now() - (isoly.DateTime.epoch(time, "milliseconds") / 1000) * 60)
 		return { days: ~~(minutes / (60 * 24)), minutes }
 	}
-	export function from(card: ModelCard): Card {
+	export function from(card: ModelCard, statistics: Statistics = initialStatistics): Card {
 		return {
 			...card,
-			used: { count: 0, amount: 0 },
-			reject: { count: 0 },
+			...statistics,
 			age: ageFromTime(card.created),
 			limit: card.limit[1], // TODO add currency conversion
 			original: { currency: card.limit[0], limit: card.limit[1] },
