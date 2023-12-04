578af4e688a55e072a3940fce677542809f76b8f
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2023-12-04 14:46:09 +0100

    Settlement clearing (#202)
    * Updated settlement for clearing.
    
    * Added year to Batch.
    
    * Settlement from.
    
    ---------
    
    Co-authored-by: Marcus Hanses <marcus.hanses@pax2pay.com>

--- Settlement/Creatable.ts
+++ Settlement/Creatable.ts
@@ -6,7 +6,7 @@ import { Total } from "./Total"
 export interface Creatable {
 	expected?: Total
 	processor: Card.Stack
-	reference: string
+	reference?: string //File name
 	batch: Batch
 }
 
@@ -14,7 +14,7 @@ export namespace Creatable {
 	export const type = isly.object<Creatable>({
 		expected: Total.type.optional(),
 		processor: Card.Stack.type,
-		reference: isly.string(),
+		reference: isly.string().optional(),
 		batch: Batch.type,
 	})
 	export const is = type.is
--- Settlement/index.ts
+++ Settlement/index.ts
@@ -1,7 +1,7 @@
-import { cryptly } from "cryptly"
 import { isoly } from "isoly"
 import { isly } from "isly"
 import { Card } from "../Card"
+import { Identifier } from "../Identifier"
 import { Batch as SettlementBatch } from "./Batch"
 import { Creatable as SettlementCreatable } from "./Creatable"
 import { Entry as SettlementEntry } from "./Entry"
@@ -14,16 +14,15 @@ export interface Settlement {
 	id: string
 	by?: string
 	created: isoly.DateTime
-	reference: string
+	reference?: string
 	batch: SettlementBatch
 	processor: Card.Stack
 	status: Status
-	expected: Settlement.Total
-	outcome?: Settlement.Total
+	expected?: Settlement.Total
+	outcome: Settlement.Total
 	settled?: Settled
-	entries?: Settlement.Entry.Summary
+	entries: Settlement.Entry.Summary
 }
-
 export namespace Settlement {
 	export const Total = SettlementTotal
 	export type Total = SettlementTotal
@@ -43,8 +42,29 @@ export namespace Settlement {
 		export type Capture = SettlementEntry.Capture
 		export type Unknown = SettlementEntry.Unknown
 	}
-	export function from(id: cryptly.Identifier, creatable: Settlement.Creatable, by: string): Settlement {
-		return { id, status: "ongoing", by, expected: Total.initiate(), ...creatable, created: isoly.DateTime.now() }
+	export function initiate(id: string, by: string, batch: Batch, processor: Card.Stack): Settlement {
+		return {
+			id: id ?? Identifier.generate(),
+			by,
+			created: isoly.DateTime.now(),
+			batch,
+			processor,
+			status: "ongoing",
+			expected: Total.initiate(),
+			outcome: Total.initiate(),
+			entries: { count: 0 },
+		}
+	}
+	export function from(id: string, creatable: Settlement.Creatable, by: string): Settlement {
+		return {
+			id,
+			status: "ongoing",
+			by,
+			outcome: Total.initiate(),
+			...creatable,
+			created: isoly.DateTime.now(),
+			entries: { count: 0 },
+		}
 	}
 	export function compile(settlement: Settlement, entries: Settlement.Entry[]): Settlement {
 		return entries.reduce(
@@ -65,20 +85,22 @@ export namespace Settlement {
 			reference: creatable.reference,
 			batch: creatable.batch,
 			expected: Total.initiate(),
+			outcome: Total.initiate(),
+			entries: { count: 0 },
 		}
 	}
 	export const type = isly.object<Settlement>({
 		id: isly.string(),
 		by: isly.string().optional(),
 		created: isly.fromIs("isoly.DateTime", isoly.DateTime.is),
-		reference: isly.string(),
+		reference: isly.string().optional(),
 		processor: Card.Stack.type,
 		batch: Batch.type,
 		status: Status.type,
-		expected: Settlement.Total.type,
-		outcome: Settlement.Total.type.optional(),
+		expected: Settlement.Total.type.optional(),
+		outcome: Settlement.Total.type,
 		settled: Settled.type.optional(),
-		entries: Settlement.Entry.Summary.type.optional(),
+		entries: Settlement.Entry.Summary.type,
 	})
 	export const is = type.is
 	export const flaw = type.flaw
