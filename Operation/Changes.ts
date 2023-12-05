b5b321de2e07d6bbb3f152e9ae677519b90ff7dd
Anton Kroner <anton.kroner@pax2pay.com>
2023-12-05 16:36:25 +0100

    Fixed Operation.Changes type.

--- Operation/Changes.ts
+++ Operation/Changes.ts
@@ -3,10 +3,10 @@ import { Balances } from "../Balances"
 import { Counterbalances } from "../CounterBalances"
 import { Change as Change } from "./Change"
 
-export type Changes = Partial<Record<Changes.Entry, Change>>
+export type Changes = Partial<Record<Changes.Entry, Change>> & Record<Counterbalances.Counterbalance.Entry, Change>
 
 export namespace Changes {
-	export type Entry = Balances.Balance.Entry | Counterbalances.Counterbalance.Entry
+	export type Entry = Balances.Balance.Entry
 	export namespace Entry {
 		export const values = [...Balances.Balance.Entry.values, ...Counterbalances.Counterbalance.Entry.values]
 		export const type = isly.string(values)
