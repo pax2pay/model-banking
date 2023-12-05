58830c95d41fe3dbe590620194f3b6e2ffb516fc
Anton Kroner <anton.kroner@pax2pay.com>
2023-12-05 16:15:22 +0100

    Counterbalances entires imports batch.

--- CounterBalances/SettlementEntry.ts
+++
@@ -1,8 +0,0 @@
-import { isly } from "isly"
-
-export type SettlementEntry = string
-
-export namespace SettlementEntry {
-	export const type = isly.string(/^(?:settlement|fee)-\w+-\d{5}$/)
-	export const is = type.is
-}
--- Settlement/Batch.ts
+++ Settlement/Batch.ts
@@ -2,6 +2,7 @@ import { isly } from "isly"
 
 export type Batch = string
 export namespace Batch {
-	export const type = isly.string<Batch>(/^20\d\d(?:[0-2]\d\d|3[0-6]\d)0[1-6]$/)
+	export const regexp = new RegExp(/20\d\d(?:[0-2]\d\d|3[0-6]\d)0[1-6]$/)
+	export const type = isly.string<Batch>(regexp)
 	export const is = type.is
 }
