3f4cc4240e61e114c51de11a0cd1099a31900b54
Nils Hansander <nilshansander@hotmail.com>
2023-12-11 11:25:45 +0100

    Settlement totals compare function (#213)
    * Settlement totals compare function
    
    * Added test for Settlement total compare function

--- Settlement/Total.ts
+++ Settlement/Total.ts
@@ -1,3 +1,4 @@
+import { isoly } from "isoly"
 import { isly } from "isly"
 import { Amounts } from "../Amounts"
 import { Fee } from "./Fee"
@@ -13,6 +14,16 @@ export namespace Total {
 	}
 	export function add(addendee: Total, addend: Total): Total {
 		return { amount: Amounts.add(addendee.amount, addend.amount), fee: Fee.add(addendee.fee, addend.fee) }
+	}
+	export function compare(expected: Total, outcome: Total): boolean {
+		return (
+			([...new Set([...Object.keys(expected.amount), ...Object.keys(outcome.amount)])] as isoly.Currency[]).every(
+				currency => expected.amount[currency] == outcome.amount[currency]
+			) &&
+			([...new Set([...Object.keys(expected.fee.other), ...Object.keys(outcome.fee.other)])] as isoly.Currency[]).every(
+				currency => expected.fee.other[currency] == outcome.fee.other[currency]
+			)
+		)
 	}
 	export const type = isly.object<Total>({ amount: Amounts.type, fee: Fee.type })
 	export const is = type.is
