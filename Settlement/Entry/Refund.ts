c14e8c34b9ca11f38ae659c139a0afba7c360bfb
Anton Kroner <anton.kroner@pax2pay.com>
2023-12-11 13:25:36 +0100

    Reworked Refund.Creatable & Entry.from.

--- Settlement/Entry/Refund.ts
+++ Settlement/Entry/Refund.ts
@@ -1,30 +1,42 @@
 import { isly } from "isly"
+import { Acquirer } from "../../Acquirer"
 import { Amount } from "../../Amount"
-import { Authorization } from "../../Authorization"
+import { Merchant } from "../../Merchant"
+import { Transaction } from "../../Transaction"
 import { Batch } from "../Batch"
 import { Fee } from "../Fee"
 
 export interface Refund extends Refund.Creatable {
 	status: "succeeded" | "failed"
 	reason?: string
+	transaction?: string
 }
 
 export namespace Refund {
 	export interface Creatable {
 		type: "refund"
-		authorization?: Authorization
+		card: string
+		authorization?: string
+		merchant: Merchant
+		acquirer: Acquirer
 		reference: string
 		batch: Batch
-		fee?: Fee
-		amount?: Amount
+		fee: Fee
+		amount: Amount
+	}
+	export function from(refund: Refund.Creatable, transaction: Transaction): Refund {
+		return { ...refund, status: "succeeded", transaction: transaction.id }
 	}
 	export namespace Creatable {
 		export const type = isly.object<Creatable>({
 			type: isly.string("refund"),
-			authorization: Authorization.type.optional(),
+			card: isly.string(),
+			authorization: isly.string().optional(),
+			merchant: Merchant.type,
+			acquirer: Acquirer.type,
 			reference: isly.string(),
-			fee: Fee.type.optional(),
-			amount: Amount.type.optional(),
+			fee: Fee.type,
+			amount: Amount.type,
 			batch: Batch.type,
 		})
 		export const is = type.is
@@ -33,6 +45,7 @@ export namespace Refund {
 	export const type = Creatable.type.extend<Refund>({
 		status: isly.string(["succeeded", "failed"]),
 		reason: isly.string().optional(),
+		transaction: isly.string().optional(),
 	})
 	export const is = type.is
 	export const flaw = type.flaw
