80658efcf51dbb09d553fb0f01f198f7ff1d72b1
Emilia By <emilia.by@pax2pay.com>
2023-12-19 10:02:25 +0100

    Rewrote Organization.Contact test.

--- Organization/Contact/Contact.spec.ts
+++ Organization/Contact/Contact.spec.ts
@@ -1,42 +1,18 @@
 import { pax2pay } from "../../index"
 
-describe("Address", () => {
-	it("countryCode determines type", () => {
+describe("Organization.Contact", () => {
+	it("is to succeed", () => {
 		expect(pax2pay.Organization.Contact.is(contact)).toBeTruthy()
-		expect(pax2pay.Organization.Contact.is(mistyped1)).toBeFalsy()
-		expect(pax2pay.Organization.Contact.is(mistyped2)).toBeFalsy()
+	})
+	it("wrong phone number", () => {
+		const mistyped = { ...contact, phone: { ...contact.phone, number: "12m45667" } }
+		expect(pax2pay.Organization.Contact.is(mistyped)).toBeFalsy()
+	})
+	it("wrong email", () => {
+		const mistyped = { ...contact, email: "this.is.not.an.email" }
+		expect(pax2pay.Organization.Contact.is(mistyped)).toBeFalsy()
 	})
 })
-
-const mistyped1 = {
-	name: { first: "Test", last: "Testsson" },
-	email: "@test.com",
-	phone: { code: "+46", number: "1245667" },
-	address: {
-		primary: {
-			countryCode: "GB",
-			building: "asdf",
-			city: "qwer",
-			street: "zxcv",
-			zipCode: "11235",
-		},
-	},
-}
-
-const mistyped2 = {
-	name: { first: "Test", last: "Testsson" },
-	email: "test@test",
-	phone: { code: "+46", number: "12m45667" },
-	address: {
-		primary: {
-			countryCode: "GB",
-			building: "asdf",
-			city: "qwer",
-			street: "zxcv",
-			zipCode: "11235",
-		},
-	},
-}
 
 const contact = {
 	name: { first: "Test", last: "Testsson" },
