b03816485b9e36ac53683d897739bcd3d9badb48
marcushanses <79835961+marcushanses@users.noreply.github.com>
2023-12-19 18:41:37 +0100

    Added test preset. (#226)

--- Card/Preset.ts
+++ Card/Preset.ts
@@ -5,13 +5,21 @@ export type Preset = typeof Preset.names[number]
 
 // "realm-scheme-interchange"
 export namespace Preset {
-	export const names = ["p2p-mc-200", "test-mc-200", "test-pg-200", "test-ta-pg-200", "test-ta-mc-200"] as const
+	export const names = [
+		"p2p-mc-200",
+		"test-mc-200",
+		"test-ta-mq-200",
+		"test-pg-200",
+		"test-ta-pg-200",
+		"test-ta-mc-200",
+	] as const
 	export const type = isly.string(names)
 	export const is = type.is
 	export const flaw = type.flaw
 	export const presets: Record<Preset, Stack> = {
 		"p2p-mc-200": "uk-mc-tpl-marqeta",
 		"test-mc-200": "testUK-marqeta",
+		"test-ta-mq-200": "testUK-tpl-marqeta",
 		"test-pg-200": "test-paxgiro",
 		"test-ta-mc-200": "test-tpl-paxgiro",
 		"test-ta-pg-200": "test-tpl-paxgiro",
