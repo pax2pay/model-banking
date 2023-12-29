e380bc243bc38a37ec938665ebd47c237b2db62e
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2023-12-29 09:19:43 +0100

    Added check ensure Organization.code only contains valid url characters. (#233)
    * Added check ensure Organization.code only contains valid url characters.
    
    * Removed accidental ().

---
+++ Organization/code.spec.ts
@@ -0,0 +1,12 @@
+import { isly } from "isly"
+
+describe("Organization.Code", () => {
+	it("contains only valid url characters", () => {
+		const Code = isly.string(new RegExp(/^[A-Za-z0-9\-_]+$/))
+		expect(Code.is("aaaa-bbbb_")).toBeTruthy()
+		expect(Code.is("aaaa-bbbb ")).toBeFalsy()
+		expect(Code.is("aa aa")).toBeFalsy()
+		expect(Code.is("aa&aa")).toBeFalsy()
+		expect(Code.is("aa|aa")).toBeFalsy()
+	})
+})
