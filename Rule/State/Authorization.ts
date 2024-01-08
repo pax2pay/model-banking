933e7429a8707c35534f7ccf0d889284d93e388e
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2024-01-08 15:34:46 +0100

    Fixed circular dependency. (#236)

--- Rule/State/Authorization.ts
+++ Rule/State/Authorization.ts
@@ -1,15 +1,15 @@
 import { isoly } from "isoly"
 import { isly } from "isly"
-import { Authorization as ModelAuthorization } from "../../Authorization"
+import { Creatable as AuthorizationCreatable } from "../../Authorization/Creatable"
 
-export interface Authorization extends Omit<ModelAuthorization.Creatable, "amount"> {
+export interface Authorization extends Omit<AuthorizationCreatable, "amount"> {
 	time: string
 	hour: number
 	currency: isoly.Currency
 	amount: number
 }
 export namespace Authorization {
-	export function from(authorization: ModelAuthorization.Creatable): Authorization {
+	export function from(authorization: AuthorizationCreatable): Authorization {
 		return {
 			...authorization,
 			time: isoly.DateTime.getTime(isoly.DateTime.now()),
@@ -18,7 +18,7 @@ export namespace Authorization {
 			amount: Math.abs(authorization.amount[1]),
 		}
 	}
-	export const type = ModelAuthorization.Creatable.type.omit(["amount"]).extend<Authorization>({
+	export const type = AuthorizationCreatable.type.omit(["amount"]).extend<Authorization>({
 		time: isly.string(),
 		hour: isly.number(),
 		currency: isly.string(isoly.Currency.types),
