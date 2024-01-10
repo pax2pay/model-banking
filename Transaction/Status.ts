50cb0a8df4176c2467daef9969de68b547e8de3a
Nils Hansander <nilshansander@hotmail.com>
2024-01-10 10:19:12 +0100

    Add cancelled to transaction status (#237)

--- Transaction/Status.ts
+++ Transaction/Status.ts
@@ -2,7 +2,7 @@ import { isly } from "isly"
 
 export type Status = typeof Status.values[number]
 export namespace Status {
-	export const values = ["created", "approved", "rejected", "processing", "finalized"] as const
+	export const values = ["created", "approved", "rejected", "processing", "finalized", "cancelled"] as const
 	export const type = isly.string(values)
 	export const is = type.is
 	export const flaw = type.flaw
