1a63d8c49b41df631b27f85d447128bd23d948f4
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-01-29 15:50:29 +0100

    Changed flag to label and added types.

--- Client/Flags.ts
+++ Client/Flags.ts
@@ -1,19 +1,19 @@
 import { gracely } from "gracely"
 import { http } from "cloudly-http"
-import { Flag } from "../Flag"
+import { Label } from "../Label"
 
 export class Flags {
 	constructor(private readonly client: http.Client) {}
-	async create(flag: Flag): Promise<Flag | gracely.Error> {
-		return this.client.post<Flag>("/flag", flag)
+	async create(flag: Label): Promise<Label | gracely.Error> {
+		return this.client.post<Label>("/flag", flag)
 	}
-	async replace(flag: Flag): Promise<Flag | gracely.Error> {
-		return this.client.put<Flag>("/flag", flag)
+	async replace(flag: Label): Promise<Label | gracely.Error> {
+		return this.client.put<Label>("/flag", flag)
 	}
-	async list(): Promise<Flag[] | gracely.Error> {
-		return this.client.get<Flag[]>("/flag")
+	async list(): Promise<Label[] | gracely.Error> {
+		return this.client.get<Label[]>("/flag")
 	}
-	async remove(name: string): Promise<Flag | gracely.Error> {
+	async remove(name: string): Promise<Label | gracely.Error> {
 		return this.client.delete(`/flag/${name}`)
 	}
 }
---
+++ Label.ts
@@ -0,0 +1,19 @@
+import { isly } from "isly"
+
+export interface Label {
+	name: string
+	color: string
+	description: string
+}
+export namespace Label {
+	export const type = isly.object<Label>({ name: isly.string(), color: isly.string(), description: isly.string() })
+	export const is = type.is
+	export const flaw = type.flaw
+	export type Type = typeof Type.values[number]
+	export namespace Type {
+		export const values = ["flag", "group"] as const
+		export const type = isly.string(values)
+		export const is = type.is
+		export const flaw = type.flaw
+	}
+}
