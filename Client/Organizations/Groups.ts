4715d7218900a2256fa0564e9796f7a9273352a8
Anton Kroner <anton.kroner@pax2pay.com>
2024-01-30 11:01:08 +0100

    Fixed group type.

--- Client/Organizations/Groups.ts
+++ Client/Organizations/Groups.ts
@@ -1,14 +1,13 @@
 import { gracely } from "gracely"
 import { http } from "cloudly-http"
 import * as rest from "cloudly-rest"
-import { Label } from "../../Label"
 
 export class Groups extends rest.Collection<gracely.Error> {
 	constructor(client: http.Client) {
 		super(client)
 	}
-	async replace(organization: string, groups: Label[]): Promise<Label[] | gracely.Error> {
-		return this.client.put<Label[]>(`/organization/label`, groups, {
+	async replace(organization: string, groups: string[]): Promise<string[] | gracely.Error> {
+		return this.client.put<string[]>(`/organization/group`, groups, {
 			organization,
 		})
 	}
