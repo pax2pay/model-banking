88c927f5f9d2ae879713fddcebbb21bf517e88e8
SiFalorSum <nilshansander@hotmail.com>
2024-01-30 16:16:34 +0100

    Add groups to Organization.Changeable

--- Organization/Changeable.ts
+++ Organization/Changeable.ts
@@ -4,12 +4,14 @@ import { Contact } from "./Contact"
 export interface Changeable {
 	name?: string
 	contact?: Contact
+	groups?: string[]
 }
 
 export namespace Changeable {
 	export const type = isly.object<Changeable>({
 		name: isly.string().optional(),
 		contact: Contact.type.optional(),
+		groups: isly.string().array().optional(),
 	})
 	export const is = type.is
 	export const flaw = type.flaw
