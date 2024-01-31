bca4e32543981efea2fdd476a22a44f53f87a91d
Anton Kroner <anton.kroner@pax2pay.com>
2024-01-31 17:43:47 +0100

    Fixed PUT org/group return type.

--- Client/Organizations/Groups.ts
+++ Client/Organizations/Groups.ts
@@ -1,13 +1,14 @@
 import { gracely } from "gracely"
 import { http } from "cloudly-http"
 import * as rest from "cloudly-rest"
+import { Organization } from "../../Organization"
 
 export class Groups extends rest.Collection<gracely.Error> {
 	constructor(client: http.Client) {
 		super(client)
 	}
-	async replace(organization: string, groups: string[]): Promise<string[] | gracely.Error> {
-		return this.client.put<string[]>(`/organization/group`, groups, {
+	async replace(organization: string, groups: string[]): Promise<Organization | gracely.Error> {
+		return this.client.put<Organization>(`/organization/group`, groups, {
 			organization,
 		})
 	}
