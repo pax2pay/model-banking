3d40547122e8f7920243293af2b46216f7ae5091
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-02-12 16:54:43 +0100

    Added properties to internal rail.

--- Rail/Address/internal.ts
+++ Rail/Address/internal.ts
@@ -1,12 +1,20 @@
 import * as cryptly from "cryptly"
+import { isly } from "isly"
 
 export interface Internal {
 	type: "internal"
+	name?: string
 	identifier: cryptly.Identifier
+	organization?: cryptly.Identifier
 }
 export namespace Internal {
 	export const currencies = ["EUR", "GBP", "SEK", "USD"] as const
-	export function is(value: Internal | any): value is Internal {
-		return typeof value == "object" && cryptly.Identifier.is(value.identifier, 8) && value.type == "internal"
-	}
+	export const type = isly.object<Internal>({
+		type: isly.string("internal"),
+		name: isly.string().optional(),
+		identifier: isly.fromIs("Identifier", cryptly.Identifier.is),
+		organization: isly.fromIs("Identifier", cryptly.Identifier.is).optional(),
+	})
+	export const is = type.is
+	export const flaw = type.flaw
 }
