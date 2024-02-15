3abc759cbfa76becd4cc41d9aa869ee31a8c3d58
Anton Kroner <anton.kroner@pax2pay.com>
2024-02-15 11:44:12 +0100

    Remove key null coalescing.

--- Operation/Signer.ts
+++ Operation/Signer.ts
@@ -2,14 +2,14 @@ import { cryptly } from "cryptly"
 import type { Operation } from "./index"
 
 export class Signer {
-	private readonly signer = cryptly.Signer.create("RSA", "SHA-256", this.keys.public ?? "", this.keys.private ?? "")
+	private readonly signer = cryptly.Signer.create("RSA", "SHA-256", this.keys.public, this.keys.private)
 
 	private constructor(private readonly keys: { public?: string; private?: string }) {}
 	async sign(operation: Operation & { previous: string }): Promise<string> {
 		return this.signer.sign(JSON.stringify(operation))
 	}
 	async verify(operation: Operation): Promise<boolean> {
-		return await this.signer.verify(JSON.stringify({ ...operation, signature: undefined }), operation.signature ?? "")
+		return this.signer.verify(JSON.stringify({ ...operation, signature: undefined }), operation.signature ?? "")
 	}
 
 	static open(keys: { public?: string; private?: string }): Signer {
