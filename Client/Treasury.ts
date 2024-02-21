235748e216bd8310bc3c4453ed79948ab0e6db4d
Emilia By <emilia.by@pax2pay.com>
2024-02-21 09:47:28 +0100

    Fixed fetch treasury path.

--- Client/Treasury.ts
+++ Client/Treasury.ts
@@ -15,7 +15,7 @@ export class Treasury extends rest.Collection<gracely.Error> {
 	}
 	async fetch(hour?: isoly.DateTime): Promise<TreasuryModel.Snapshot | gracely.Error> {
 		const path = hour ? `/${isoly.DateTime.truncate(hour, "hours")}` : ""
-		return this.client.get<TreasuryModel.Snapshot>(`/treasury/snapshot/${path}`)
+		return this.client.get<TreasuryModel.Snapshot>(`/treasury/snapshot${path}`)
 	}
 	async listTransactions(accountId: string): Promise<TreasuryModel.Transaction[] | gracely.Error> {
 		return this.client.get<TreasuryModel.Transaction[]>(`/treasury/account/${accountId}/transaction`)
