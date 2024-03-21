8f7b7890b8b693ec8f6520e12aa3c087b2c0a8a7
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-03-21 16:42:38 +0100

    Simplify operation counterbalance.

--- Operation/Changes.ts
+++ Operation/Changes.ts
@@ -6,21 +6,11 @@ import { Counterbalances } from "../CounterBalances"
 import { Change } from "./Change"
 
 // TODO: remove old counterbalances
-export type Changes = Partial<Record<Balances.Balance.Entry, Change>> &
-	Record<Counterbalances.Counterbalance.Entry.Settlement | Changes.Entry.Counterbalance, Change>
+export type Changes = Partial<Record<Balances.Balance.Entry, Change>> & Record<Changes.Entry.Counterbalance, Change>
 
 export namespace Changes {
 	export namespace Entry {
 		export type Counterbalance = `${Counterbalance2.Link}-${isoly.DateTime}`
-		export const Counterbalance = isly.fromIs<Counterbalance>(
-			"Changes.Entry.Counterbalance",
-			(value: any | Counterbalance) => {
-				const result = !value ? false : typeof value == "string" && value.split("-")
-				return (
-					result && Counterbalance2.Link.is(`${result[0]}-${result[1]}`) && isoly.DateTime.is(result.slice(2).join("-"))
-				)
-			}
-		)
 		export function split(counterbalance: Counterbalance): [Counterbalance2.Link, isoly.DateTime] {
 			const split = counterbalance.split("-")
 			const [supplier, account, hour] = [split[0], split[1], split.slice(2).join("-")]
@@ -28,14 +18,7 @@ export namespace Changes {
 		}
 	}
 	export type Entry = Balances.Balance.Entry | Entry.Counterbalance | Counterbalances.Counterbalance.Entry.Settlement
-	export const type = isly.record<Changes>(
-		isly.union<Entry, Balances.Balance.Entry, Entry.Counterbalance, Counterbalances.Counterbalance.Entry>(
-			Balances.Balance.Entry.type,
-			Entry.Counterbalance,
-			Counterbalances.Counterbalance.Entry.type
-		),
-		Change.type
-	)
+	export const type = isly.record<Changes>(isly.string(), Change.type)
 	export const is = type.is
 	export const flaw = type.flaw
 }
