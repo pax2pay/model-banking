d231013a52eb9313ec6988ca47c100c24a8fb58c
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2024-05-13 15:16:43 +0200

    Added paxgiro-credit to suppliers. (#314)

--- Realm.ts
+++ Realm.ts
@@ -20,14 +20,14 @@ export namespace Realm {
 		eu: "EUR",
 	}
 	export const suppliers: Record<Realm, modelSupplier[]> = {
-		test: ["paxgiro"],
+		test: ["paxgiro", "paxgiro-credit"],
 		testUK: ["clearbank"],
 		uk: ["clearbank"],
 		eu: [],
 		upcheck: ["paxgiro"],
 	}
 	export interface Suppliers extends Record<Realm, modelSupplier[]> {
-		test: ["paxgiro"]
+		test: ["paxgiro", "paxgiro-credit"]
 		testUK: ["clearbank"]
 		uk: ["clearbank"]
 		eu: []
