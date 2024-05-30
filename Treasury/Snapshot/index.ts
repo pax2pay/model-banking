911b8aaae9e47053d923b8295c3404e487e958bc
SiFalorSum <nilshansander@hotmail.com>
2024-05-30 14:35:46 +0200

    reworked funding snapshot

--- Treasury/Snapshot/index.ts
+++ Treasury/Snapshot/index.ts
@@ -1,8 +1,8 @@
 import { isoly } from "isoly"
 import { isly } from "isly"
-import { Credit as SnapshotCredit } from "./Credit"
 import { Emoney as SnapshotEmoney } from "./Emoney"
 import { Fragment as SnapshotFragment } from "./Fragment"
+import { funding as snapshotFunding } from "./funding"
 import { Warning as SnapshotWarning } from "./Warning"
 
 export type Snapshot = Partial<Record<isoly.Currency, Snapshot.Fragment>>
@@ -10,7 +10,7 @@ export type Snapshot = Partial<Record<isoly.Currency, Snapshot.Fragment>>
 export namespace Snapshot {
 	export import Fragment = SnapshotFragment
 	export import Warning = SnapshotWarning
-	export import Credit = SnapshotCredit
+	export import funding = snapshotFunding
 	export type Emoney = SnapshotEmoney
 	export const type = isly.record(isly.fromIs("Currency", isoly.Currency.is), Fragment.type)
 }
