f2967a25fe1a1a003dc6d1b54b279fda7554441a
Anton Kroner <anton.kroner@pax2pay.com>
2024-05-31 11:56:34 +0200

    Name suggestions for authorization jit funding.

--- Rail/Address/PaxgiroCredit.ts
+++ Rail/Address/PaxgiroCredit.ts
@@ -3,6 +3,9 @@ import { isly } from "isly"
 export interface PaxgiroCredit {
 	type: "paxgiro-credit"
 	reference: string
+	onAuthorization?: boolean
+	immediate?: boolean
+	justInTime?: boolean
 }
 export namespace PaxgiroCredit {
 	export const type = isly.object<PaxgiroCredit>({ type: isly.string("paxgiro-credit"), reference: isly.string() })
