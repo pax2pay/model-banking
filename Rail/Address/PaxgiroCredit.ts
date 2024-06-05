55478988c5130d4eafbd7aa4c8ab6bd9063a5dc7
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2024-06-05 11:26:23 +0200

    Name suggestions for authorization jit funding. (#325)
    * Name suggestions for authorization jit funding.
    
    * decided for justInTime
    
    ---------
    
    Co-authored-by: SiFalorSum <nilshansander@hotmail.com>

--- Rail/Address/PaxgiroCredit.ts
+++ Rail/Address/PaxgiroCredit.ts
@@ -3,7 +3,12 @@ import { isly } from "isly"
 export interface PaxgiroCredit {
 	type: "paxgiro-credit"
 	reference: string
+	justInTime?: boolean
 }
 export namespace PaxgiroCredit {
-	export const type = isly.object<PaxgiroCredit>({ type: isly.string("paxgiro-credit"), reference: isly.string() })
+	export const type = isly.object<PaxgiroCredit>({
+		type: isly.string("paxgiro-credit"),
+		reference: isly.string(),
+		justInTime: isly.boolean().optional(),
+	})
 }
