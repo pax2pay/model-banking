35078923f5bc34ee052a686a335374b39ac9009d
Tove Brandt <tove.brandt@pax2pay.com>
2024-06-20 10:46:34 +0200

    Fix for finding configuration message

--- Log/Message/index.ts
+++ Log/Message/index.ts
@@ -11,12 +11,12 @@ export namespace Message {
 		traces: TraceLog[]
 	): Pick<Log, "realm" | "collection" | "resource" | "entries"> | undefined {
 		const configuration: Pick<Log, "realm" | "collection" | "resource"> | undefined =
-			Message.Configuration.fromTraceLog(traces.slice(0, 1)[0])
+			Message.Configuration.fromTraceLog(traces.find(trace => Message.Configuration.type.is(trace.message[0])))
 		const result: Pick<Log, "realm" | "collection" | "resource" | "entries"> | undefined = configuration
 			? { ...configuration, entries: [] }
 			: undefined
 		if (result)
-			for (const trace of traces.slice(1)) {
+			for (const trace of traces) {
 				const logFragment: { entry: Log.Entry; resource: Log["resource"] } | undefined =
 					Message.Entry.fromEventLogs(trace)
 				logFragment?.resource && (result.resource ??= logFragment.resource)
