71321db575100de1912001d0652af933d364e932
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2024-06-26 16:50:16 +0200

    Added more acquirer fields. (#334)

--- Acquirer/index.ts
+++ Acquirer/index.ts
@@ -5,14 +5,15 @@ export interface Acquirer {
 	id: string
 	number: string
 	country?: isoly.CountryCode.Alpha2
+	retrievalReferenceNumber?: string
+	systemTraceAuditNumber?: string
 }
-
 export namespace Acquirer {
 	export const type = isly.object<Acquirer>({
 		id: isly.string(),
 		number: isly.string(),
 		country: isly.fromIs("Acquirer.country", isoly.CountryCode.Alpha2.is).optional(),
+		retrievalReferenceNumber: isly.string().optional(),
+		systemTraceAuditNumber: isly.string().optional(),
 	})
-	export const is = type.is
-	export const flaw = type.flaw
 }
