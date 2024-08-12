6277bc0adb616dc4839aea5369cf48ea7d83e14a
Anton Kroner <anton.kroner@pax2pay.com>
2024-08-12 16:39:12 +0200

    Disabled analytics configuration.

--- Analytics/Configuration/Schema.ts
+++
@@ -1,7 +0,0 @@
-import { Listener } from "cloudly-analytics-administration"
-
-export type Schema<T extends string = string> = Schema.Field<T>[]
-export namespace Schema {
-	export import Field = Listener.Configuration.BigQuery.Api.BaseField
-	export const type = Field.type.array()
-}
