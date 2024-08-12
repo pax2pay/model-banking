0f753069e8c39f43aa1723e6e3d8f79f01555ced
marcushanses <79835961+marcushanses@users.noreply.github.com>
2024-08-12 15:37:12 +0200

    Created settlement identifier. (#357)
    * Created settlement identifier.
    
    * Requested changes.
    
    * Requested changes.

---
+++ Settlement/Identifier.ts
@@ -0,0 +1,17 @@
+import { isoly } from "isoly"
+import { isly } from "isly"
+import { Card } from "../Card"
+
+//(date without dashed)(stack.Character)(single digit number representing batch)
+//20200102a3
+export type Identifier = `${isoly.Date}${string}`
+export namespace Identifier {
+	export const type = isly.string()
+	export function create(date: isoly.Date, stack: Card.Stack, order: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9): Identifier {
+		return date.replace(/-/g, "") + Card.Stack.Character.from(stack) + order
+	}
+	export function getDate(identifier: Identifier): isoly.Date | undefined {
+		const date = identifier.substring(0, 4) + "-" + identifier.substring(4, 6) + "-" + identifier.substring(6, 8)
+		return isoly.Date.is(date) ? date : undefined
+	}
+}
