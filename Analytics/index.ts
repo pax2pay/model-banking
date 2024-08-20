aa5a26e49c7c565f7c1324e41073eb29e6d62964
Nils Hansander <nilshansander@hotmail.com>
2024-08-20 17:42:26 +0200

    Reenable and change analytics configuration (#363)

--- Analytics/index.ts
+++ Analytics/index.ts
@@ -1,5 +1,7 @@
+import { Configuration as AnalyticsConfiguration } from "./Configuration"
 import { Event as AnalyticsEvent } from "./Event"
 
 export namespace Analytics {
 	export import Event = AnalyticsEvent
+	export import Configuration = AnalyticsConfiguration
 }
