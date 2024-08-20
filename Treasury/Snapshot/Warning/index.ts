4c98f0a23da540defe745438635a9e76f77da29d
Anton Kroner <anton.kroner@pax2pay.com>
2024-08-20 17:08:28 +0200

    Exported warning subtypes.

--- Treasury/Snapshot/Warning/index.ts
+++ Treasury/Snapshot/Warning/index.ts
@@ -1,9 +1,11 @@
 import { isly } from "isly"
-import { Overdraft } from "./Overdraft"
-import { Unguarded } from "./Unguarded"
+import { Overdraft as WarningOverdraft } from "./Overdraft"
+import { Unguarded as WarningUnguarded } from "./Unguarded"
 
-export type Warning = Overdraft | Unguarded
+export type Warning = Warning.Overdraft | Warning.Unguarded
 
 export namespace Warning {
+	export import Overdraft = WarningOverdraft
+	export import Unguarded = WarningUnguarded
 	export const type = isly.union<Warning, Overdraft, Unguarded>(Overdraft.type, Unguarded.type)
 }
