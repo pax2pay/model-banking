d3958637e675ae2bd8c0df0201f681280481ebf9
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2024-08-21 17:10:09 +0200

    Added create function for creating unguarded warnings. (#364)

--- Treasury/Snapshot/Warning/Unguarded.ts
+++ Treasury/Snapshot/Warning/Unguarded.ts
@@ -1,5 +1,8 @@
 import { isoly } from "isoly"
 import { isly } from "isly"
+import { Holidays } from "../../../Holidays"
+import { Account } from "../../Account"
+import { Transaction } from "../../Transaction"
 
 export interface Unguarded {
 	type: "unguarded"
@@ -14,4 +17,30 @@ export namespace Unguarded {
 		currency: isly.string(),
 		transaction: isly.object<Unguarded["transaction"]>({ id: isly.string(), created: isly.string() }),
 	})
+	export function create(account: Account, transactions: Transaction[]): Unguarded[] {
+		const result: Unguarded[] = []
+		for (const [currency, amount] of Object.entries(account.balance)) {
+			let oldest: { id: string; date: isoly.Date; created: isoly.DateTime } | undefined = undefined
+			let remainder = amount
+			for (const transaction of transactions) {
+				if (transaction.amount > 0 && transaction.currency == currency && remainder > 0) {
+					remainder = isoly.Currency.subtract(currency, remainder, transaction.amount)
+					oldest = {
+						id: transaction.id,
+						date: isoly.DateTime.getDate(transaction.created),
+						created: transaction.created,
+					}
+				} else if (remainder < 0)
+					break
+			}
+			if (oldest && isoly.Date.now() > isoly.Date.nextBusinessDay(oldest.date, 3, Holidays.dates["England"]))
+				result.push({
+					type: "unguarded",
+					currency: currency as isoly.Currency,
+					date: oldest.date,
+					transaction: { id: oldest.id, created: oldest.created },
+				})
+		}
+		return result
+	}
 }
