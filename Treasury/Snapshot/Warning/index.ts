26630d9ed015d3802788f6ff27059493c58b3291
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2024-08-22 13:17:05 +0200

    Added buffer warnings. (#365)
    * Added buffer warnings.
    
    * Added type to buffer warning.
    
    * Added omit dev dependencies flag to audit job.

--- Treasury/Snapshot/Warning/index.ts
+++ Treasury/Snapshot/Warning/index.ts
@@ -1,11 +1,13 @@
 import { isly } from "isly"
+import { Buffer as WarningBuffer } from "./Buffer"
 import { Overdraft as WarningOverdraft } from "./Overdraft"
 import { Unguarded as WarningUnguarded } from "./Unguarded"
 
-export type Warning = Warning.Overdraft | Warning.Unguarded
+export type Warning = Warning.Overdraft | Warning.Unguarded | Warning.Buffer
 
 export namespace Warning {
 	export import Overdraft = WarningOverdraft
 	export import Unguarded = WarningUnguarded
-	export const type = isly.union<Warning, Overdraft, Unguarded>(Overdraft.type, Unguarded.type)
+	export import Buffer = WarningBuffer
+	export const type = isly.union<Warning, Overdraft, Unguarded, Buffer>(Overdraft.type, Unguarded.type, Buffer.type)
 }
