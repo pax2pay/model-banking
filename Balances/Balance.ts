43fc2dad4b67b12c5307cd5fef5f667280e2eb7e
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-08-22 15:45:26 +0200

    Added buffer to balance.

--- Balances/Balance.ts
+++ Balances/Balance.ts
@@ -6,7 +6,7 @@ export type Balance = Partial<Record<Balance.Entry, number>>
 export namespace Balance {
 	export type Entry = typeof Entry.values[number]
 	export namespace Entry {
-		export const values = ["actual", "incomingReserved", "outgoingReserved"] as const
+		export const values = ["actual", "incomingReserved", "outgoingReserved", "buffer"] as const
 		export const type = isly.string(values)
 		export const is = type.is
 		export const flaw = type.flaw
