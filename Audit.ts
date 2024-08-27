572dc9722ff0d0122bafc15339a72451901e1916
marcushanses <79835961+marcushanses@users.noreply.github.com>
2024-08-27 16:53:18 +0200

    Add user to audit types. (#370)

--- Audit.ts
+++ Audit.ts
@@ -2,6 +2,6 @@ import { storage } from "cloudly-storage"
 
 export type Audit = storage.AuditLogger.Entry<Audit.Type>
 export namespace Audit {
-	export type Type = { rule: "change" | "add" }
+	export type Type = { rule: "change" | "add"; user: "login" | "change" }
 	export type Resource = Extract<keyof Type, string>
 }
