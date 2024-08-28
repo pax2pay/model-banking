d311bb602bab163f689b192b1cb003bd8393f914
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-08-28 14:02:46 +0200

    Fixed audit log client.

--- Client/Audit.ts
+++ Client/Audit.ts
@@ -5,6 +5,6 @@ import { Audit as AuditLog } from "../Audit"
 export class Audit {
 	constructor(private readonly client: http.Client) {}
 	async list(resource?: AuditLog.Resource): Promise<AuditLog[] | gracely.Error> {
-		return this.client.get<AuditLog[]>(`/audit/${resource}`)
+		return this.client.get<AuditLog[]>(`/audit${resource ? "/" + resource : ""}`)
 	}
 }
