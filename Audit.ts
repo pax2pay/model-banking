63ccb333ca2f530db233b8524fbbed07cf1e7874
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-08-28 11:33:50 +0200

    Improved audit type.

--- Audit.ts
+++ Audit.ts
@@ -1,7 +1,12 @@
 import { storage } from "cloudly-storage"
+import { isly } from "isly"
 
 export type Audit = storage.AuditLogger.Entry<Audit.Type>
 export namespace Audit {
 	export type Type = { rule: "change" | "add"; user: "login" | "change" }
-	export type Resource = Extract<keyof Type, string>
+	export type Resource = typeof Resource.values[number]
+	export namespace Resource {
+		export const values = ["rule", "user"] as const
+		export const type = isly.string(values)
+	}
 }
