5661590304d709f6e6d065c715bcc6d39fcf9809
SiFalorSum <nilshansander@hotmail.com>
2024-08-30 15:48:56 +0200

    Added audit log to analytics models

--- Analytics/Configuration/index.ts
+++ Analytics/Configuration/index.ts
@@ -1,79 +1,11 @@
-import { filter, listener } from "cloudly-analytics-common"
+import { AuditLog as ConfigAuditLog } from "./AuditLog"
 import { Base as ConfigBase } from "./Base"
 import { Operation as ConfigOperation } from "./Operation"
 import { Transaction as ConfigTransaction } from "./Transaction"
 
-//cSpell: ignore: paxpay nonprod
 export namespace Configuration {
 	export import Base = ConfigBase
 	export import Transaction = ConfigTransaction
 	export import Operation = ConfigOperation
-	export type BigQueryTableConfig = {
-		projectName: "paxpay-prod" | "paxpay-nonprod"
-		datasetName: string
-		tableName: string
-	}
-	export function create(
-		name: string,
-		mapping: filter.Mapping.RecordWithSelector<string>,
-		schema: listener.BigQueryApi.BaseField[],
-		config: BigQueryTableConfig,
-		filter: string
-	): listener.BigQueryBaseConfiguration {
-		return {
-			name,
-			type: "bigquery",
-			filter: [
-				{
-					type: "selectively",
-					expression: filter,
-				},
-				{ type: "useragent" },
-				{
-					type: "mapping",
-					mapping: {
-						...Base.mapping,
-						...mapping,
-					},
-				},
-			],
-			batchSize: 10,
-			batchInterval: 3,
-			...config,
-			tableSchema: [...Base.schema, ...schema],
-		}
-	}
-	export const backup = create(
-		"backup",
-		{ value: { selector: "value", transform: "stringify" } },
-		[{ name: "value", type: "STRING" }],
-		{
-			projectName: "paxpay-prod",
-			datasetName: "pays_ledger",
-			tableName: "backup",
-		},
-		"source:pax2pay-worker-banking-ledger realm:within(uk, eu)"
-	)
-	export const operation = create(
-		"operations",
-		Operation.mapping,
-		Operation.schema,
-		{
-			projectName: "paxpay-prod",
-			datasetName: "pays_ledger",
-			tableName: "operations",
-		},
-		"source:pax2pay-worker-banking-ledger entity.type:operation realm:within(uk, eu)"
-	)
-	export const transaction = create(
-		"transactions",
-		Transaction.mapping,
-		Transaction.schema,
-		{
-			projectName: "paxpay-prod",
-			datasetName: "pays_ledger",
-			tableName: "transactions",
-		},
-		"source:pax2pay-worker-banking-ledger entity.type:transaction realm:within(uk, eu)"
-	)
+	export import AuditLog = ConfigAuditLog
 }
--- Analytics/Event/index.ts
+++ Analytics/Event/index.ts
@@ -1,10 +1,12 @@
+import { AuditLog as AuditLogEvent } from "./AuditLog"
 import { Base as EventBase } from "./Base"
 import { Operation as OperationEvent } from "./Operation"
 import { Transaction as TransactionEvent } from "./Transaction"
 
-export type Event = (Event.Transaction | Event.Operation) & { version: string }
+export type Event = (Event.Transaction | Event.Operation | Event.AuditLog) & { version: string }
 export namespace Event {
 	export type Base<T> = EventBase<T>
 	export import Transaction = TransactionEvent
 	export import Operation = OperationEvent
+	export import AuditLog = AuditLogEvent
 }
