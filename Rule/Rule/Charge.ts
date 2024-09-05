fd8f7255620567124ff97e0b1c191ef5f2aa8301
Tove Brandt <tove.brandt@pax2pay.com>
2024-09-05 15:58:30 +0200

    Added charge type

--- Rule/Rule/Charge.ts
+++ Rule/Rule/Charge.ts
@@ -3,6 +3,7 @@ import { Base } from "./Base"
 
 export interface Charge extends Base {
 	action: Charge.Action
+	type: Charge.Type
 	fee: {
 		percentage: number
 		// flat?: number
@@ -13,8 +14,13 @@ export namespace Charge {
 	export namespace Action {
 		export const value = "charge"
 	}
+	export type Type = typeof Type.value[number]
+	export namespace Type {
+		export const value = ["refund", "capture"] as const
+	}
 	export const type = Base.type.extend<Charge>({
 		action: isly.string(Action.value),
+		type: isly.string(Type.value),
 		fee: isly.object({
 			percentage: isly.number(),
 			// flat?: number
