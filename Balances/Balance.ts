2da50b430cc906531e3b21b6f4238fd5a29d3c81
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-09-05 15:57:55 +0200

    Balance restructrue.

--- Balances/Balance.ts
+++ Balances/Balance.ts
@@ -2,11 +2,18 @@ import { isoly } from "isoly"
 import { isly } from "isly"
 
 export type Balance = Partial<Record<Balance.Entry, number>>
+export type Balance2 = { available: number; reserved: Record<Balance.Reserve, number> }
 
 export namespace Balance {
+	export type Reserve = typeof Reserve.values[number]
+	export namespace Reserve {
+		export const values = ["in", "out", "buffer"]
+	}
 	export type Entry = typeof Entry.values[number]
 	export namespace Entry {
-		export const values = ["actual", "incomingReserved", "outgoingReserved", "bufferReserved"] as const
+		export const reserved = ["in", "out", "buffer"]
+		export const values = ["available", "reservedIn", "reservedOut", "reservedBuffer"] as const
+		// export const values = ["actual", "incomingReserved", "outgoingReserved", "bufferReserved"] as const
 		export const type = isly.string(values)
 		export const is = type.is
 		export const flaw = type.flaw
