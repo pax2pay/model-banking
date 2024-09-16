68116f7dd948ef925d77316984df115b5fd5fa71
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-09-16 17:31:07 +0200

    Added counterbalance warning.

--- Treasury/Snapshot/Warning/index.ts
+++ Treasury/Snapshot/Warning/index.ts
@@ -1,13 +1,20 @@
 import { isly } from "isly"
+import { Counterbalance as WarningCounterbalance } from "../../../Counterbalance"
 import { Buffer as WarningBuffer } from "./Buffer"
 import { Overdraft as WarningOverdraft } from "./Overdraft"
 import { Unguarded as WarningUnguarded } from "./Unguarded"
 
-export type Warning = Warning.Overdraft | Warning.Unguarded | Warning.Buffer
+export type Warning = Warning.Overdraft | Warning.Unguarded | Warning.Buffer | Warning.Counterbalance
 
 export namespace Warning {
-	export import Overdraft = WarningOverdraft
+	export import Counterbalance = WarningCounterbalance
 	export import Unguarded = WarningUnguarded
+	export import Overdraft = WarningOverdraft
 	export import Buffer = WarningBuffer
-	export const type = isly.union<Warning, Overdraft, Unguarded, Buffer>(Overdraft.type, Unguarded.type, Buffer.type)
+	export const type = isly.union<Warning, Overdraft, Unguarded, Buffer, Counterbalance>(
+		Overdraft.type,
+		Unguarded.type,
+		Buffer.type,
+		Counterbalance.type
+	)
 }
