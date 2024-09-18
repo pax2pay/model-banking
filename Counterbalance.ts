073eea39e466531f35bc96ef5a847bba419fde28
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-09-18 16:15:06 +0200

    Added charge to counterbalance.

--- Counterbalance.ts
+++ Counterbalance.ts
@@ -1,10 +1,10 @@
-import { isoly } from "isoly"
 import { isly } from "isly"
 
 export type Counterbalance = {
 	minted: Partial<Record<Counterbalance.Source, number>>
 	burned: Partial<Record<Counterbalance.Sink, number>>
 	internal?: Partial<Record<string, number>>
+	charge?: number
 }
 export namespace Counterbalance {
 	export type Source = string
@@ -15,17 +15,8 @@ export namespace Counterbalance {
 		minted: isly.record<Counterbalance["minted"]>(Source, isly.number()),
 		burned: isly.record<Counterbalance["burned"]>(Sink, isly.number()),
 		internal: isly.record<Required<Counterbalance>["internal"]>(isly.string(), isly.number()).optional(),
+		charge: isly.number().optional(),
 	})
 	export type Link = Source | Sink
 	export const Link = isly.string()
-	export function add(currency: isoly.Currency, addendee: Counterbalance, addend: Counterbalance): Counterbalance {
-		const result: Counterbalance = { minted: { ...addend.minted }, burned: { ...addend.burned } }
-		for (const [source, value] of Object.entries(addendee["minted"]) as [Source, number][]) {
-			result["minted"][source] = isoly.Currency.add(currency, value ?? 0, addend["minted"][source] ?? 0)
-		}
-		for (const [sink, value] of Object.entries(addendee["burned"]) as [Sink, number][]) {
-			result["burned"][sink] = isoly.Currency.add(currency, value ?? 0, addend["burned"][sink] ?? 0)
-		}
-		return result
-	}
 }
