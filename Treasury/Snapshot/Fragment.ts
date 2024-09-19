be2a2210499b80f3e23b83540d376d832db5bf05
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-09-19 16:06:07 +0200

    Change minted and burned to counterbalance, snapshot.

--- Treasury/Snapshot/Fragment.ts
+++ Treasury/Snapshot/Fragment.ts
@@ -1,14 +1,14 @@
 import { isoly } from "isoly"
 import { isly } from "isly"
 import { Balance } from "../../Balance"
+import { Counterbalance } from "../../Counterbalance"
 import { Account } from "../Account"
 import { Warning } from "./Warning"
 
 export interface Fragment {
 	warnings: Warning[]
 	emoney: Balance.Extended
-	minted: Fragment.Coinage.Minted
-	burned: Fragment.Coinage.Burned
+	counterbalance: Record<string, { total: number; account: Record<string, { amount: number }> }>
 	fiat: {
 		safe: number
 		unsafe: number
@@ -19,32 +19,9 @@ export interface Fragment {
 	}
 }
 export namespace Fragment {
-	export namespace Coinage {
-		export type LedgerAccount = string
-		export type Change = { account: Record<LedgerAccount, number>; amount: number }
-		export const change = isly.object<Change>({
-			account: isly.record(isly.string(), isly.number()),
-			amount: isly.number(),
-		})
-		type Source = string
-		export type Minted = Record<Source, Change>
-		export namespace Minted {
-			export const type = isly.record<Minted>(isly.string(), Fragment.Coinage.change)
-		}
-		type Sink = string
-		export type Burned = Record<Sink, Change>
-		export namespace Burned {
-			export const type = isly.record<Burned>(isly.string(), Fragment.Coinage.change)
-		}
-		export function sum(currency: isoly.Currency, coinage: Burned | Minted): number {
-			return Object.values(coinage).reduce((result, change) => isoly.Currency.add(currency, result, change.amount), 0)
-		}
-	}
 	export const type = isly.object<Fragment>({
 		warnings: Warning.type.array(),
-		emoney: Balance.type,
-		minted: Fragment.Coinage.Minted.type,
-		burned: Fragment.Coinage.Burned.type,
+		emoney: isly.object({ balance: Balance.type, counterbalance: Counterbalance.type }),
 		fiat: isly.object({
 			safe: isly.number(),
 			unsafe: isly.number(),
