c2625a1cb63f87ebee710c3e22cd23499d0d6e5d
Anton Kroner <anton.kroner@pax2pay.com>
2024-09-23 18:18:43 +0200

    Isly type and helper functions.

--- Treasury/Snapshot/Fragment.ts
+++ Treasury/Snapshot/Fragment.ts
@@ -1,14 +1,13 @@
 import { isoly } from "isoly"
 import { isly } from "isly"
 import { Balance } from "../../Balance"
-import { Counterbalance } from "../../Counterbalance"
 import { Account } from "../Account"
 import { Warning } from "./Warning"
 
 export interface Fragment {
 	warnings: Warning[]
 	emoney: Balance.Extended
-	counterbalance: Record<string, { total: number; account: Record<string, { amount: number }> }>
+	counterbalance: Fragment.Counterbalance
 	fiat: {
 		safe: number
 		unsafe: number
@@ -19,9 +18,36 @@ export interface Fragment {
 	}
 }
 export namespace Fragment {
+	export type Legacy = Override<Fragment, "counterbalance">
+	export type Counterbalance = Record<string, { total: number; account: Record<string, { amount: number }> }>
+	export namespace Counterbalance {
+		export const type = isly.record<Counterbalance>(
+			isly.string(),
+			isly.object<Counterbalance[string]>({
+				total: isly.number(),
+				account: isly.record<Counterbalance[string]["account"]>(
+					isly.string(),
+					isly.object<Counterbalance[string]["account"][string]>({ amount: isly.number() })
+				),
+			})
+		)
+		export function sum(currency: isoly.Currency, accounts: Record<string, { amount: number }>): number {
+			return Object.values(accounts).reduce(
+				(result, account) => isoly.Currency.add(currency, result, account.amount),
+				0
+			)
+		}
+		export function validate(currency: isoly.Currency, counterbalances: Counterbalance): boolean {
+			for (const counterbalance of Object.values(counterbalances))
+				if (isoly.Currency.subtract(currency, counterbalance.total, sum(currency, counterbalance.account)))
+					return false
+			return true
+		}
+	}
 	export const type = isly.object<Fragment>({
 		warnings: Warning.type.array(),
-		emoney: isly.object({ balance: Balance.type, counterbalance: Counterbalance.type }),
+		emoney: Balance.Extended,
+		counterbalance: Counterbalance.type,
 		fiat: isly.object({
 			safe: isly.number(),
 			unsafe: isly.number(),
@@ -32,8 +58,9 @@ export namespace Fragment {
 		}),
 	})
 	export function validate(currency: isoly.Currency, fragment: Fragment): boolean {
+		const validCounterbalance = Counterbalance.validate(currency, fragment.counterbalance)
 		const issuable = fragment.fiat.total
 		const actual = fragment.emoney.actual ?? 0
-		return issuable == actual
+		return validCounterbalance && issuable == actual
 	}
 }
