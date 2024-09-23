0c06f6aca9c8a49897606898a2448a0a0aac1cbc
Anton Kroner <anton.kroner@pax2pay.com>
2024-09-23 16:52:43 +0200

    Added function for summing counterbalances.

--- Operation/Changes.ts
+++ Operation/Changes.ts
@@ -36,6 +36,17 @@ export namespace Changes {
 			0
 		)
 	}
+	export function counterbalance(changes: Changes, currency: isoly.Currency): number {
+		return Object.entries(changes).reduce(
+			(r, [entry, change]) =>
+				isoly.Currency.add(
+					currency,
+					r,
+					Entry.Balance.type.is(entry) ? 0 : (change.type == "subtract" ? -1 : 1) * (change.amount ?? 0)
+				),
+			0
+		)
+	}
 	export type Sum = Partial<Record<Changes.Entry.Balance, number>> & Record<Changes.Entry.Counterbalance, number>
 	export type MaybeLegacy = Changes | Legacy
 	export const type = isly.record<Legacy>(isly.string(), Change.type)
