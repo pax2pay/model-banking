640e69d9713511e2dac7859473c933e89f8d91e7
Anton Kroner <anton.kroner@pax2pay.com>
2024-09-24 14:17:24 +0200

    Finished Fragment fromlegacy fucntion.

--- Treasury/Snapshot/Fragment.ts
+++ Treasury/Snapshot/Fragment.ts
@@ -18,58 +18,6 @@ export interface Fragment {
 	}
 }
 export namespace Fragment {
-	export const type = isly.object<Fragment>({
-		warnings: Warning.type.array(),
-		emoney: Balance.Extended,
-		counterbalance: Counterbalance.type,
-		fiat: isly.object({
-			safe: isly.number(),
-			unsafe: isly.number(),
-			total: isly.number(),
-			other: isly.number(),
-			buffer: isly.number(),
-			accounts: Account.type.array(),
-		}),
-	})
-	export type Legacy = Omit<Fragment, "counterbalance"> & { minted: Legacy.Coinage; burned: Legacy.Coinage }
-	export namespace Legacy {
-		export type LedgerAccount = string
-		export type Change = { account: Record<LedgerAccount, number>; amount: number }
-		export const change = isly.object<Change>({
-			account: isly.record(isly.string(), isly.number()),
-			amount: isly.number(),
-		})
-		export type Coinage = Record<string, Change>
-		export const type = isly.record<Coinage>(isly.string(), change)
-		export function toCounterbalance(minted: Coinage, burned: Coinage): Counterbalance {
-			const result: Counterbalance = {}
-			for (const [code, change] of Object.entries(minted)) {
-				result[code] = { total: change.amount, account: { asdf: { amount: 5 } } }
-			}
-			return result
-		}
-	}
-
-	export function fromLegacy(fragment: Fragment | Legacy): Fragment {
-		if ("counterbalance" in fragment) {
-			return fragment
-		} else {
-			const result: Fragment = {
-				counterbalance: {},
-				emoney: {},
-				fiat: {
-					safe: 0,
-					unsafe: 0,
-					total: 0,
-					other: 0,
-					buffer: 0,
-					accounts: [],
-				},
-				warnings: [],
-			}
-			return result
-		}
-	}
 	export type Counterbalance = Record<string, { total: number; account: Record<string, { amount: number }> }>
 	export namespace Counterbalance {
 		export const type = isly.record<Counterbalance>(
@@ -100,5 +48,71 @@ export namespace Fragment {
 		const issuable = fragment.fiat.total
 		const actual = fragment.emoney.actual ?? 0
 		return validCounterbalance && issuable == actual
+	}
+	export const type = isly.object<Fragment>({
+		warnings: Warning.type.array(),
+		emoney: Balance.Extended,
+		counterbalance: Counterbalance.type,
+		fiat: isly.object({
+			safe: isly.number(),
+			unsafe: isly.number(),
+			total: isly.number(),
+			other: isly.number(),
+			buffer: isly.number(),
+			accounts: Account.type.array(),
+		}),
+	})
+	export type Legacy = Omit<Fragment, "counterbalance"> & { minted: Legacy.Coinage; burned: Legacy.Coinage }
+	export namespace Legacy {
+		export type LedgerAccount = string
+		export type Change = { account: Record<LedgerAccount, number>; amount: number }
+		export const change = isly.object<Change>({
+			account: isly.record(isly.string(), isly.number()),
+			amount: isly.number(),
+		})
+		export type Coinage = Record<string, Change>
+		export const type = isly.record<Coinage>(isly.string(), change)
+		function mergeAccounts(
+			currency: isoly.Currency,
+			burned: Record<string, { amount: number }>,
+			minted?: Record<string, { amount: number }>
+		): Record<string, { amount: number }> {
+			const result: Record<string, { amount: number }> = { ...(minted ?? {}) }
+			for (const [id, account] of Object.entries(burned)) {
+				result[id] = { amount: isoly.Currency.subtract(currency, result[id]?.amount ?? 0, account.amount) }
+			}
+			return result
+		}
+		function accountToNew(account: Record<LedgerAccount, number>): Record<string, { amount: number }> {
+			return Object.fromEntries(
+				Object.entries(account).map<[string, { amount: number }]>(([id, amount]) => [id, { amount }])
+			)
+		}
+		export function toCounterbalance(currency: isoly.Currency, minted: Coinage, burned: Coinage): Counterbalance {
+			const result: Counterbalance = {}
+			for (const [code, change] of Object.entries(minted)) {
+				result[code] = { total: change.amount, account: accountToNew(change.account) }
+			}
+			for (const [code, change] of Object.entries(burned)) {
+				result[code] = {
+					total: isoly.Currency.subtract(currency, result[code]?.total ?? 0, change.amount),
+					account: mergeAccounts(currency, accountToNew(change.account), result[code]?.account),
+				}
+			}
+			return result
+		}
+	}
+	export function fromLegacy(currency: isoly.Currency, fragment: Fragment | Legacy): Fragment {
+		let result: Fragment
+		if ("counterbalance" in fragment) {
+			result = fragment
+		} else {
+			const counterbalance = Legacy.toCounterbalance(currency, fragment.minted, fragment.burned)
+			const result: Fragment = { ...fragment, counterbalance }
+			"burned" in result && delete result.burned
+			"minted" in result && delete result.minted
+			return result
+		}
+		return result
 	}
 }
