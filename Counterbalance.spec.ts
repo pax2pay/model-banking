cbbdc0cab11668a1394d87cca5c99200ef6343e5
marcushanses <79835961+marcushanses@users.noreply.github.com>
2024-09-25 12:04:39 +0200

    Added charge to counterbalance. (#401)
    * Added charge to counterbalance.
    
    * Removed test for add.

--- Counterbalance.spec.ts
+++ Counterbalance.spec.ts
@@ -5,15 +5,4 @@ describe("Counterbalances", () => {
 		expect(pax2pay.Counterbalance.Sink.is(`paxgiro-safe01`)).toBeTruthy()
 		expect(pax2pay.Counterbalance.Link.is(`paxgiro-safe01`)).toBeTruthy()
 	})
-	it("Counterbalance.add", () => {
-		const Counterbalance1: pax2pay.Counterbalance = { minted: { [`paxgiro-safe01`]: 123 }, burned: {} }
-		const Counterbalance: pax2pay.Counterbalance = {
-			minted: { [`paxgiro-safe01`]: 123, [`clearbank-safe01`]: 123 },
-			burned: {},
-		}
-		expect(pax2pay.Counterbalance.add("USD", Counterbalance1, Counterbalance)).toEqual({
-			minted: { [`paxgiro-safe01`]: 246, [`clearbank-safe01`]: 123 },
-			burned: {},
-		})
-	})
 })
--- Counterbalances.ts
+++ Counterbalances.ts
@@ -5,13 +5,6 @@ import { Counterbalance } from "./Counterbalance"
 export type Counterbalances = Partial<Record<isoly.Currency, Counterbalance>>
 
 export namespace Counterbalances {
-	export function add(addendee: Counterbalances, addend: Counterbalances): Counterbalances {
-		const result: Counterbalances = {}
-		for (const [currency, counterbalance] of Object.entries(addendee) as [isoly.Currency, Counterbalance][]) {
-			result[currency] = Counterbalance.add(currency, counterbalance, addend[currency] ?? { minted: {}, burned: {} })
-		}
-		return result
-	}
 	export const type = isly.record<Counterbalances>(
 		isly.fromIs("isoly.Currency", isoly.Currency.is),
 		Counterbalance.type
