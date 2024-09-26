95f1a7a54e41dba6aa919ad46da3f8437254133d
Tove Brandt <tove.brandt@pax2pay.com>
2024-09-26 11:40:32 +0200

    wipwipwip

--- Warning/index.ts
+++ Warning/index.ts
@@ -1,3 +1,4 @@
+import { isly } from "isly"
 import { Base as WarningBase } from "./Base"
 import { Snapshot as WarningSnapshot } from "./Snapshot"
 
@@ -6,4 +7,5 @@ export type Warning = Warning.Snapshot //| Settlement
 export namespace Warning {
 	export import Snapshot = WarningSnapshot
 	export import Base = WarningBase
+	export const type = isly.union<Warning>(Snapshot.type)
 }
