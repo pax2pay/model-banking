0e7ecc2caa9cde196386203221edb1f17c709f64
Anton Kroner <anton.kroner@pax2pay.com>
2024-09-26 17:23:53 +0200

    Add events array to card so history can be kept for legacy reasons.

--- Card/index.ts
+++ Card/index.ts
@@ -27,7 +27,8 @@ export interface Card {
 	limit: Amount
 	spent: Amount
 	status: "active" | "cancelled"
-	history: CardEvent[]
+	history: any[]
+	events?: Card.Event[]
 	rules: Rule[]
 	meta?: CardMeta
 }
@@ -52,7 +53,8 @@ export namespace Card {
 		limit: isly.tuple(isly.fromIs("isoly.Currency", isoly.Currency.is), isly.number()),
 		spent: isly.tuple(isly.fromIs("isoly.Currency", isoly.Currency.is), isly.number()),
 		status: isly.union(isly.string("active"), isly.string("cancelled")),
-		history: isly.union(CardEvent.type, isly.any()).array(),
+		history: isly.any(),
+		events: CardEvent.type.array().optional(),
 		rules: ruleType.array(),
 		meta: isly.fromIs("Card.Meta", CardMeta.is).optional(),
 	})
@@ -68,12 +70,16 @@ export namespace Card {
 	const csvMap: Record<string, (card: Card) => string | number | undefined> = {
 		id: card => card.id,
 		created: card => readableDate(card.created),
-		cancelled: card =>
-			readableDate(
-				card.history.find(
-					o => o.type == "create" || ((o as any).type == "card" && "status" in o && o.status == "cancelled") // as any for legacy reasons
-				)?.created
-			),
+		cancelled: card => {
+			if (card.events) {
+				return readableDate(card.events.find(o => o.type == "cancel")?.created)
+			} else {
+				// for legacy reasons
+				return readableDate(
+					card.history?.find(o => o.type == "card" && "status" in o && o.status == "cancelled")?.created
+				)
+			}
+		},
 		"organization.code": card => card.organization,
 		realm: card => card.realm,
 		account: card => card.account,
@@ -87,18 +93,18 @@ export namespace Card {
 		expiry: card => readableDate(Expiry.toDateTime(card.details.expiry)),
 		iin: card => card.details.iin,
 		holder: card => card.details.holder,
-		"authorization.count": card =>
-			card.history.filter(
-				o =>
-					(o.type == "authorization" && "status" in o && o.status == "created") || // for legacy reasons
-					(o.type == "authorization" && o.outcome == "created")
-			).length,
-		"capture.count": card =>
-			card.history.filter(
-				o =>
-					(o.type == "authorization" && "status" in o && o.status == "created") || // for legacy reasons
-					o.type == "capture"
-			).length,
+		"authorization.count": card => {
+			const authorizations = card.events?.filter(o => o.type == "authorization" && o.outcome == "created").length ?? 0
+			const legacy =
+				card.history?.filter(o => o.type == "authorization" && "status" in o && o.status == "created").length ?? 0
+			return authorizations + legacy
+		},
+		"capture.count": card => {
+			const captures = card.events?.filter(o => o.type == "capture").length ?? 0
+			const legacy =
+				card.history?.filter(o => o.type == "authorization" && "status" in o && o.status == "captured").length ?? 0
+			return captures + legacy
+		},
 	}
 	function readableDate(date: isoly.DateTime | undefined): string | undefined {
 		return date && date.slice(0, 10) + " " + (date.endsWith("Z") ? date.slice(11, -1) : date.slice(11))
