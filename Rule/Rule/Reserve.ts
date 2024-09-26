619bccd76605d16355cf12e0566f37350a1c620b
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-09-26 13:48:57 +0200

    Reserve rule.

---
+++ Rule/Rule/Reserve.ts
@@ -0,0 +1,46 @@
+import { isoly } from "isoly"
+import { isly } from "isly"
+import { Amount } from "../../Amount"
+import { Realm } from "../../Realm"
+import { Base } from "./Base"
+
+export interface Reserve extends Base {
+	action: Reserve.Action
+	reserve: {
+		percentage?: number
+		fixed?: number | Amount
+	}
+}
+export namespace Reserve {
+	export type Action = typeof Action.value
+	export namespace Action {
+		export const value = "reserve"
+	}
+	export const type = Base.type.extend<Reserve>({
+		action: isly.string(Action.value),
+		reserve: isly.object({
+			percentage: isly.number(),
+			fixed: isly.union<number | Amount>(isly.number(), Amount.type).optional(),
+		}),
+	})
+	export interface Backend extends Reserve {
+		reserve: {
+			percentage?: number
+			fixed?: Amount
+		}
+	}
+	export function toBackend(rule: Reserve, realm: Realm): Backend
+	export function toBackend(rule: Reserve, currency: isoly.Currency): Backend
+	export function toBackend(rule: Reserve, currency: Realm | isoly.Currency): Backend {
+		return {
+			...rule,
+			reserve: {
+				...rule.reserve,
+				fixed:
+					typeof rule.reserve.fixed == "number"
+						? [Realm.is(currency) ? Realm.currency[currency] : currency, rule.reserve.fixed]
+						: rule.reserve.fixed,
+			},
+		}
+	}
+}
--- Rule/Rule/index.ts
+++ Rule/Rule/index.ts
@@ -3,14 +3,16 @@ import { Realm } from "../../Realm"
 import { Base as RuleBase } from "./Base"
 import { Charge as RuleCharge } from "./Charge"
 import { Other as RuleOther } from "./Other"
+import { Reserve as RuleReserve } from "./Reserve"
 import { Score as RuleScore } from "./Score"
 
-export type Rule = Rule.Other | Rule.Score | Rule.Charge
+export type Rule = Rule.Other | Rule.Score | Rule.Charge | Rule.Reserve
 
 export namespace Rule {
 	export import Other = RuleOther
 	export import Score = RuleScore
 	export import Charge = RuleCharge
+	export import Reserve = RuleReserve
 	export import Base = RuleBase
 	export type Api = Rule.Other | Rule.Score | Rule.Charge.Api
 	export namespace Api {
@@ -25,13 +27,21 @@ export namespace Rule {
 	}
 	export type Action = typeof Action.values[number]
 	export namespace Action {
-		export const values = [...Other.Action.values, Score.Action.value, Charge.Action.value] as const
+		export const values = [
+			...Other.Action.values,
+			Score.Action.value,
+			Charge.Action.value,
+			Reserve.Action.value,
+		] as const
 		export const type = isly.string<Action>(values)
 	}
 }
 // Outside of the namespace otherwise the Rule import in Card/Card.Creatable and Organization causes a circular dependency crash
-export const type = isly.union<Rule.Other | Rule.Score | Rule.Charge, Rule.Other, Rule.Score, Rule.Charge.Api>(
-	Rule.Other.type,
-	Rule.Score.type,
-	Rule.Charge.Api.type
-)
+
+export const type = isly.union<
+	Rule.Other | Rule.Score | Rule.Charge.Api | Rule.Reserve,
+	Rule.Other,
+	Rule.Score,
+	Rule.Charge.Api,
+	Rule.Reserve
+>(Rule.Other.type, Rule.Score.type, Rule.Charge.Api.type, Rule.Reserve.type)
