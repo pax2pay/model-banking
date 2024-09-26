9e634fc226cec810793471ea7b50140992e97ce7
Anton Kroner <anton.kroner@pax2pay.com>
2024-09-26 18:08:04 +0200

    Some requested changes.

--- Card/Event/Change.ts
+++ Card/Event/Change.ts
@@ -5,14 +5,14 @@ import { Changeable } from "../Changeable"
 export interface Change {
 	type: "change"
 	created: isoly.DateTime
-	from?: Changeable
-	to?: Changeable
+	from: Changeable
+	to: Changeable
 }
 export namespace Change {
 	export const type = isly.object<Change>({
 		type: isly.string("change"),
 		created: isly.fromIs("isoly.DateTime", isoly.DateTime.is),
-		from: Changeable.type.optional(),
-		to: Changeable.type.optional(),
+		from: Changeable.type,
+		to: Changeable.type,
 	})
 }
--- Card/Event/Create.ts
+++ Card/Event/Create.ts
@@ -1,13 +1,16 @@
 import { isoly } from "isoly"
 import { isly } from "isly"
+import { Amount } from "../../Amount"
 
 export interface Create {
 	type: "create"
 	created: isoly.DateTime
+	limit: Amount
 }
 export namespace Create {
 	export const type = isly.object<Create>({
 		type: isly.string("create"),
 		created: isly.fromIs("isoly.DateTime", isoly.DateTime.is),
+		limit: Amount.type,
 	})
 }
