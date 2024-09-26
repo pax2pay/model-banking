1c4286839c951218f793af95a161f4477457f13f
Tove Brandt <tove.brandt@pax2pay.com>
2024-09-26 13:18:46 +0200

    wipwipwiii

--- Warning/Snapshot/Overdraft.ts
+++ Warning/Snapshot/Overdraft.ts
@@ -4,12 +4,14 @@ import { Base } from "../Base"
 
 export interface Overdraft extends Base {
 	type: "overdraft"
+	account: string
 	balance: Balance.Extended
 }
 
 export namespace Overdraft {
 	export const type = Base.type.extend<Overdraft>({
 		type: isly.string("overdraft"),
+		account: isly.string(),
 		balance: Balance.Extended,
 	})
 }
