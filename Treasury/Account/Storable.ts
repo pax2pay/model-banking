7b5207078bccfb640d42c4ad48b761e819a4593d
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2024-10-02 15:56:22 +0200

    Updated treasury account. (#409)
    * Updated treasury account.
    Removed unused type.
    
    * Cleaned up.
    
    * Fixed build.

--- Treasury/Account/Storable.ts
+++ Treasury/Account/Storable.ts
@@ -1,4 +1,3 @@
-import { cryptly } from "cryptly"
 import { isoly } from "isoly"
 import { isly } from "isly"
 import { Realm } from "../../Realm"
@@ -7,27 +6,22 @@ import { Category } from "./Category"
 import { Conditions } from "./Conditions"
 
 export interface Storable {
-	id: cryptly.Identifier
+	code: string
 	created: isoly.DateTime
 	realm: Realm
 	supplier: Supplier
-	link: string
 	type: Category
 	reference: string
 	conditions?: Conditions
 }
-
 export namespace Storable {
 	export const type = isly.object<Storable>({
-		id: isly.string(),
+		code: isly.string(),
 		created: isly.fromIs("Treasury.Account.Storable", isoly.DateTime.is),
-		realm: isly.fromIs("realm", Realm.is),
-		supplier: isly.fromIs("supplier", Supplier.is),
-		link: isly.string(),
+		realm: Realm.type,
+		supplier: Supplier.type,
 		type: Category.type,
 		reference: isly.string(),
 		conditions: Conditions.type.optional(),
 	})
-	export const is = type.is
-	export const flaw = type.flaw
 }
