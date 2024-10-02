a0540b3894c22415ca2a7ab20d8a1661f4d4dbaf
marcushanses <79835961+marcushanses@users.noreply.github.com>
2024-10-02 17:17:19 +0200

    Use old original amount for transaction state. (#424)

--- Rule/State/Transaction.ts
+++ Rule/State/Transaction.ts
@@ -20,20 +20,31 @@ export interface Transaction extends ModelTransaction.Creatable {
 export namespace Transaction {
 	export function from(
 		accountName: string,
-		transaction: ModelTransaction.Creatable & { counterpart: Address },
+		transaction: (ModelTransaction.Creatable & { counterpart: Address }) | ModelTransaction,
 		kind: Rule.Base.Kind,
 		stage: "finalize" | "initiate"
 	): Transaction {
+		const [amount, total] =
+			"state" in transaction
+				? [
+						transaction.state?.transaction.original.amount ?? transaction.amount,
+						isoly.Currency.subtract(
+							transaction.currency,
+							transaction.state?.transaction.original.total ?? transaction.amount,
+							stage === "finalize" ? transaction.state?.transaction.original.reserve ?? 0 : 0
+						),
+				  ]
+				: [transaction.amount, transaction.amount]
 		return {
 			...transaction,
 			stage,
 			kind,
-			amount: Math.abs(transaction.amount),
+			amount: Math.abs(amount),
 			type: ModelTransaction.getType(transaction.counterpart, accountName),
 			original: {
 				currency: transaction.currency,
-				amount: Math.abs(transaction.amount),
-				total: Math.abs(transaction.amount),
+				amount: Math.abs(amount),
+				total: Math.abs(total),
 			},
 		}
 	}
