911e7b3ba7cd169e48227ba17feb6d10fe7617e8
Tove Brandt <tove.brandt@pax2pay.com>
2024-10-08 13:06:14 +0200

    updated props

--- Warning/Snapshot/Counterbalance.ts
+++ Warning/Snapshot/Counterbalance.ts
@@ -5,12 +5,10 @@ import { Base } from "../Base"
 export interface Counterbalance extends Base {
 	type: "counterbalance"
 	currency: isoly.Currency
-	counterbalance: string
 }
 export namespace Counterbalance {
 	export const type = Base.type.extend<Counterbalance>({
 		type: isly.string("counterbalance"),
 		currency: isly.string(),
-		counterbalance: isly.string(),
 	})
 }
