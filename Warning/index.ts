cfd41e484db24735e84efde60bd9a5e382c0753b
tovebr <70133569+tovebr@users.noreply.github.com>
2024-10-09 09:58:13 +0200

    Export issue (#431)

--- Warning/index.ts
+++ Warning/index.ts
@@ -1,5 +1,6 @@
 import { isly } from "isly"
 import { Base as WarningBase } from "./Base"
+import { Issue as WarningIssue } from "./Issue"
 import { Settlement as WarningSettlement } from "./Settlement"
 import { Snapshot as WarningSnapshot } from "./Snapshot"
 
@@ -9,5 +10,6 @@ export namespace Warning {
 	export import Snapshot = WarningSnapshot
 	export import Settlement = WarningSettlement
 	export import Base = WarningBase
+	export import Issue = WarningIssue
 	export const type = isly.union<Warning, Snapshot, Settlement>(Snapshot.type, Settlement.type)
 }
