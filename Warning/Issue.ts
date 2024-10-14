e749c5c7819a4ab735886a9934bd01cf29d31cea
tovebr <70133569+tovebr@users.noreply.github.com>
2024-10-14 11:51:20 +0200

    Created issue creatable. (#434)

--- Warning/Issue.ts
+++ Warning/Issue.ts
@@ -1,13 +1,27 @@
+import { isoly } from "isoly"
 import { isly } from "isly"
 
 export interface Issue {
 	link: string
 	status: "closed" | "open"
 }
-
 export namespace Issue {
 	export const type = isly.object<Issue>({
 		link: isly.string(),
 		status: isly.string(["closed", "open"]),
 	})
+	export interface Creatable {
+		type: string
+		currency: isoly.Currency
+		resource?: string
+		issue: Issue
+	}
+	export namespace Creatable {
+		export const type = isly.object<Creatable>({
+			type: isly.string(),
+			currency: isly.fromIs("Currency", isoly.Currency.is),
+			resource: isly.string().optional(),
+			issue: Issue.type,
+		})
+	}
 }
