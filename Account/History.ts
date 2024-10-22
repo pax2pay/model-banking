1988de3a76db45f42e21d9999f91ec87d613797f
Tove Brandt <brandt.tove@gmail.com>
2024-10-22 14:53:33 +0200

    fixes

--- Account/History.ts
+++ Account/History.ts
@@ -3,20 +3,20 @@ import { isly } from "isly"
 
 export type History = {
 	timestamp: isoly.DateTime
-	property: "status" | "rails" | "counterparts" | "rules"
-	to: string
+	property: "status" | "rails" | "counterparts" | "rules" | "created"
+	to?: any
 }
 
 export namespace History {
 	export type Property = typeof Property.values[number]
 	export namespace Property {
-		export const values = ["status", "rails", "counterparts", "rules"] as const
+		export const values = ["status", "rails", "counterparts", "rules", "created"] as const
 		export const type = isly.string<Property>(values)
 	}
 	export const type = isly.object<History>({
 		timestamp: isly.fromIs("isoly.DateTime", isoly.DateTime.is),
 		property: Property.type,
-		to: isly.string(),
+		to: isly.any().optional(),
 	})
 	export const is = type.is
 	export const flaw = type.flaw
