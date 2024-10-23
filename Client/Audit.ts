5bfccec1c7e9ba8d8e3d2930a0df474f29bf9a35
Anton Kroner <anton.kroner@pax2pay.com>
2024-10-23 09:56:12 +0200

    Added queries to audit log list endpoint.

--- Client/Audit.ts
+++ Client/Audit.ts
@@ -1,10 +1,20 @@
 import { gracely } from "gracely"
+import { isoly } from "isoly"
 import { http } from "cloudly-http"
 import { Audit as AuditLog } from "../Audit"
 
 export class Audit {
 	constructor(private readonly client: http.Client) {}
-	async list(resource?: AuditLog.Resource): Promise<AuditLog[] | gracely.Error> {
-		return this.client.get<AuditLog[]>(`/audit${resource ? "/" + resource : ""}`)
+	async list(
+		options?: {
+			resource?: AuditLog.Resource
+			limit?: number
+			cursor?: string
+		} & Partial<isoly.DateRange>
+	): Promise<AuditLog[] | gracely.Error> {
+		const queries = options && http.Search.stringify(options)
+		return this.client.get<AuditLog[]>(
+			`/audit${options?.resource ? "/" + options?.resource : ""}${queries ? "?" + queries : ""}`
+		)
 	}
 }
