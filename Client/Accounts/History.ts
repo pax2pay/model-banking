bd28622049e92cb55d07bdb58d2e89ef8623335e
Tove Brandt <brandt.tove@gmail.com>
2024-10-23 11:25:24 +0200

    fix

--- Client/Accounts/History.ts
+++ Client/Accounts/History.ts
@@ -7,7 +7,7 @@ export class History extends rest.Collection<gracely.Error> {
 	constructor(client: http.Client) {
 		super(client)
 	}
-	async list(account: string, status: Account.Status): Promise<Account.History[] | gracely.Error> {
+	async list(account: string): Promise<Account.History[] | gracely.Error> {
 		return this.client.get<Account.History[]>(`/account/${account}/history`)
 	}
 }
