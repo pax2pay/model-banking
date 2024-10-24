9ec13aaed2458db865d8dc689492628d2bf93060
Tove Brandt <brandt.tove@gmail.com>
2024-10-24 10:20:51 +0200

    Update to Audit to fit card

--- Audit.ts
+++ Audit.ts
@@ -3,7 +3,7 @@ import { isly } from "isly"
 
 export type Audit = storage.AuditLogger.Entry<Audit.Type>
 export namespace Audit {
-	export type Type = { rule: "change" | "add"; user: "login" | "change" }
+	export type Type = { rule: "change" | "add"; user: "login" | "change"; card: "create" | "change" | "remove" }
 	export type Resource = typeof Resource.values[number]
 	export namespace Resource {
 		export const values = ["rule", "user"] as const
