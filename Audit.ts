98a4f876ce872206b5f3e45c93e9d24fb0b47ee4
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-10-24 10:44:43 +0200

    Added all card audit logs.

--- Audit.ts
+++ Audit.ts
@@ -3,10 +3,25 @@ import { isly } from "isly"
 
 export type Audit = storage.AuditLogger.Entry<Audit.Type>
 export namespace Audit {
-	export type Type = { rule: "change" | "add"; user: "login" | "change"; card: "create" | "change" | "remove" }
+	export type Type = {
+		rule: "change" | "add"
+		user: "login" | "change"
+		settlements:
+			| "archive"
+			| "collect"
+			| "create"
+			| "inspect"
+			| "payout"
+			| "remove"
+			| "expect"
+			| "issue"
+			| "create-entry"
+			| "replace-entry"
+		marqeta: "gateway-create" | "gateway-update" | "hook-register" | "hook-update" | "product-create" | "user-create"
+	}
 	export type Resource = typeof Resource.values[number]
 	export namespace Resource {
-		export const values = ["rule", "user"] as const
+		export const values = ["rule", "user", "settlements", "marqeta"] as const
 		export const type = isly.string(values)
 	}
 }
