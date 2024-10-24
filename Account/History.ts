138bf32b8af81d66a5d0729a33ae45673c3960ef
Tove Brandt <brandt.tove@gmail.com>
2024-10-24 14:41:02 +0200

    Add status to account.

---
+++ Account/History.ts
@@ -0,0 +1,32 @@
+import { isoly } from "isoly"
+import { isly } from "isly"
+
+export type History = {
+	timestamp: isoly.DateTime
+	property: History.Property
+	to?: any
+}
+
+export namespace History {
+	export type Property = typeof Property.values[number]
+	export namespace Property {
+		export const values = [
+			"status",
+			"rails",
+			"counterparts",
+			"rules",
+			"created",
+			"updated",
+			"buffer",
+			"listeners",
+		] as const
+		export const type = isly.string<Property>(values)
+	}
+	export const type = isly.object<History>({
+		timestamp: isly.fromIs("isoly.DateTime", isoly.DateTime.is),
+		property: Property.type,
+		to: isly.any().optional(),
+	})
+	export const is = type.is
+	export const flaw = type.flaw
+}
