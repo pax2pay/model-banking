e0ca1a621735fbe1fd99a5462a50976a188060e0
tovebr <70133569+tovebr@users.noreply.github.com>
2024-10-28 10:07:12 +0100

    Update to Audit to fit card (#450)
    * Update to Audit to fit card
    
    * Added all card audit logs.
    
    * More logs.
    
    * removed double
    
    ---------
    
    Co-authored-by: Marcus Hanses <marcus.hanses@pax2pay.com>

--- Audit.ts
+++ Audit.ts
@@ -3,10 +3,56 @@ import { isly } from "isly"
 
 export type Audit = storage.AuditLogger.Entry<Audit.Type>
 export namespace Audit {
-	export type Type = { rule: "change" | "add"; user: "login" | "change" }
+	export type Type = {
+		rule: "change" | "add" | "remove"
+		user: "login" | "change"
+		settlements:
+			| "archive"
+			| "collect"
+			| "create"
+			| "inspect"
+			| "payout"
+			| "remove"
+			| "expect"
+			| "issue"
+			| "create-entry"
+			| "replace-entry"
+		marqeta: "gateway-create" | "gateway-update" | "hook-register" | "hook-update" | "product-create" | "user-create"
+		account:
+			| "buffer"
+			| "counterpart-remove"
+			| "counterpart-replace"
+			| "listener-create"
+			| "listener-remove"
+			| "rail-create"
+			| "rail-remove"
+			| "rail-replace"
+			| "rule-create"
+			| "rule-remove"
+			| "rule-replace"
+			| "create"
+			| "status"
+			| "operation"
+			| "remove"
+			| "update"
+		label: "create" | "remove" | "replace"
+		transaction: "operation" | "cancel" | "system" | "update" | "note"
+		organization:
+			| "group-replace"
+			| "listener-create"
+			| "listener-remove"
+			| "rule-create"
+			| "rule-remove"
+			| "rule-replace"
+			| "create"
+			| "remove"
+			| "update"
+		route: "create"
+		clearbank: "assessmentFailed" | "validationFailed"
+	}
 	export type Resource = typeof Resource.values[number]
 	export namespace Resource {
-		export const values = ["rule", "user"] as const
+		export const values = ["rule", "user", "settlements", "marqeta"] as const
 		export const type = isly.string(values)
 	}
 }
