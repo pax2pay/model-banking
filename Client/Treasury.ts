aaadb7c801d0ae3fb60bf79eee0868af4d6169b3
marcushanses <79835961+marcushanses@users.noreply.github.com>
2024-10-31 10:31:12 +0100

    Remove rebalance. (#457)

--- Client/Treasury.ts
+++ Client/Treasury.ts
@@ -2,13 +2,9 @@ import { gracely } from "gracely"
 import { isoly } from "isoly"
 import { http } from "cloudly-http"
 import { Treasury as TreasuryModel } from "../Treasury"
-import { Result } from "../Treasury/Balance"
 
 export class Treasury {
 	constructor(private readonly client: http.Client) {}
-	async change(currency: isoly.Currency, changes: Result[]): Promise<gracely.Result> {
-		return this.client.patch(`/treasury/${currency}`, changes)
-	}
 	async fetch(hour?: isoly.DateTime): Promise<TreasuryModel.Snapshot | gracely.Error> {
 		const path = hour ? `/${isoly.DateTime.truncate(hour, "hours")}` : ""
 		return this.client.get<TreasuryModel.Snapshot>(`/treasury/snapshot${path}`)
