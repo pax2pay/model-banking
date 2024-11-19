a0804ae84ac159bd01492d8a869795262de8df68
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-11-19 11:49:26 +0100

    Add back old preset.

--- Card/Preset.ts
+++ Card/Preset.ts
@@ -14,6 +14,7 @@ export namespace Preset {
 		"test-ta-pg-200",
 		"test-ta-mc-200",
 		"p2p-diners-175",
+		"p2p-diners-200",
 		"test-diners-200",
 		"test-diners-202",
 	] as const
@@ -23,6 +24,7 @@ export namespace Preset {
 	export const presets: Record<Preset, Stack> = {
 		"p2p-mc-200": "uk-mc-tpl-marqeta",
 		"p2p-diners-175": "uk-diners-dpg",
+		"p2p-diners-200": "uk-diners-dpg",
 		"test-diners-200": "testUK-diners-dpg",
 		"test-diners-202": "testUK-diners-dpg",
 		"test-mc-200": "testUK-marqeta",
