1fcf42c78cdc0e14f68e1edd16d3ae26cc93d1f1
Anton Kroner <anton.kroner@pax2pay.com>
2024-11-19 17:33:12 +0100

    Added account to capture entry creatable.

--- Settlement/Entry/Capture.ts
+++ Settlement/Entry/Capture.ts
@@ -8,13 +8,13 @@ export interface Capture extends Capture.Creatable {
 	status: "succeeded" | "failed"
 	reason?: string
 }
-
 export namespace Capture {
 	export function from(creatable: Creatable): Capture {
 		return { status: "succeeded", ...creatable }
 	}
 	export interface Creatable {
 		type: "capture"
+		account?: string // Only defined when using the new card id + account id card references
 		authorization: Authorization
 		reference: string // card transaction
 		batch: Batch
@@ -24,6 +24,7 @@ export namespace Capture {
 	export namespace Creatable {
 		export const type = isly.object<Creatable>({
 			type: isly.string("capture"),
+			account: isly.string().optional(),
 			authorization: Authorization.type,
 			reference: isly.string(),
 			fee: Fee.type,
