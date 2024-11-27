baad174c36700502140d1ac66683014a934f3580
tovebr <tove.brandt@pax2pay.com>
2024-11-27 15:26:12 +0100

    Fix

--- Log/Locations.ts
+++ Log/Locations.ts
@@ -1,25 +1,24 @@
 import { isly } from "isly"
 
 export interface Locations {
-	cfConnectionIp?: string
-	cfIpCountry?: string
+	"cf-connecting-ip"?: string
+	"cf-ipcountry"?: string
 	datacenter?: string
 	country?: string
 }
 export namespace Locations {
 	export const type = isly.object<Locations>({
-		cfConnectionIp: isly.string().optional(),
-		cfIpCountry: isly.string().optional(),
+		"cf-connecting-ip": isly.string().optional(),
+		"cf-ipcountry": isly.string().optional(),
 		datacenter: isly.string().optional(),
 		country: isly.string().optional(),
 	})
 	export function getLocations(request: any): Locations | undefined {
-		const locations = {
+		return {
 			"cf-connecting-ip": request.headers.get("cf-connecting-ip") ?? undefined,
 			"cf-ipcountry": request.headers.get("cf-ipcountry") ?? undefined,
 			datacenter: request.cf?.colo ?? undefined,
 			country: request.cf?.country ?? undefined,
 		}
-		return locations
 	}
 }
