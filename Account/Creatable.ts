86f584f2ade713dcb50334579121cb461bfd5936
marcushanses <79835961+marcushanses@users.noreply.github.com>
2024-11-29 14:55:35 +0100

    Add location to account. (#481)
    * Add location to account.
    
    * Fixed tests.

--- Account/Creatable.ts
+++ Account/Creatable.ts
@@ -1,9 +1,20 @@
+import { DurableObjectLocationHint } from "@cloudflare/workers-types"
 import { isly } from "isly"
 
 export interface Creatable {
 	name: string
+	location?: Creatable.Location
 }
 
 export namespace Creatable {
-	export const type = isly.object<Creatable>({ name: isly.string() })
+	export type Location = typeof Location.values[number]
+	export namespace Location {
+		export const values = ["east-north-america", "west-europe"] as const
+		export function toCloudflare(value: Location): DurableObjectLocationHint {
+			return value == "east-north-america" ? "enam" : "weur"
+		}
+		// All location hints: ["wnam", "enam", "sam", "weur", "eeur", "apac", "oc", "afr", "me"]
+		export const type = isly.string(values)
+	}
+	export const type = isly.object<Creatable>({ name: isly.string(), location: Location.type.optional() })
 }
