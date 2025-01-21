d165fd986735bf3bc2e13fb29a787001d0afa476
Nils Hansander <nilshansander@hotmail.com>
2025-01-21 15:32:56 +0100

    Improve statistics tests (#493)
    * Improve statistics tests
    
    * fix vulnerability

--- Transaction/Statistics.spec.ts
+++ Transaction/Statistics.spec.ts
@@ -1,3 +1,4 @@
+import { isoly } from "isoly"
 import { pax2pay } from "../index"
 
 describe("Transaction Statistics", () => {
@@ -20,52 +21,14 @@ describe("Transaction Statistics", () => {
 		expect(pax2pay.Transaction.Statistics.type.is(statistics)).toBeTruthy()
 	})
 	it("should combine two statistics", () => {
-		const toCombine: pax2pay.Transaction.Statistics = {
-			capture: {
-				domestic: { count: 2, amount: 200 },
-				intraRegion: { count: 0, amount: 0 },
-				extraRegion: { count: 30, amount: 10500 },
-			},
-			refund: {
-				domestic: { count: 0, amount: 0 },
-				intraRegion: { count: 0, amount: 0 },
-				extraRegion: { count: 1, amount: 10 },
-			},
-			cursor: "cursor",
-		}
-		const expected: pax2pay.Transaction.Statistics = {
-			capture: {
-				domestic: { count: 3, amount: 300 },
-				intraRegion: { count: 0, amount: 0 },
-				extraRegion: { count: 35, amount: 10505 },
-			},
-			refund: {
-				domestic: { count: 0, amount: 0 },
-				intraRegion: { count: 0, amount: 0 },
-				extraRegion: { count: 1, amount: 10 },
-			},
-		}
-		const result = pax2pay.Transaction.Statistics.combine(statistics, toCombine, "SEK")
-		expect(result).toEqual(expected)
+		expect(pax2pay.Transaction.Statistics.combine(statistics, testData.toCombine, "SEK")).toEqual(
+			testData.combinationExpected
+		)
 	})
-	it("should append a transaction to statistics", () => {
-		const transactionToCount = {
-			counterpart: {
-				type: "card",
-				merchant: { name: "", id: "", category: "", address: "", city: "", zip: "", country: "SE" },
-				acquirer: { id: "", number: "" },
-			},
-			state: { transaction: { kind: "capture", amount: 99 } },
-		} as unknown as pax2pay.Transaction
-		const transactionToNotCount = {
-			counterpart: {
-				type: "card",
-				merchant: { name: "", id: "", category: "", address: "", city: "", zip: "", country: "NO" },
-				acquirer: { id: "", number: "" },
-			},
-			state: { transaction: { kind: "authorization", amount: 100 } },
-		} as unknown as pax2pay.Transaction
-		expect(pax2pay.Transaction.Statistics.append(statistics, transactionToCount, ["SE"], ["NO"], "SEK")).toEqual({
+	it("should append a domestic transaction to statistics", () => {
+		expect(
+			pax2pay.Transaction.Statistics.append(statistics, testData.transactionToCount, ["SE"], ["NO"], "SEK")
+		).toEqual({
 			capture: {
 				domestic: { count: 2, amount: 199 },
 				intraRegion: { count: 0, amount: 0 },
@@ -77,8 +40,87 @@ describe("Transaction Statistics", () => {
 				extraRegion: { count: 0, amount: 0 },
 			},
 		})
-		expect(pax2pay.Transaction.Statistics.append(statistics, transactionToNotCount, ["SE"], ["NO"], "SEK")).toEqual(
-			statistics
-		)
+	})
+	it("should only append capture or refund transactions to statistics", () => {
+		expect(
+			pax2pay.Transaction.Statistics.append(statistics, testData.transactionToNotCount, ["SE"], ["NO"], "SEK")
+		).toEqual(statistics)
+	})
+	it("should append a intraRegion transaction to statistics", () => {
+		expect(
+			pax2pay.Transaction.Statistics.append(statistics, testData.intraRegionTransaction, ["SE"], ["NO"], "SEK")
+		).toEqual({
+			capture: {
+				domestic: { count: 1, amount: 100 },
+				intraRegion: { count: 1, amount: 22 },
+				extraRegion: { count: 5, amount: 5 },
+			},
+			refund: {
+				domestic: { count: 0, amount: 0 },
+				intraRegion: { count: 0, amount: 0 },
+				extraRegion: { count: 0, amount: 0 },
+			},
+		})
+	})
+	it("should append a extraRegion transaction to statistics", () => {
+		expect(
+			pax2pay.Transaction.Statistics.append(statistics, testData.extraRegionTransaction, ["SE"], ["NO"], "SEK")
+		).toEqual({
+			capture: {
+				domestic: { count: 1, amount: 100 },
+				intraRegion: { count: 0, amount: 0 },
+				extraRegion: { count: 5, amount: 5 },
+			},
+			refund: {
+				domestic: { count: 0, amount: 0 },
+				intraRegion: { count: 0, amount: 0 },
+				extraRegion: { count: 1, amount: 33 },
+			},
+		})
 	})
 })
+namespace testData {
+	export const toCombine: pax2pay.Transaction.Statistics = {
+		capture: {
+			domestic: { count: 2, amount: 200 },
+			intraRegion: { count: 0, amount: 0 },
+			extraRegion: { count: 30, amount: 10500 },
+		},
+		refund: {
+			domestic: { count: 0, amount: 0 },
+			intraRegion: { count: 0, amount: 0 },
+			extraRegion: { count: 1, amount: 10 },
+		},
+		cursor: "cursor",
+	}
+	export const combinationExpected: pax2pay.Transaction.Statistics = {
+		capture: {
+			domestic: { count: 3, amount: 300 },
+			intraRegion: { count: 0, amount: 0 },
+			extraRegion: { count: 35, amount: 10505 },
+		},
+		refund: {
+			domestic: { count: 0, amount: 0 },
+			intraRegion: { count: 0, amount: 0 },
+			extraRegion: { count: 1, amount: 10 },
+		},
+	}
+	function mockTransaction(
+		kind: pax2pay.Rule.Base.Kind,
+		amount: number,
+		country: isoly.CountryCode.Alpha2
+	): pax2pay.Transaction {
+		return {
+			counterpart: {
+				type: "card",
+				merchant: { name: "", id: "", category: "", address: "", city: "", zip: "", country },
+				acquirer: { id: "", number: "" },
+			},
+			state: { transaction: { kind, amount } },
+		} as unknown as pax2pay.Transaction
+	}
+	export const transactionToCount = mockTransaction("capture", 99, "SE")
+	export const transactionToNotCount = mockTransaction("authorization", 100, "NO")
+	export const intraRegionTransaction = mockTransaction("capture", 22, "NO")
+	export const extraRegionTransaction = mockTransaction("refund", 33, "CA")
+}
