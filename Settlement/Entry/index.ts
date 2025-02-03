45fdce9feae95c0a005de78779689f4dc545225e
SiFalorSum <nilshansander@hotmail.com>
2025-02-03 09:21:09 +0100

    Added Entry.from function

--- Settlement/Entry/index.ts
+++ Settlement/Entry/index.ts
@@ -1,6 +1,9 @@
+import { gracely } from "gracely"
 import { isoly } from "isoly"
 import { isly } from "isly"
+import { Card } from "../../Card"
 import { Rail } from "../../Rail"
+import { Transaction } from "../../Transaction"
 import { Creatable as EntryCreatable } from "./Creatable"
 import { Failed as EntryFailed } from "./Failed"
 import * as mapping from "./fromLegacy"
@@ -31,4 +34,34 @@ export namespace Entry {
 		transaction: isly.string(),
 		created: isly.fromIs("isoly.DateTime", isoly.DateTime.is),
 	})
+	export function from(
+		creatable: Entry.Creatable,
+		transaction: Transaction.CardTransaction | gracely.Error | string,
+		card?: Card
+	): Entry | Entry.Failed {
+		let result: Entry | Entry.Failed
+		const reasons: string[] = []
+		const created = isoly.DateTime.now()
+		if (creatable.type == "unknown")
+			reasons.push("Unknown entry type.")
+		if (gracely.Error.is(transaction))
+			reasons.push(`gracely error: ${JSON.stringify(transaction)}`)
+		else if (typeof transaction == "string")
+			reasons.push(transaction || "No reason provided.")
+		else if (transaction.status != "finalized")
+			reasons.push(`Transaction ${transaction.id} on account ${transaction.accountId} unable to be finalized.`)
+		if (reasons.length > 0)
+			result = { status: "failed", reason: reasons.join("\n"), ...creatable, created }
+		else if (!card)
+			result = { status: "failed", reason: "Missing card", ...creatable, created }
+		else
+			result = {
+				status: "succeeded",
+				...(creatable as Creatable.Known),
+				created,
+				card: Rail.Address.Card.from(card),
+				transaction: (transaction as Transaction.CardTransaction).id,
+			}
+		return result
+	}
 }
