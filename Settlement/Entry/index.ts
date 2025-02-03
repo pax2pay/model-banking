b2168870502e097e137b0bbc3a1ac02d6ba3d3db
SiFalorSum <nilshansander@hotmail.com>
2025-02-03 09:39:15 +0100

    Entry union type

--- Settlement/Entry/index.ts
+++ Settlement/Entry/index.ts
@@ -8,32 +8,24 @@ import { Creatable as EntryCreatable } from "./Creatable"
 import { Failed as EntryFailed } from "./Failed"
 import * as mapping from "./fromLegacy"
 import { Entry as LegacyEntry } from "./Legacy"
+import { Succeeded as EntrySucceeded } from "./Succeeded"
 import { Summary as EntrySummary } from "./Summary"
 
-export interface Entry extends Omit<Entry.Creatable.Known, "transaction" | "card"> {
-	status: "succeeded"
-	card: Rail.Address.Card
-	transaction: string
-	created: isoly.DateTime
-}
+export type Entry = EntrySucceeded | EntryFailed
 export namespace Entry {
-	export interface Capture extends Entry {
+	export interface Capture extends EntrySucceeded {
 		type: "capture"
 	}
-	export interface Refund extends Entry {
+	export interface Refund extends EntrySucceeded {
 		type: "refund"
 	}
 	export import Creatable = EntryCreatable
 	export import Summary = EntrySummary
 	export import Legacy = LegacyEntry
 	export import Failed = EntryFailed
+	export import Succeeded = EntrySucceeded
 	export const fromLegacy = mapping.fromLegacy
-	export const type = EntryCreatable.Known.type.omit<"transaction" | "card">(["card", "transaction"]).extend<Entry>({
-		status: isly.string<"succeeded">("succeeded"),
-		card: Rail.Address.Card.type,
-		transaction: isly.string(),
-		created: isly.fromIs("isoly.DateTime", isoly.DateTime.is),
-	})
+	export const type = isly.union<EntrySucceeded | EntryFailed>(EntrySucceeded.type, EntryFailed.type)
 	export function from(
 		creatable: Entry.Creatable,
 		transaction: Transaction.CardTransaction | gracely.Error | string,
