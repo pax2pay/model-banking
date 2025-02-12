cb2f66b455a7d616ce1397708fc587afecfccae6
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2025-02-12 13:20:04 +0100

    Added visa card scheme. (#512)
    * Added visa card scheme.
    
    * Fixed build.

--- Card/Scheme.ts
+++ Card/Scheme.ts
@@ -2,6 +2,6 @@ import { isly } from "isly"
 
 export type Scheme = typeof Scheme.schemes[number]
 export namespace Scheme {
-	export const schemes = ["mastercard", "diners"] as const
+	export const schemes = ["mastercard", "diners", "visa"] as const
 	export const type = isly.string<Scheme>(schemes)
 }
