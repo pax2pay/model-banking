e544850c4f71b2c196df43d21c296327c0a28872
Anton Kroner <anton.kroner@pax2pay.com>
2025-02-17 16:04:27 +0100

    Almost working.

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite"
+
+export default defineConfig({
+	test: {
+		typecheck: { tsconfig: "./tsconfig.json" },
+		coverage: {
+			reporter: ["text", "json", "html"],
+			enabled: true,
+			all: true,
+			cleanOnRerun: true,
+			thresholds: { statements: 99, branches: 94, functions: 95, lines: 99 },
+		},
+		globals: true,
+		include: ["**/*.spec.[tj]s"],
+		testTimeout: 20000,
+		isolate: false,
+		exclude: ["node_modules", "dist"],
+		server: {
+			deps: {
+				inline: [
+					"cryptly",
+					"flagly",
+					"authly",
+					"isly",
+					"isoly",
+					"gracely",
+					"cloudly-http",
+					"cloudly-rest",
+					"cloudly-router",
+					"cloudly-formdata",
+					"cloudly-storage",
+					"cloudly-analytics-administration",
+					"cloudly-analytics-common",
+					"@cloudflare/workers-types",
+					"@userwidgets",
+					"@userwidgets/model",
+					"selectively",
+				],
+			},
+		},
+	},
+})
