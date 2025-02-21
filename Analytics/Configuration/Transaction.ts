7631dd97ff47f04dbf33de02ced0d29c26be39d1
Marcus Hanses <marcus.hanses@pax2pay.com>
2025-02-21 16:38:34 +0100

    Fixed some analytics stuff.

--- Analytics/Configuration/Transaction.ts
+++ Analytics/Configuration/Transaction.ts
@@ -5,7 +5,7 @@ import { Base } from "./Base"
 
 export namespace Transaction {
 	type Selectors = Base.Selectors | `value.${FlattenKeys<Required<ModelTransaction>>}${"[*]" | ""}`
-	export const mapping = {
+	export const mapping: filter.Mapping.RecordWithSelector<Selectors> = {
 		...Base.mapping,
 		counterpartType: "value.counterpart.type",
 		counterpart: { selector: "value.counterpart", transform: "stringify" },
@@ -32,34 +32,37 @@ export namespace Transaction {
 		actualBalance: { selector: "value.balance.actual", transform: "string" },
 		reservedBalance: { selector: "value.balance.reserved", transform: "string" },
 		availableBalance: { selector: "value.balance.available", transform: "string" },
-		operations: { selector: "value.operations[*]", transform: {
-			account: "account",
-			currency: "currency",
-			changes: { 
-				selector: "changes", 
-				transform: [
-					"array",
-					{
-						key: "key",
-						value: {
-							selector: "value",
-							transform: {
-								type: "type",
-								amount: { selector: "amount", transform: "string" },
-								status: "status",
-								result: { selector: "result", transform: "string" }
-							}
-						}
-					}
-				]
+		operations: {
+			selector: "value.operations[*]",
+			transform: {
+				account: "account",
+				currency: "currency",
+				changes: {
+					selector: "changes",
+					transform: [
+						"array",
+						{
+							key: "key",
+							value: {
+								selector: "value",
+								transform: {
+									type: "type",
+									amount: { selector: "amount", transform: "string" },
+									status: "status",
+									result: { selector: "result", transform: "string" },
+								},
+							},
+						},
+					],
+				},
+				type: "type",
+				transaction: "transaction",
+				counter: { selector: "counter", transform: "string" },
+				created: "created",
+				signature: "signature",
+				previous: "previous",
 			},
-			type: "type",
-			transaction: "transaction",
-			counter: { selector: "counter", transform: "string" },
-			created: "created",
-			signature: "signature",
-			previous: "previous",
-		}},
+		},
 		status: "value.status",
 		rail: "value.rail",
 		flags: "value.flags",
@@ -67,8 +70,7 @@ export namespace Transaction {
 		notes: { selector: "value.notes", transform: "stringify" },
 		risk: { selector: "value.risk", transform: "string" },
 		state: { selector: "value.state", transform: "stringify" },
-		// eslint-disable-next-line
-	} as const satisfies filter.Mapping.RecordWithSelector<Selectors>;
+	}
 	export type Fields = keyof typeof mapping
 	export const schema: listener.BigQueryApi.BaseField<Fields>[] = [
 		...Base.schema,
@@ -97,25 +99,38 @@ export namespace Transaction {
 		{ name: "actualBalance", type: "NUMERIC" },
 		{ name: "reservedBalance", type: "NUMERIC" },
 		{ name: "availableBalance", type: "NUMERIC" },
-		{ name: "operations", type: "RECORD", mode: "REPEATED", fields: [
+		{
+			name: "operations",
+			type: "RECORD",
+			mode: "REPEATED",
+			fields: [
 				{ name: "account", type: "STRING" },
 				{ name: "currency", type: "STRING" },
-				{ name: "changes", type: "RECORD", mode: "REPEATED", fields: [
-					{ name: "key", type: "STRING" },
-					{ name: "value", type: "RECORD", fields: [
-						{ name: "type", type: "STRING" },
-						{ name: "amount", type: "NUMERIC" },
-						{ name: "status", type: "STRING" },
-						{ name: "result", type: "NUMERIC", mode: "NULLABLE" },
-					]}
-				] },
+				{
+					name: "changes",
+					type: "RECORD",
+					mode: "REPEATED",
+					fields: [
+						{ name: "key", type: "STRING" },
+						{
+							name: "value",
+							type: "RECORD",
+							fields: [
+								{ name: "type", type: "STRING" },
+								{ name: "amount", type: "NUMERIC" },
+								{ name: "status", type: "STRING" },
+								{ name: "result", type: "NUMERIC", mode: "NULLABLE" },
+							],
+						},
+					],
+				},
 				{ name: "type", type: "STRING" },
 				{ name: "transaction", type: "STRING" },
 				{ name: "counter", type: "NUMERIC" },
 				{ name: "created", type: "TIMESTAMP" },
 				{ name: "signature", type: "STRING" },
 				{ name: "previous", type: "STRING" },
-			]
+			],
 		},
 		{ name: "status", type: "STRING" },
 		{ name: "rail", type: "STRING", mode: "NULLABLE" },
