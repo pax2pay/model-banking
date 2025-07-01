1bcf0e533a7ba9e403793abcee3fe9c93029faf5
Anton Kroner <anton.kroner@pax2pay.com>
2025-07-01 14:33:34 +0200

    [postgres] Transaction queries type.

--- Rail/index.ts
+++ Rail/index.ts
@@ -1,4 +1,5 @@
 import { isly } from "isly"
+import { isly as isly2 } from "isly2"
 import { Address as RailAddress } from "./Address"
 
 export type Rail = typeof Rail.rails[number]
@@ -17,5 +18,6 @@ export namespace Rail {
 		"credit",
 	] as const
 	export const type = isly.string<Rail>(rails)
+	export const type2 = isly2.string<Rail>("value", ...rails)
 	export import Address = RailAddress
 }
