3592cc1b9dbcb70f04b1563d55a06be8e7906a8c
Marcus Hanses <marcus.hanses@pax2pay.com>
2025-07-03 17:53:33 +0200

    Updated creatable.

--- User/index.ts
+++ User/index.ts
@@ -3,33 +3,48 @@ import { isly } from "isly"
 import { Access as UserAccess } from "./Access"
 import { Identity as UserIdentity } from "./Identity"
 import { JWT as UserJWT } from "./JWT"
+import { Me as UserMe } from "./Me"
 import { Password as UserPassword } from "./Password"
 
 export interface User {
 	email: string
-	// password: User.Password
 	access: User.Access
 	changed: isoly.DateTime
 	created: isoly.DateTime
+	password: {
+		changed: isoly.DateTime
+	}
 }
 export namespace User {
 	export import Access = UserAccess
 	export import Identity = UserIdentity
 	export import JWT = UserJWT
+	export import Me = UserMe
 	export import Password = UserPassword
-	export function fromCreatable(email: string, access: Access): User {
+	export function fromInvite(invite: Invite): User {
+		const now = isoly.DateTime.now()
 		return {
-			email,
-			// password,
-			access,
-			changed: isoly.DateTime.now(),
-			created: isoly.DateTime.now(),
+			email: invite.email,
+			access: invite.access,
+			changed: now,
+			created: now,
+			password: {
+				changed: now,
+			},
 		}
 	}
 	export interface Creatable {
 		invite: string
-		email: string
-		password: { value: string; confirm: string }
+		password: {
+			new: string
+			repeat: string
+		}
+	}
+	export namespace Creatable {
+		export const type = isly.object<Creatable>({
+			invite: isly.string(),
+			password: isly.object({ new: isly.string(), repeat: isly.string() }),
+		})
 	}
 	export interface Invite {
 		id: string
