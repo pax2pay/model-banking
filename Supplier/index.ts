53c182ebc2f137641f39d68970e123d64eac058c
Anton Kroner <anton.kroner@pax2pay.com>
2025-07-09 17:21:25 +0200

    [bankingcircle] Added bankingcircle supplier to test realm.

--- Supplier/index.ts
+++ Supplier/index.ts
@@ -3,6 +3,6 @@ import { isly } from "isly"
 export type Supplier = typeof Supplier.names[number]
 
 export namespace Supplier {
-	export const names = ["paxgiro", "clearbank", "paxgiroCredit"] as const
+	export const names = ["paxgiro", "clearbank", "bankingcircle", "paxgiroCredit"] as const
 	export const type = isly.string<Supplier>(names)
 }
