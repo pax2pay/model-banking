8c81b413022298656d41c7986e0940fb5d702403
marcushanses <79835961+marcushanses@users.noreply.github.com>
2025-07-11 13:37:13 +0100

    [User] New user client. (#570)
    * Making identity backwards compatible.
    
    * [Users] Created user client.

--- Client/index.ts
+++ Client/index.ts
@@ -15,26 +15,28 @@ import { Rules } from "./Rules"
 import { Settlements } from "./Settlements"
 import { Transactions as ClientTransactions } from "./Transactions"
 import { Treasury } from "./Treasury"
+import { Users } from "./Users"
 import { Version } from "./Version"
 
 export class Client {
 	realm?: string
 	organization?: string
 	readonly accounts: Accounts
-	readonly cards: Cards
-	readonly operations: Operations
-	readonly exchanges: Exchanges
-	readonly organizations: Organizations
-	readonly reports: Reports
 	readonly audits: Audit
+	readonly cards: Cards
+	readonly exchanges: Exchanges
+	readonly flags: Labels
+	readonly groups: Labels
 	readonly logs: Logs
+	readonly operations: Operations
+	readonly organizations: Organizations
+	readonly processors: Processor
+	readonly reports: Reports
 	readonly rules: Rules
 	readonly settlements: Settlements
 	readonly transactions: ClientTransactions
 	readonly treasury: Treasury
-	readonly flags: Labels
-	readonly groups: Labels
-	readonly processors: Processor
+	readonly users: Users
 	readonly version: Version
 	set key(value: string | undefined) {
 		this.client.key = value
@@ -51,20 +53,21 @@ export class Client {
 	private constructor(private readonly client: http.Client<gracely.Error>) {
 		this.client.onUnauthorized = async () => this.onUnauthorized != undefined && (await this.onUnauthorized(this))
 		this.accounts = new Accounts(this.client)
-		this.cards = new Cards(this.client)
-		this.operations = new Operations(this.client)
-		this.exchanges = new Exchanges(this.client)
-		this.organizations = new Organizations(this.client)
-		this.reports = new Reports(this.client)
 		this.audits = new Audit(this.client)
+		this.cards = new Cards(this.client)
+		this.exchanges = new Exchanges(this.client)
+		this.flags = new Labels(this.client, "flag")
+		this.groups = new Labels(this.client, "group")
 		this.logs = new Logs(this.client)
+		this.operations = new Operations(this.client)
+		this.organizations = new Organizations(this.client)
+		this.processors = new Processor(this.client)
+		this.reports = new Reports(this.client)
 		this.rules = new Rules(this.client)
 		this.settlements = new Settlements(this.client)
 		this.transactions = new ClientTransactions(this.client)
 		this.treasury = new Treasury(this.client)
-		this.flags = new Labels(this.client, "flag")
-		this.groups = new Labels(this.client, "group")
-		this.processors = new Processor(this.client)
+		this.users = new Users(this.client)
 		this.version = new Version(this.client)
 	}
 	readonly userwidgets = (server: string, application: string) =>
--- Key/index.ts
+++ Key/index.ts
@@ -1,5 +1,7 @@
+import { isoly } from "isoly"
 import { userwidgets } from "@userwidgets/model"
 import { Realm } from "../Realm"
+import { User } from "../User"
 import { Permissions as KeyPermissions } from "./Permissions"
 import { Roles as KeyRoles } from "./Roles"
 
@@ -10,17 +12,26 @@ type Claims = {
 
 export type Key = userwidgets.User.Key<userwidgets.User.Key.Creatable.Claims | Claims, Key.Permissions>
 export namespace Key {
-	export type Permissions = KeyPermissions
-	export namespace Permissions {
-		export type Realm = KeyPermissions.Realm
-		export type Organization = KeyPermissions.Organization
-	}
+	export import Permissions = KeyPermissions
 	export type Roles = KeyRoles
 	export const Roles = KeyRoles
 	export namespace Roles {
 		export type Role = KeyRoles.Role
 		export namespace Organization {
 			export type Role = KeyRoles.Organization.Role
+		}
+	}
+	export function from(payload: User.JWT.Payload, token: string): Key {
+		return {
+			audience: payload.aud,
+			email: payload.sub,
+			realm: payload.realm,
+			permissions: { [payload.realm + "-*"]: Permissions.from(payload.permission) },
+			expires: payload.exp ? isoly.DateTime.create(payload.exp) : isoly.DateTime.nextYear(isoly.DateTime.now(), 10),
+			issued: isoly.DateTime.create(payload.iat),
+			issuer: payload.iss,
+			name: { first: "", last: "" },
+			token,
 		}
 	}
 }
