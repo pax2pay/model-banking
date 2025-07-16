22ab498938abd24f97654b0590e74428f87329d2
Anton Kroner <anton.kroner@pax2pay.com>
2025-07-16 10:13:23 +0200

    [banking circle] Available currencies on account details and record of available currencies on suppliers.

--- Supplier/index.ts
+++ Supplier/index.ts
@@ -1,8 +1,13 @@
+import { isoly } from "isoly"
 import { isly } from "isly"
 
 export type Supplier = typeof Supplier.names[number]
-
 export namespace Supplier {
 	export const names = ["paxgiro", "clearbank", "bankingcircle", "paxgiroCredit"] as const
 	export const type = isly.string<Supplier>(names)
+	export const currencies: Record<Supplier, isoly.Currency[]> = {
+		clearbank: ["GBP"],
+		paxgiro: ["GBP", "SEK", "USD", "EUR"],
+		paxgiroCredit: ["GBP", "SEK", "USD", "EUR"],
+	}
 }
