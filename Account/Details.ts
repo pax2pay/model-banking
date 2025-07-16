703d8d769c649c8108d7af8be7b64f19ee6b6268
Anton Kroner <anton.kroner@pax2pay.com>
2025-07-16 10:13:23 +0200

    [banking circle] Available currencies on account details and record of available currencies on suppliers.

--- Account/Details.ts
+++ Account/Details.ts
@@ -1,16 +1,29 @@
+import { isoly } from "isoly"
 import { isly } from "isly"
 import { Rail } from "../Rail"
 import { Supplier } from "../Supplier"
 
 export interface Details {
 	supplier: Supplier
+	currencies: isoly.Currency[]
 	reference?: string
 	addresses: Rail.Address[]
 }
 export namespace Details {
 	export const type = isly.object<Details>({
 		supplier: Supplier.type,
+		currencies: isly.string(isoly.Currency.values).array(),
 		reference: isly.string().optional(),
 		addresses: Rail.Address.type.array(),
 	})
+	export interface Creatable {
+		supplier: Supplier
+		currency: isoly.Currency
+	}
+	export namespace Creatable {
+		export const type = isly.object<Creatable>({
+			supplier: Supplier.type,
+			currency: isly.string(isoly.Currency.values),
+		})
+	}
 }
