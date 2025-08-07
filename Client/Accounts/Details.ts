3240435e008ebd7e944b9fc44badf44bd7805799
Anton Kroner <anton.kroner@pax2pay.com>
2025-08-07 11:39:01 +0200

    Fixed return type of details.

--- Client/Accounts/Details.ts
+++ Client/Accounts/Details.ts
@@ -1,12 +1,11 @@
 import { gracely } from "gracely"
 import { http } from "cloudly-http"
 import { Account } from "../../Account"
-import { Rail } from "../../Rail"
 
 export class Details {
 	constructor(private readonly client: http.Client) {}
-	// TODO: fix return type of endpoint
-	async create(account: string, details: Account.Details.Creatable): Promise<Rail[] | gracely.Error> {
-		return this.client.post<Rail[]>(`/account/${account}/details`, details)
+
+	async create(account: string, details: Account.Details.Creatable): Promise<Account.Details | gracely.Error> {
+		return this.client.post<Account.Details>(`/account/${account}/details`, details)
 	}
 }
