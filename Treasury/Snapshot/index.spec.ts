85bd7f6c6ca5678b5f2c57a88461e06660197d71
Marcus Hanses <marcus.hanses@pax2pay.com>
2025-08-21 14:41:46 +0200

    Improve snapshot.

--- Treasury/Snapshot/index.spec.ts
+++ Treasury/Snapshot/index.spec.ts
@@ -7,42 +7,11 @@ describe("Treasury.Snapshot", () => {
 	it("is", () => {
 		expect(pax2pay.Treasury.Snapshot.type.is(snapshot)).toBeTruthy()
 	})
-	it("sum snapshot counterbalance accounts", () => {
-		const accounts: pax2pay.Treasury.Snapshot.Counterbalance[string]["account"] = {
-			id1: { amount: 5 },
-			id2: { amount: 5 },
-			id3: { amount: -5 },
-		}
-		expect(pax2pay.Treasury.Snapshot.Counterbalance.sum("USD", accounts)).toEqual(5)
-	})
-	it("sum validate counterbalance", () => {
-		const counterbalance: pax2pay.Treasury.Snapshot.Counterbalance = {
-			"safe-01": {
-				total: 5,
-				account: { id1: { amount: 5 }, id2: { amount: 5 }, id3: { amount: -5 } },
-			},
-		}
-		expect(pax2pay.Treasury.Snapshot.Counterbalance.validate("USD", counterbalance)).toEqual(true)
-	})
-	it("validate snapshot", () => {
-		const snapshot: pax2pay.Treasury.Snapshot = {
-			warnings: [],
-			supplier: "paxgiro",
-			currency: "GBP",
-			fiat: { safe: 23846.03, unsafe: 0, total: 23846.03, other: 0, buffer: 0, accounts: [] },
-			emoney: { actual: 23846.03 },
-			counterbalance: {
-				"safe-01": { total: 5, account: { id1: { amount: 5 } } },
-				"credit-01": { total: -5, account: { id2: { amount: -5 } } },
-			},
-		}
-		expect(pax2pay.Treasury.Snapshot.validate(snapshot)).toEqual(true)
-	})
 })
 const snapshot: pax2pay.Treasury.Snapshot = {
 	supplier: "paxgiro",
 	currency: "GBP",
-	emoney: { actual: 2 },
+	emoney: { available: 2 },
 	counterbalance: {
 		"safe-01": { total: 5, account: { id1: { amount: 5 } } },
 		"credit-01": { total: -5, account: { id2: { amount: -5 } } },
