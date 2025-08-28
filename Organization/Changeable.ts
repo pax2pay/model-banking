88a50d79a4edb1be30d61a2d357932dac8a0cf04
Anton Kroner <anton.kroner@pax2pay.com>
2025-08-28 16:53:53 +0200

    Organization.changeable has contact creatable type.

--- Organization/Changeable.ts
+++ Organization/Changeable.ts
@@ -3,11 +3,11 @@ import { Contact } from "./Contact"
 
 export interface Changeable {
 	name?: string
-	contact?: Contact
+	contact?: Contact.Creatable
 }
 export namespace Changeable {
 	export const type = isly.object<Changeable>({
 		name: isly.string().optional(),
-		contact: Contact.type.optional(),
+		contact: Contact.Creatable.type.optional(),
 	})
 }
