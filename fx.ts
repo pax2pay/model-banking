c3278a4ab57aff2b1535353f19fc3ce01babe171
SiFalorSum <nilshansander@hotmail.com>
2025-10-07 14:58:47 +0200

    Add isly type

--- fx.ts
+++ fx.ts
@@ -1,11 +1,12 @@
 import { isoly } from "isoly"
+import { isly } from "isly"
 
 export namespace fx {
 	export interface Quote {
 		id: string
 		created: isoly.DateTime
 		expires: isoly.DateTime
-		from: { currency: isoly.Currency; amount: number }
+		from: { amount: number; currency: isoly.Currency }
 		to: { amount: number; currency: isoly.Currency }
 		rate: {
 			base: number
@@ -19,6 +20,14 @@ export namespace fx {
 			from: isoly.Currency
 			to: isoly.Currency
 			amount: number
+		}
+		export namespace Creatable {
+			export const type = isly.object<Creatable>({
+				type: isly.string(["sell", "buy"]),
+				from: isly.fromIs("isoly.Currency", isoly.Currency.is),
+				to: isly.fromIs("isoly.Currency", isoly.Currency.is),
+				amount: isly.number(),
+			})
 		}
 	}
 }
