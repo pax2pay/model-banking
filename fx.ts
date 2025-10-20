98b35ab0d4978f6b9855c92a50dfafbf4f9b40b4
SiFalorSum <nilshansander@hotmail.com>
2025-10-20 12:04:30 +0200

    Ideas

--- fx.ts
+++ fx.ts
@@ -3,23 +3,20 @@ import { isly } from "isly"
 
 export namespace fx {
 	export interface Quote {
-		type: "ask" | "bid"
+		type: "sell" | "buy"
 		id: string
 		created: isoly.DateTime
 		expires: isoly.DateTime
 		markup: number // percent
 		from: { amount: number; currency: isoly.Currency }
 		to: { amount: number; currency: isoly.Currency }
-		bid: {
-			// Selling
-			rate: { base: number; markup: number; effective: number } // "volume" = `${to.currency} per ${from.currency}`
-		}
-		// For transparency, probably not needed? "ask" = what you would get back if you reversed the trade
-		// Why we might need this: "base" is not a reference/spot rate, it's Banking Circle's bid/ask rates.
-		// Accurate and relevant spot rates are hard to get, would require another service call and cost time.
-		ask: {
-			// Buying
-			rate: { base: number; markup: number; effective: number } // "price" = `${to.currency} per ${from.currency}`
+		rates: {
+			bid: { base: number; markup: number; effective: number }
+			// For transparency, probably not needed? "ask" = what you would get back if you reversed the trade
+			// Why we might need this: "base" is not a reference/spot rate, it's Banking Circle's bid/ask rates.
+			ask: { base: number; markup: number; effective: number }
+			// Spot rates would require another service call and cost time.
+			spotPrice?: number
 		}
 	}
 	const base = 11.08
@@ -34,6 +31,15 @@ export namespace fx {
 	}
 
 	export namespace Quote {
+		export type Creatable3 =
+			| {
+					from: { amount: number; currency: isoly.Currency }
+					to: isoly.Currency
+			  }
+			| {
+					from: isoly.Currency
+					to: { amount: number; currency: isoly.Currency }
+			  }
 		export type Creatable2 =
 			| { type: "sell"; bid: { from: isoly.Currency; to: isoly.Currency; amount: number } }
 			| { type: "buy"; from: isoly.Currency; to: isoly.Currency; amount: number }
