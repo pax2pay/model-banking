baa91e7f553564aa939aac79d437a735604627ce
SiFalorSum <nilshansander@hotmail.com>
2025-10-22 11:21:26 +0200

    remove fx from org changeable

--- Organization/Changeable.ts
+++ Organization/Changeable.ts
@@ -1,16 +1,13 @@
 import { isly } from "isly"
-import { Fx } from "../Account/Fx"
 import { Contact } from "./Contact"
 
 export interface Changeable {
 	name?: string
 	contact?: Contact.Creatable
-	fx?: Fx
 }
 export namespace Changeable {
 	export const type = isly.object<Changeable>({
 		name: isly.string().optional(),
 		contact: Contact.Creatable.type.optional(),
-		fx: Fx.type.optional(),
 	})
 }
