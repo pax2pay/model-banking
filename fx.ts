7fcf1c346af41c197dbf9404d91eee219ecb1c6f
AntonCroner <113976225+AntonCroner@users.noreply.github.com>
2025-10-23 11:16:57 +0200

    [fx] New quote. (#637)
    * [fx] New quote.
    
    * Removed lines.
    
    * Quote isly type.

--- fx.ts
+++ fx.ts
@@ -6,28 +6,54 @@ export namespace fx {
 		id: string
 		created: isoly.DateTime
 		expires: isoly.DateTime
+		account: { id: string; fx: { markup: number } }
+		fixed: Quote.Fixed
 		from: { amount: number; currency: isoly.Currency }
 		to: { amount: number; currency: isoly.Currency }
-		rate: {
-			base: number
-			markup: number
-			effective: number
-		}
+		rate: { base: number; markup: number; effective: number }
 	}
 	export namespace Quote {
-		export interface Creatable {
-			type: "sell" | "buy"
-			from: isoly.Currency
-			to: isoly.Currency
-			amount: number
+		export type Fixed = typeof Fixed.values[number]
+		export namespace Fixed {
+			export const values = ["from", "to"] as const
+			export const type = isly.string<Fixed>(values)
 		}
+		export type Creatable = Creatable.From | Creatable.To
 		export namespace Creatable {
-			export const type = isly.object<Creatable>({
-				type: isly.string(["sell", "buy"]),
-				from: isly.string(isoly.Currency.values),
-				to: isly.string(isoly.Currency.values),
-				amount: isly.number(),
-			})
+			export interface From {
+				from: { amount: number; currency: isoly.Currency }
+				to: isoly.Currency
+			}
+			export namespace From {
+				export const type = isly.object<From>({
+					from: isly.object<From["from"]>({ currency: isly.string(isoly.Currency.values), amount: isly.number() }),
+					to: isly.string(isoly.Currency.values),
+				})
+			}
+			export interface To {
+				from: isoly.Currency
+				to: { amount: number; currency: isoly.Currency }
+			}
+			export namespace To {
+				export const type = isly.object<To>({
+					from: isly.string(isoly.Currency.values),
+					to: isly.object<To["to"]>({ currency: isly.string(isoly.Currency.values), amount: isly.number() }),
+				})
+			}
+			export const type = isly.union<Creatable, From, To>(From.type, To.type)
 		}
 	}
+	export const type = isly.object<Quote>({
+		id: isly.string(),
+		created: isly.string(),
+		expires: isly.string(),
+		account: isly.object<Quote["account"]>({
+			id: isly.string(),
+			fx: isly.object<Quote["account"]["fx"]>({ markup: isly.number() }),
+		}),
+		fixed: Quote.Fixed.type,
+		from: isly.object<Quote["from"]>({ amount: isly.number(), currency: isly.string(isoly.Currency.values) }),
+		to: isly.object<Quote["to"]>({ amount: isly.number(), currency: isly.string(isoly.Currency.values) }),
+		rate: isly.object<Quote["rate"]>({ base: isly.number(), markup: isly.number(), effective: isly.number() }),
+	})
 }
