254506905641911d5873697f5646ac2cf7352a17
Anton Kroner <anton.kroner@pax2pay.com>
2025-10-29 15:22:09 +0100

    Again,.

--- Transaction/Exchange.ts
+++ Transaction/Exchange.ts
@@ -4,22 +4,15 @@ import { Amount } from "../Amount"
 export type Exchange = Exchange.From | Exchange.To
 export namespace Exchange {
 	export interface From {
-		type: "from"
 		rate: number
 		from: Amount
 	}
-	export const From = isly.object<From>({ rate: isly.number(), from: Amount.type, type: isly.string("from") })
+	export const From = isly.object<From>({ rate: isly.number(), from: Amount.type })
 	export interface To {
-		type: "to"
 		rate: number
 		to: Amount
 		quote: string
 	}
-	export const To = isly.object<To>({
-		rate: isly.number(),
-		to: Amount.type,
-		quote: isly.string(),
-		type: isly.string("to"),
-	})
+	export const To = isly.object<To>({ rate: isly.number(), to: Amount.type, quote: isly.string() })
 	export const type = isly.union<Exchange, From, To>(From, To)
 }
