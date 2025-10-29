990ce7800774da532dce8c7b863bbd476ca19e17
Anton Kroner <anton.kroner@pax2pay.com>
2025-10-29 13:33:33 +0100

    try

--- Transaction/Exchange.ts
+++ Transaction/Exchange.ts
@@ -6,8 +6,9 @@ export namespace Exchange {
 	export interface From {
 		rate: number
 		from: Amount
+		quote?: string
 	}
-	export const From = isly.object<From>({ rate: isly.number(), from: Amount.type })
+	export const From = isly.object<From>({ rate: isly.number(), from: Amount.type, quote: isly.string().optional() })
 	export interface To {
 		rate: number
 		to: Amount
