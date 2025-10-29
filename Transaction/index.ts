19d9c1bbc4fa8c3d0fa08fbc7cfeed9049a59151
Tove Brandt <tove.brandt@pax2pay.com>
2025-10-29 11:23:20 +0100

    lil fix

--- Transaction/index.ts
+++ Transaction/index.ts
@@ -148,8 +148,8 @@ export namespace Transaction {
 		account: { id: string; name: string; organization: string; address: Rail.Address },
 		balance: { actual: number; reserved: number; available: number },
 		operation: Operation | Status.Reason,
-		charges: Amount.Charge,
-		by: string | undefined
+		charges?: Amount.Charge,
+		by?: string
 	): Transaction {
 		const status: Status =
 			typeof operation == "string"
