9edc2a804502c66c13f1d4a3cffbdd5e2ef620af
SiFalorSum <nilshansander@hotmail.com>
2025-11-13 14:20:45 +0100

    Add record of type Audit.Type

--- Audit.ts
+++ Audit.ts
@@ -3,57 +3,61 @@ import { isly } from "isly"
 
 export type Audit = storage.AuditLogger.Entry<Audit.Type>
 export namespace Audit {
-	export type Type = {
-		rule: "change" | "add" | "remove"
-		user: "login" | "change"
-		settlements:
-			| "archive"
-			| "collect"
-			| "create"
-			| "inspect"
-			| "payout"
-			| "remove"
-			| "expect"
-			| "issue"
-			| "create-entry"
-			| "replace-entry"
-		marqeta: "gateway-create" | "gateway-update" | "hook-register" | "hook-update" | "product-create" | "user-create"
-		account:
-			| "buffer"
-			| "counterpart-remove"
-			| "counterpart-replace"
-			| "listener-create"
-			| "listener-remove"
-			| "rail-create"
-			| "rail-remove"
-			| "rail-replace"
-			| "rule-create"
-			| "rule-remove"
-			| "rule-replace"
-			| "create"
-			| "status"
-			| "operation"
-			| "remove"
-			| "update"
-		label: "create" | "remove" | "replace"
-		transaction: "operation" | "cancel" | "system" | "update" | "note"
-		organization:
-			| "group-replace"
-			| "listener-create"
-			| "listener-remove"
-			| "rule-create"
-			| "rule-remove"
-			| "rule-replace"
-			| "create"
-			| "remove"
-			| "update"
-			| "inactivate"
-		route: "create"
-		clearbank: "assessmentFailed" | "validationFailed"
-	}
-	export type Resource = typeof Resource.values[number]
+	export const actionsRecord = {
+		rule: ["change", "add", "remove"],
+		user: ["login", "change"],
+		settlements: [
+			"archive",
+			"collect",
+			"create",
+			"inspect",
+			"payout",
+			"remove",
+			"expect",
+			"issue",
+			"create-entry",
+			"replace-entry",
+		],
+		marqeta: ["gateway-create", "gateway-update", "hook-register", "hook-update", "product-create", "user-create"],
+		account: [
+			"buffer",
+			"counterpart-remove",
+			"counterpart-replace",
+			"listener-create",
+			"listener-remove",
+			"rail-create",
+			"rail-remove",
+			"rail-replace",
+			"rule-create",
+			"rule-remove",
+			"rule-replace",
+			"create",
+			"status",
+			"operation",
+			"remove",
+			"update",
+		],
+		label: ["create", "remove", "replace"],
+		transaction: ["operation", "cancel", "system", "update", "note"],
+		organization: [
+			"group-replace",
+			"listener-create",
+			"listener-remove",
+			"rule-create",
+			"rule-remove",
+			"rule-replace",
+			"create",
+			"remove",
+			"update",
+			"inactivate",
+		],
+		route: ["create"],
+		clearbank: ["assessmentFailed", "validationFailed"],
+	} as const
+	export type Type = { [K in keyof typeof actionsRecord]: typeof actionsRecord[K][number] }
+	export type Resource = keyof typeof actionsRecord
 	export namespace Resource {
-		export const values = ["rule", "user", "settlements", "marqeta"] as const
+		export const values = Object.keys(actionsRecord) as ReadonlyArray<Resource>
 		export const type = isly.string(values)
 	}
 }
