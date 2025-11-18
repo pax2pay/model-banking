92718667ff82695a6a000d5aee9164f199007567
Anton Kroner <anton.kroner@pax2pay.com>
2025-11-18 16:36:15 +0100

    [unguarded ream] Added realm.

--- Realm.ts
+++ Realm.ts
@@ -6,7 +6,7 @@ import { Supplier as modelSupplier } from "./Supplier"
 export type Realm = typeof Realm.realms[number]
 
 export namespace Realm {
-	export const realms = ["test", "uk", "eea"] as const
+	export const realms = ["test", "uk", "uguk", "eea"] as const
 	export const type = isly.string<Realm>(realms)
 	export const type2 = isly2
 		.string<Realm>("value", ...realms)
@@ -15,15 +15,17 @@ export namespace Realm {
 	export function toString(): string {
 		return realms.toString().replaceAll(",", ", ") + "."
 	}
-	export const currency: Record<Realm, isoly.Currency> = { test: "EUR", uk: "GBP", eea: "EUR" }
+	export const currency: Record<Realm, isoly.Currency> = { test: "EUR", uk: "GBP", uguk: "GBP", eea: "EUR" }
 	export const suppliers: Record<Realm, modelSupplier[]> = {
 		test: ["paxgiro", "bankingcircle", "paxgiroCredit"],
 		uk: ["clearbank", "bankingcircle"],
+		uguk: ["bankingcircle"],
 		eea: [],
 	}
 	export interface Suppliers extends Record<Realm, modelSupplier[]> {
 		test: ["paxgiro", "bankingcircle", "paxgiroCredit"]
 		uk: ["clearbank", "bankingcircle"]
+		uguk: ["bankingcircle"]
 		eea: []
 	}
 	export type Supplier<P extends keyof Suppliers> = Pick<Suppliers, P>[P][number]
--- User/Access/index.ts
+++ User/Access/index.ts
@@ -4,6 +4,7 @@ import { Permission as AccessPermission } from "./Permission"
 /** read < write < developer < admin */
 export interface Access {
 	uk?: Access.Permission.Realm
+	uguk?: Access.Permission.Realm
 	eea?: Access.Permission.Realm
 	test?: Access.Permission.Realm
 	"*"?: Access.Permission.Global
@@ -12,6 +13,7 @@ export namespace Access {
 	export import Permission = AccessPermission
 	export const type = isly.object({
 		uk: Access.Permission.type.optional(),
+		uguk: Access.Permission.type.optional(),
 		eea: Access.Permission.type.optional(),
 		test: Access.Permission.type.optional(),
 		"*": isly.object({ user: Access.Permission.Level.type.optional() }).optional(),
