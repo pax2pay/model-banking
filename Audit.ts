9f1ca6901d05217efce3e013ae3c8379f7939989
Nils Hansander <nilshansander@hotmail.com>
2025-11-18 11:25:38 +0100

    Add record of type Audit.Type (#645)
    * Add record of type Audit.Type
    
    * Improvements

--- Audit.ts
+++ Audit.ts
@@ -3,57 +3,61 @@ import { isly } from "isly"
 
 export type Audit = storage.AuditLogger.Entry<Audit.Type>
 export namespace Audit {
-	export type Type = {
-		rule: "change" | "add" | "remove"
-		user: "login" | "change"
-		settlements:
-			| "archive"
-			| "collect"
-			| "create"
-			| "inspect"
-			| "payout"
-			| "remove"
-			| "expect"
-			| "issue"
-			| "create-entry"
-			| "replace-entry"
-		marqeta: "gateway-create" | "gateway-update" | "hook-register" | "hook-update" | "product-create" | "user-create"
-		account:
-			| "buffer"
-			| "counterpart-remove"
-			| "counterpart-replace"
-			| "listener-create"
-			| "listener-remove"
-			| "rail-create"
-			| "rail-remove"
-			| "rail-replace"
-			| "rule-create"
-			| "rule-remove"
-			| "rule-replace"
-			| "create"
-			| "status"
-			| "operation"
-			| "remove"
-			| "update"
-		label: "create" | "remove" | "replace"
-		transaction: "operation" | "cancel" | "system" | "update" | "note"
-		organization:
-			| "group-replace"
-			| "listener-create"
-			| "listener-remove"
-			| "rule-create"
-			| "rule-remove"
-			| "rule-replace"
-			| "create"
-			| "remove"
-			| "update"
-			| "inactivate"
-		route: "create"
-		clearbank: "assessmentFailed" | "validationFailed"
-	}
-	export type Resource = typeof Resource.values[number]
+	export type Type = { [K in keyof typeof Resource.value]: typeof Resource.value[K][number] }
+	export type Resource = keyof typeof Resource.value
 	export namespace Resource {
-		export const values = ["rule", "user", "settlements", "marqeta"] as const
-		export const type = isly.string(values)
+		export const value = {
+			rule: ["change", "add", "remove"],
+			user: ["login", "change"],
+			settlements: [
+				"archive",
+				"collect",
+				"create",
+				"inspect",
+				"payout",
+				"remove",
+				"expect",
+				"issue",
+				"create-entry",
+				"replace-entry",
+			],
+			marqeta: ["gateway-create", "gateway-update", "hook-register", "hook-update", "product-create", "user-create"],
+			account: [
+				"buffer",
+				"counterpart-remove",
+				"counterpart-replace",
+				"listener-create",
+				"listener-remove",
+				"rail-create",
+				"rail-remove",
+				"rail-replace",
+				"rule-create",
+				"rule-remove",
+				"rule-replace",
+				"create",
+				"status",
+				"operation",
+				"remove",
+				"update",
+			],
+			label: ["create", "remove", "replace"],
+			transaction: ["operation", "cancel", "system", "update", "note"],
+			organization: [
+				"group-replace",
+				"listener-create",
+				"listener-remove",
+				"rule-create",
+				"rule-remove",
+				"rule-replace",
+				"create",
+				"remove",
+				"update",
+				"inactivate",
+			],
+			route: ["create"],
+			clearbank: ["assessmentFailed", "validationFailed"],
+		} as const
+		export const keys = Object.keys(value) as (keyof typeof value)[]
+		export const type = isly.string(keys)
 	}
 }
