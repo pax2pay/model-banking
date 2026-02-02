325310ee8100c9fb9bbbfec9cbfb7d6f988aa5c6
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2026-02-02 14:29:52 +0100

    prohibited

--- Organization/Risk.ts
+++ Organization/Risk.ts
@@ -3,6 +3,6 @@ import { isly } from "isly"
 export type Risk = typeof Risk.values[number]
 
 export namespace Risk {
-	export const values = ["low", "medium", "high"] as const
+	export const values = ["low", "medium", "high", "prohibited"] as const
 	export const type = isly.string(values)
 }
