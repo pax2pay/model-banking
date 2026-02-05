f12f4d20c272a3364e13e1fd1f93f4815c3540eb
tovebr <70133569+tovebr@users.noreply.github.com>
2026-02-05 08:30:53 +0100

    [Safeguarding] Remove UGUK. (#667)

--- Realm.ts
+++ Realm.ts
@@ -7,7 +7,7 @@ import { zod } from "./zod"
 export type Realm = zod.infer<typeof Realm.typeZod>
 
 export namespace Realm {
-	export const realms = ["test", "uk", "uguk", "eea"] as const
+	export const realms = ["test", "uk", "eea"] as const
 	export const type = isly.string<Realm>(realms)
 	export const typeZod = zod.enum(realms)
 	export const type2 = isly2
@@ -17,17 +17,15 @@ export namespace Realm {
 	export function toString(): string {
 		return realms.toString().replaceAll(",", ", ") + "."
 	}
-	export const currency: Record<Realm, isoly.Currency> = { test: "EUR", uk: "GBP", uguk: "GBP", eea: "EUR" }
+	export const currency: Record<Realm, isoly.Currency> = { test: "EUR", uk: "GBP", eea: "EUR" }
 	export const suppliers: Record<Realm, modelSupplier[]> = {
 		test: ["paxgiro", "bankingcircle", "paxgiroCredit"],
 		uk: ["clearbank", "bankingcircle"],
-		uguk: ["bankingcircle"],
 		eea: [],
 	}
 	export interface Suppliers extends Record<Realm, modelSupplier[]> {
 		test: ["paxgiro", "bankingcircle", "paxgiroCredit"]
 		uk: ["clearbank", "bankingcircle"]
-		uguk: ["bankingcircle"]
 		eea: []
 	}
 	export type Supplier<P extends keyof Suppliers> = Pick<Suppliers, P>[P][number]
--- User/Access/index.ts
+++ User/Access/index.ts
@@ -8,14 +8,12 @@ export namespace Access {
 	export import Permission = AccessPermission
 	export const type = isly.object({
 		uk: Access.Permission.type.optional(),
-		uguk: Access.Permission.type.optional(),
 		eea: Access.Permission.type.optional(),
 		test: Access.Permission.type.optional(),
 		"*": isly.object({ user: Access.Permission.Level.type.optional() }).optional(),
 	})
 	export const typeZod = zod.object({
 		uk: Access.Permission.typeZod.optional(),
-		uguk: Access.Permission.typeZod.optional(),
 		eea: Access.Permission.typeZod.optional(),
 		test: Access.Permission.typeZod.optional(),
 		"*": zod.object({ user: Access.Permission.Level.typeZod.optional() }).optional(),
