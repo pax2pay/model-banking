c6e81f12eeb5d983a558459e785767cff73e18ac
Anton Kroner <anton.kroner@pax2pay.com>
2026-02-05 18:05:53 +0100

    Removed thing.

--- Organization/Changeable.ts
+++ Organization/Changeable.ts
@@ -1,16 +1,13 @@
 import { isly } from "isly"
-import { Type } from "../Account/Type"
 import { Contact } from "./Contact"
 
 export interface Changeable {
 	name?: string
 	contact?: Contact.Creatable
-	type?: Type
 }
 export namespace Changeable {
 	export const type = isly.object<Changeable>({
 		name: isly.string().optional(),
 		contact: Contact.Creatable.type.optional(),
-		type: Type.type.optional(),
 	})
 }
