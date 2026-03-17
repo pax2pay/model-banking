e1eb85f2f1590ccc17b282be7c92787c0c380cc7
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2026-03-17 16:51:22 +0100

    Add transaction.reference.instruction.

--- Transaction/Reference.ts
+++ Transaction/Reference.ts
@@ -3,9 +3,10 @@ import { Supplier } from "../Supplier"
 
 export interface Reference {
 	supplier?: Supplier
-	reference?: string
+	reference?: string // Suppliers id for the transaction
 	returnId?: string
 	endToEndId?: string
+	instruction?: string // SWIFT instruction id, TransactionReference on Banking circle hook
 }
 
 export namespace Reference {
@@ -14,5 +15,6 @@ export namespace Reference {
 		reference: isly.string().optional(),
 		returnId: isly.string().optional(),
 		endToEndId: isly.string().optional(),
+		instruction: isly.string().optional(),
 	})
 }
