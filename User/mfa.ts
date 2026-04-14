4a59de6fdaee64a95b50cc0c775c6a13e4f7fd3c
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2026-04-14 13:34:55 +0200

    add typeguard

--- User/mfa.ts
+++ User/mfa.ts
@@ -6,10 +6,12 @@ export namespace mfa {
 		export const typeZod = zod.object({
 			key: zod.string().nonempty(), // secret
 		})
+		export const is = (value: any): value is Totp => typeZod.safeParse(value).success
 
 		export type Otp = zod.infer<typeof Otp.typeZod>
 		export namespace Otp {
 			export const typeZod = zod.string().length(6).regex(/^\d+$/)
+			export const is = (value: any): value is Otp => typeZod.safeParse(value).success
 		}
 	}
 }
