bbf2949ca4cf5601a1e818b3fb90e0242f052fea
Anton Kroner <anton.kroner@pax2pay.com>
2026-04-22 16:14:17 +0200

    [cass] Added snapshot note endpoint to client.

--- Client/Treasury.ts
+++ Client/Treasury.ts
@@ -1,6 +1,8 @@
 import { gracely } from "gracely"
 import { isoly } from "isoly"
 import { http } from "cloudly-http"
+import { Supplier } from "../Supplier"
+import { Transaction } from "../Transaction"
 import { Treasury as TreasuryModel } from "../Treasury"
 
 export class Treasury {
@@ -15,6 +17,14 @@ export class Treasury {
 	}
 	async listAccounts(): Promise<TreasuryModel.Account.Listable[] | gracely.Error> {
 		return this.client.get<TreasuryModel.Account.Listable[]>(`/treasury/account`)
+	}
+	async notify(
+		hour: isoly.DateTime,
+		supplier: Supplier,
+		currency: isoly.Currency,
+		note: Transaction.Note.Creatable
+	): Promise<Transaction.Note[] | gracely.Error> {
+		return this.client.post<Transaction.Note[]>(`/treasury/snapshot/${hour}/${supplier}/${currency}/note`, note)
 	}
 	async report(start: isoly.Date, end: isoly.Date, supplier: string): Promise<string | gracely.Error> {
 		return this.client.get<string>(`/treasury/${supplier}/report?start=${start}&end=${end}`)
