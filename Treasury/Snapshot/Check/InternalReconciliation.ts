3b2266a6ad938d76a79c233c4deaa913cb87dee7
marcushanses <79835961+marcushanses@users.noreply.github.com>
2026-05-05 11:46:56 +0200

    Add accounts to internal reconciliation. (#695)

--- Treasury/Snapshot/Check/InternalReconciliation.ts
+++ Treasury/Snapshot/Check/InternalReconciliation.ts
@@ -1,3 +1,5 @@
+import { isoly } from "isoly"
+import { Emoney } from "../Emoney"
 import { Base as ResultBase } from "./Base"
 
 export interface InternalReconciliation extends ResultBase {
@@ -5,5 +7,11 @@ export interface InternalReconciliation extends ResultBase {
 	counterbalance: number
 	emoney: number
 	discrepancy: number
+	accounts: InternalReconciliation.Account[]
 }
-export namespace InternalReconciliation {}
+export namespace InternalReconciliation {
+	export interface Account extends Omit<Emoney, "balances"> {
+		balance: number
+		currency: isoly.Currency
+	}
+}
