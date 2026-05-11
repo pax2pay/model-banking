f2dcd2b845a464ad01bae51f17da2fedb0d8f3d8
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2026-05-11 11:38:07 +0200

    lkasfjsdsdl;fjkksdf

--- Treasury/Snapshot/Check/LedgerIntegrity.ts
+++ Treasury/Snapshot/Check/LedgerIntegrity.ts
@@ -1,7 +1,11 @@
+import { isoly } from "isoly"
 import { Base } from "./Base"
 
 export interface LedgerIntegrity extends Base {
 	check: "ledger integrity"
+	opening: { at: isoly.DateTime; balance: number }
+	ledger: { change: number }
+	closing: { at: isoly.DateTime; balance: number }
 	failed: string[]
 	incomplete: string[]
 	passed: number
