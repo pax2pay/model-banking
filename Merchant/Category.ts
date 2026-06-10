df69d4a5a1daeb8121e22fe02576b76266601111
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2026-06-10 13:56:38 +0200

    make sure Category is string

--- Merchant/Category.ts
+++ Merchant/Category.ts
@@ -3,7 +3,8 @@ import { isly } from "isly"
 export type Category = string
 
 export namespace Category {
-	export const type = isly.fromIs<Category>("Merchant.Category", (value: any): value is Category =>
-		/^\d{4}$/.test(value)
+	export const type = isly.fromIs<Category>(
+		"Merchant.Category",
+		(value: any): value is Category => typeof value === "string" && /^\d{4}$/.test(value)
 	)
 }
