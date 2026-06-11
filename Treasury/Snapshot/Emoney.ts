2993ce7505c1bc3381120f05d31530ed5c5d73ef
Marcus Hanses <marcus.hanses@pax2pay.com>
2026-06-11 13:46:32 +0200

    Added created information on the emoney snapshot.

--- Treasury/Snapshot/Emoney.ts
+++ Treasury/Snapshot/Emoney.ts
@@ -5,6 +5,7 @@ import { Supplier } from "../../Supplier"
 export interface Emoney {
 	organization: string
 	account: string
+	created?: isoly.DateTime
 	supplier?: Supplier
 	currencies?: isoly.Currency[]
 	timestamp: isoly.DateTime
