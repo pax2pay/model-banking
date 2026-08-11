dc05d7f5aeb09a9f54948bae5ee7d4b97c252d73
Tove Brandt <tove.brandt@pax2pay.com>
2026-08-11 14:11:43 +0200

    Retype rules to be never.

--- Rule/control.ts
+++
@@ -1,7 +0,0 @@
-import { selectively } from "selectively"
-import type { Rule } from "."
-import type { State } from "./State"
-
-export function control(rule: Rule, state: State, macros?: Record<string, selectively.Definition>): boolean {
-	return selectively.parse(rule.condition).is(state)
-}
--- Rule/type.ts
+++
@@ -1,3 +0,0 @@
-import { Other } from "./Other"
-
-export const type = Other.type
