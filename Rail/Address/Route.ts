817854fa8856c2bc495993d4c0b74e5f62efdf8d
Magnus Gustafsson <magnus.gustafsson@pax2pay.com>
2026-08-17 15:41:03 +0200

    Add typeZod for Route type.

--- Rail/Address/Route.ts
+++ Rail/Address/Route.ts
@@ -1,10 +1,12 @@
 import { isly } from "isly"
+import { zod } from "../../zod"
 
 export type Route = Route.Account | Route.Service
 export namespace Route {
 	export type Account = { type: "account"; id: string }
 	export namespace Account {
 		export const type = isly.object<Account>({ type: isly.string("account"), id: isly.string() })
+		export const typeZod = zod.object({ type: zod.literal("account"), id: zod.string() })
 	}
 	export type Service = { type: "service"; service: string; configuration: string }
 	export namespace Service {
@@ -13,8 +15,14 @@ export namespace Route {
 			service: isly.string(),
 			configuration: isly.string(),
 		})
+		export const typeZod = zod.object({
+			type: zod.literal("service"),
+			service: zod.string(),
+			configuration: zod.string(),
+		})
 	}
 	export const type = isly.union<Route, Account, Service>(Account.type, Service.type)
+	export const typeZod = zod.union([Account.typeZod, Service.typeZod])
 	export function fromLegacy(route: string | Route): Route {
 		return typeof route == "string" ? { type: "account", id: route } : route
 	}
