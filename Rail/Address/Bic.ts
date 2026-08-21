71afeaba04f41815a799670630c36152789fba78
Marcus Hanses <marcus.hanses@pax2pay.com>
2026-08-21 15:25:59 +0200

    Added swift address type

---
+++ Rail/Address/Bic.ts
@@ -0,0 +1,27 @@
+import { isly } from "isly"
+import { zod } from "../../zod"
+
+export interface Bic {
+	type: "bic"
+	account: string
+	institution: string // BIC, validated against the SWIFT directory by the supplier
+	holder: string
+	transactor?: string
+}
+export namespace Bic {
+	export const currencies = ["USD"] as const
+	export const type = isly.object<Bic>({
+		type: isly.string("bic"),
+		account: isly.string(),
+		institution: isly.string(),
+		holder: isly.string(),
+		transactor: isly.string().optional(),
+	})
+	export const typeZod = zod.object({
+		type: zod.literal("bic"),
+		account: zod.string(),
+		institution: zod.string(),
+		holder: zod.string(),
+		transactor: zod.string().optional(),
+	})
+}
