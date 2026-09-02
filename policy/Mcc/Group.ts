0123562fb70028c9389dcb3d4a0ae30a808337f4
Magnus Gustafsson <magnus.ja.gustafsson@gmail.com>
2026-09-02 10:51:31 +0200

    Zod Policy Mcc. (#746)
    * Zod Policy Mcc.
    
    * zod.ZodType<...>
    
    * ZodType<Category>

--- policy/Mcc/Group.ts
+++ policy/Mcc/Group.ts
@@ -1,5 +1,6 @@
 import { isly } from "isly"
 import { Merchant } from "../../Merchant"
+import { zod } from "../../zod"
 
 export interface Group {
 	values: Merchant.Category[]
@@ -12,8 +13,16 @@ export namespace Group {
 	}
 	export namespace Range {
 		export const type = isly.object<Range>({ from: Merchant.Category.type, to: Merchant.Category.type })
+		export const typeZod: zod.ZodType<Range> = zod.object({
+			from: Merchant.Category.typeZod,
+			to: Merchant.Category.typeZod,
+		})
 	}
 	export const type = isly.object<Group>({ values: Merchant.Category.type.array(), ranges: Range.type.array() })
+	export const typeZod: zod.ZodType<Group> = zod.object({
+		values: zod.array(Merchant.Category.typeZod),
+		ranges: zod.array(Range.typeZod),
+	})
 	export function within(set: Group, category: Merchant.Category): boolean {
 		return set.values.includes(category) || set.ranges.some(r => r.from <= category && category <= r.to)
 	}
